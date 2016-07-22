21c7b1e598fdda1f7f5ab704bc79ba219967deb3
Edward Andress <eddie_andress@hotmail.com>
2016-07-22 16:55:39 +0100

    correspondence controller #index spec

--- spec/controllers/correspondence_controller_spec.rb
+++ spec/controllers/correspondence_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe CorrespondenceController, type: :controller do
 
+  let(:all_correspondence) { create_list(:correspondence, 5) }
+
+  context 'GET index' do
+
+    before { get :index }
+
+    it 'assigns @correspondence' do
+      expect(assigns(:correspondence)).to match(all_correspondence)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+  end
+
 end
