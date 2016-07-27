b2a06de00f1b2abe363c67d6f4c9cdac116b9947
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-07-27 15:52:20 +0100

    Style the Correspondence results

--- spec/features/correspondence_list_view_spec.rb
+++ spec/features/correspondence_list_view_spec.rb
@@ -9,7 +9,7 @@ feature 'a user can see all correspondence on the system' do
   scenario 'in a single list' do
     visit '/'
     Correspondence.all.each do |correspondence|
-      expect(page).to have_content(correspondence.created_at)
+      expect(page).to have_content(correspondence.name)
       expect(page).to have_content(correspondence.typus.humanize)
       expect(page).to have_content(correspondence.topic.humanize)
     end
