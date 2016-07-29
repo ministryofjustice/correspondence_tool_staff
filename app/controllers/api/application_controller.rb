6ca357ff48358e306f43b1d173631e9ef6fa943e
Edward Andress <eddie_andress@hotmail.com>
2016-07-29 16:17:46 +0100

    skip csrf protection if request is json

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,7 @@
 module Api
 
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+    protect_from_forgery unless: -> { request.format.json? }
 
     before_action :authenticate
 
