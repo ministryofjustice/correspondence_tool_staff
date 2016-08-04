f599471ceab29fed7f44a91d60dea7e15ccce7f2
Edward Andress <eddie_andress@hotmail.com>
2016-08-04 11:54:21 +0100

    list view: format date and add status

--- spec/features/correspondence_list_view_spec.rb
+++ spec/features/correspondence_list_view_spec.rb
@@ -9,7 +9,7 @@ feature 'a user can see all correspondence on the system' do
   scenario 'in a single list' do
     visit '/'
     Correspondence.all.each do |correspondence|
-      expect(page).to have_content(correspondence.created_at.strftime("%d/%m/%Y %H:%M"))
+      expect(page).to have_content(correspondence.created_at.strftime("%d/%m/%y"))
       expect(page).to have_content(correspondence.category.humanize)
       expect(page).to have_content(correspondence.topic.humanize)
     end
