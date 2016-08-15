5ee022aeaab09de6c8df46caa5b6e67bb34d5a92
Edward Andress <eddie_andress@hotmail.com>
2016-08-15 10:24:46 +0100

    seeds categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
+puts "----Seeding Categories----"
+Category.find_or_create_by!(name: 'freedom_of_information_request', abbreviation: 'foi', internal_time_limit: 9, external_time_limit: 19)
+Category.find_or_create_by!(name: 'general_enquiry', abbreviation: 'gq', internal_time_limit: 9, external_time_limit: 14)
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
   factory :category do
 
     name "freedom_of_information_request"
-    abbreviation "FOI"
+    abbreviation "foi"
     internal_time_limit 9
     external_time_limit 19
 
     trait :gq do
       name "general_enquiry"
-      abbreviation "GQ"
+      abbreviation "gq"
       external_time_limit 14
     end
 
