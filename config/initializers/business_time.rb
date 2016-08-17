8853432135e2fdd91c9900964c20afa371cda974
Edward Andress <eddie_andress@hotmail.com>
2016-08-17 17:37:09 +0100

    Initializer for all business_time config

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,6 +1,7 @@
 #BusinessTime::Config.load("#{Rails.root}/config/business_time.yml")
 
 # or you can configure it manually:  look at me!  I'm Tim Ferriss!
-#  BusinessTime::Config.beginning_of_workday = "10:00 am"
-#  BusinessTime::Config.end_of_workday = "11:30 am"
+BusinessTime::Config.beginning_of_workday = "09:00 am"
+BusinessTime::Config.end_of_workday = "17:00 pm"
+BusinessTime::Config.work_week = %w( mon tue wed thu fri )
 BusinessTime::Config.holidays = BankHoliday.all.map(&:date).map(&:to_date)
