3fb31aaedfbd7b5aa2374904e44a9ff8b7ed5a35
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-12-14 15:51:38 +0000

    Remove SSL test, this is done at nginx lvl

--- spec/controllers/heartbeat_controller_spec.rb
+++ spec/controllers/heartbeat_controller_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 RSpec.describe HeartbeatController, type: :controller do
 
   describe '#ping' do
-    describe 'does not force ssl' do
-      before do
-        allow(Rails).to receive(:env).and_return(double(development?: false, production?: true))
-      end
-
-      it 'ping the endpoint' do
-        get :ping
-        expect(response.status).not_to eq(301)
-      end
-
-    end
-
     it 'returns JSON with app information' do
       get :ping
 
