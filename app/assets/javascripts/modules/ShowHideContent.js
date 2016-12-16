098e7aedd7473ee084426099607e05789ba96699
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-12-16 10:15:21 +0000

    CT-223 Show/Hide Reasons for rejection (UI)
    .js-hidden class hides content by default unless js is disabled
    
    GOVUK Frontend Toolkit  has a method called ShowHideContent() which
    helps showing/hiding content and applies accessibility ARIA HTML
    attributes
    
    GOVUK.ShowHideContent() uses the HTML data attribute called data-target
    to determine what element to show/hide. The element that you would like
    to be shown or hidden needs to have an ID that matches the data-target's
    value.
    
    Unfortunately, GOVUK FormBuilder gem does not apply this HTML attribute.
    As a workaround I create a moj module called ShowHideContent() which
    searches through the page and then this data attribute dynamically.
    It takes the radio button label's 'for' attribute and adds '_content'
    as a suffix.
    
    Example:
    <label class="block-label selection-button-radio" for="assignment_state_rejected">
      <input type="radio" value="rejected" name="assignment[state]" id="assignment_state_rejected">Reject
    </label>
    
    Generates:
    <label class="block-label selection-button-radio" for="assignment_state_rejected" data-target="assignment_state_rejected_content">
      <input type="radio" value="rejected" name="assignment[state]" id="assignment_state_rejected">Reject
    </label>

---
+++ app/assets/javascripts/modules/ShowHideContent.js
@@ -0,0 +1,26 @@
+moj.Modules.ShowHideContent = {
+  el: 'label:has(":radio, :checkbox")',
+  init: function() {
+    var self = this;
+
+    var showHideContent = new GOVUK.ShowHideContent();
+
+    self.cacheEls();
+
+    self.bindEvents();
+
+    showHideContent.init();
+  },
+
+  cacheEls: function(){
+    this.radioButtonLabels = $(this.el);
+  },
+
+  bindEvents: function() {
+    this.radioButtonLabels.each(function (index, element) {
+      var showHideTarget = element.getAttribute('for') + '_content';
+      element.setAttribute('data-target', showHideTarget)
+    })
+  }
+
+}
--- spec/features/03_respond_to_drafter_assignment/respond_to_assignment_spec.rb
+++ spec/features/03_respond_to_drafter_assignment/respond_to_assignment_spec.rb
@@ -27,7 +27,7 @@ feature 'respond to drafter assignment' do
     expect(page).to have_content 'I would like to know about XYZ'
 
     choose 'Accept'
-    expect(page).not_to have_content 'Reasons for rejection'
+    expect(page).to have_selector('#assignment_reasons_for_rejection', visible: false)
     click_button 'Confirm'
 
     expect(current_path).to eq case_path kase
@@ -42,7 +42,7 @@ feature 'respond to drafter assignment' do
     expect(page).to have_content 'I would like to know about XYZ'
 
     choose 'Reject'
-    expect(page).to have_content 'Reasons for rejection'
+    expect(page).to have_selector('#assignment_reasons_for_rejection', visible: true)
     fill_in 'Reasons for rejection', with: 'I am not the correct KILO for this'
     click_button 'Confirm'
 
