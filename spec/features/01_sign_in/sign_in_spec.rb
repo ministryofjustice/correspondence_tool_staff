7da3e4736d739f83f7b58dd1b8729295f2ca32d5
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-01-16 16:04:34 +0000

    ct-357 Add “Track a query” logo + "Sign out" link
    This work brings the application more inline with the existing
    designs. It will display who the user is signed in as and
    provide a sign out link for them to leave the service

--- spec/features/01_sign_in/sign_in_spec.rb
+++ spec/features/01_sign_in/sign_in_spec.rb
@@ -1,36 +1,39 @@
 require 'rails_helper'
 
 feature "Signing in" do
+  given(:page) { LoginPage.new }
 
   let(:staff) { create(:user) }
 
   scenario "Signing in with correct credentials" do
-    visit new_user_session_path
-    within("#new_user") do
-      fill_in 'Email', with: staff.email
-      fill_in 'Password', with: staff.password
-    end
-    click_button 'Sign in'
+    page.load
+
+    expect(page).to have_no_user_card
+
+    page.log_in(staff.email, staff.password )
+
+    expect(page).to have_user_card
+    expect(page.user_card.greetings).to have_content(staff.email)
+
+    expect(page.user_card).to have_link('Sign out', href: destroy_user_session_path)
+
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario "Signing in using invalid email" do
-    visit new_user_session_path
-    within("#new_user") do
-      fill_in 'Email', with: Faker::Internet.email
-      fill_in 'Password', with: staff.password
-    end
-    click_button 'Sign in'
-    expect(page).to have_content 'Invalid email or password'
+    page.load
+
+    page.log_in(Faker::Internet.email, staff.password )
+
+    expect(page.error_message).to have_content 'Invalid email or password'
   end
 
   scenario "Signing in using invalid password" do
-    visit new_user_session_path
-    within("#new_user") do
-      fill_in 'Email', with: staff.email
-      fill_in 'Password', with: Faker::Lorem.characters(8)
-    end
-    click_button 'Sign in'
-    expect(page).to have_content 'Invalid email or password'
+    page.load
+
+    page.log_in(staff.email, Faker::Lorem.characters(8) )
+
+    expect(page.error_message).to have_content 'Invalid email or password'
   end
+
 end
