13ac4de28a6514236cc7f4ae33da053a2a52d802
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-01-24 12:22:01 +0000

    Test Case number and Subject are on the page

--- spec/features/03_respond_to_drafter_assignment/respond_to_assignment_spec.rb
+++ spec/features/03_respond_to_drafter_assignment/respond_to_assignment_spec.rb
@@ -22,7 +22,8 @@ feature 'respond to drafter assignment' do
 
   scenario 'kilo accepts assignment' do
     visit edit_case_assignment_path kase, assignment
-    expect(page).to have_content kase.name
+    expect(page).to have_content kase.number
+    expect(page).to have_content kase.subject
     expect(page).to have_content 'A message about XYZ'
     expect(page).to have_content 'I would like to know about XYZ'
 
@@ -37,7 +38,8 @@ feature 'respond to drafter assignment' do
 
   scenario 'kilo rejects assignment' do
     visit edit_case_assignment_path kase, assignment
-    expect(page).to have_content kase.name
+    expect(page).to have_content kase.number
+    expect(page).to have_content kase.subject
     expect(page).to have_content 'A message about XYZ'
     expect(page).to have_content 'I would like to know about XYZ'
 
@@ -45,9 +47,30 @@ feature 'respond to drafter assignment' do
     expect(page).to have_selector('#assignment_reasons_for_rejection', visible: true)
     fill_in 'Reason for rejecting this case', with: 'I am not the correct KILO for this'
     click_button 'Confirm'
- 
+
     expect(current_path).to eq case_path kase
     expect(Assignment.exists?(assignment.id)).to be false
     expect(kase.reload.current_state).to eq 'unassigned'
+  end
+
+  scenario 'kilo rejects assignment but provides no reasons for rejection' do
+    visit edit_case_assignment_path kase, assignment
+    expect(page).to have_content kase.number
+    expect(page).to have_content kase.subject
+    expect(page).to have_content 'A message about XYZ'
+    expect(page).to have_content 'I would like to know about XYZ'
+
+    choose 'Reject'
+    expect(page).
+      to have_selector('#assignment_reasons_for_rejection', visible: true)
+    click_button 'Confirm'
+
+    expect(page.find('#assignment_state_rejected')).to be_checked
+    expect(page).
+      to have_selector('#assignment_reasons_for_rejection', visible: true)
+    expect(kase.reload.current_state).to eq 'awaiting_responder'
+
+    expect(page).
+      to have_content("Reason for rejecting this case can't be blank")
   end
 end
