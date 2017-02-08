f625b164bbe777900611ce5f4d9553b8486d10da
Misha Gorodnitzky <misaka@pobox.com>
2017-02-08 20:34:07 +0000

    CT-248 add action to upload attachments to case detail view

--- spec/features/04_respond_to_correspondence/upload_response_spec.rb
+++ spec/features/04_respond_to_correspondence/upload_response_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 feature 'Upload response' do
-  given(:page) { CaseUploadPage.new }
+  given(:page)    { CaseUploadPage.new }
   given(:drafter) { create(:drafter) }
-  given(:kase) do
-    create(:accepted_case, assignee: drafter)
-  end
+  given(:kase)    { create(:accepted_case, assignee: drafter) }
   given(:attachment_1) do
     create(:correspondence_response, case: kase)
   end
@@ -13,15 +11,42 @@ feature 'Upload response' do
   background do
     create(:category, :foi)
     attachment_1
-    login_as drafter
   end
 
-  scenario 'viewing uploaded files' do
-    visit new_response_upload_case_path kase
+  context 'as the assigned drafter' do
+    background do
+      login_as drafter
+    end
 
-    attachment_filename = File.basename(
-      URI.parse(attachment_1.url).path
-    )
-    expect(page.existing_files).to have_content(attachment_filename)
+    scenario 'clicking link on case detail page goes to upload page' do
+      visit case_path(kase)
+
+      click_link 'Upload files'
+
+      expect(current_path).to eq new_response_upload_case_path(kase)
+    end
+
+    scenario 'viewing uploaded files' do
+      visit new_response_upload_case_path kase
+
+      attachment_filename = File.basename(
+        URI.parse(attachment_1.url).path
+      )
+      expect(page.existing_files).to have_content(attachment_filename)
+    end
+  end
+
+  context "as a drafter that isn't assigned to the case" do
+    given(:unassigned_drafter) { create(:drafter) }
+
+    background do
+      login_as unassigned_drafter
+    end
+
+    scenario "link to case upload page isn't visible on detail page" do
+      visit case_path(kase)
+
+      expect(page).not_to have_link('Upload file')
+    end
   end
 end
