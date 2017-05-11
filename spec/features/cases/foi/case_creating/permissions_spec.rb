0a3b0e3de597c1a67a86aed823ad8281b29eee1a
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-05-11 11:23:43 +0100

    CT-693 Rename "New case" button to "Create" case
    I did some refactoring on the feature specs, I removed the permissions
    spec for the responders as this is covered by a test in the view
    specs for index_html_slim and the cases_controller also tests the
    user is redirected.

--- spec/features/cases/foi/case_creating/permissions_spec.rb
+++ spec/features/cases/foi/case_creating/permissions_spec.rb
@@ -2,27 +2,13 @@ require 'rails_helper'
 
 feature 'Only assigners can create cases' do
 
-  let(:responder) { create(:responder) }
   let(:manager)   { create(:manager)   }
 
   scenario 'As an assigner I can navigate to the New Case form' do
     login_as manager
-    visit cases_path
-    expect(page).
-      to have_link('New case', href: Rails.root.join(new_case_path))
-    click_link "New case"
-    expect(page).to have_content 'Create new case'
-    expect(page).to have_button('Next - Assign case')
-  end
-
-  scenario 'As a responder I cannot navigate to the New Case form' do
-    login_as responder
-    visit cases_path
-    expect(page).
-      not_to have_link('New case', href: Rails.root.join(new_case_path))
-    visit new_case_path
-    expect(page).
-      to have_content "You are not authorised to create new cases."
+    cases_page.load
+    cases_page.new_case_button.click
+    expect(cases_new_page).to be_displayed
   end
 
 end
