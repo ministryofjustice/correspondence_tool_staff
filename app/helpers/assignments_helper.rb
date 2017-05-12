d77e39415b216b72f0adc939202d1506381504b1
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-05-12 14:07:02 +0100

    CT-687 Apply heading/sub headings to assignments
    New Assignments page had a specific use case where it
    had to determine whether the assignment was for a new case
    or a assignment on an existing case that had previously been
    rejected.
    
    As part of this work it meant that we did not have to worry
    about the copy of the button in either cases so I removed it
    of the assignments helper. We still needed to know whether it
    was new/existing case for the sub heading.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,17 +1,9 @@
 module AssignmentsHelper
-  def page_heading(creating_case)
+  def sub_heading(creating_case)
     if creating_case
-      t('assignments.new.new_assignment_while_creating')
-    else
       t('assignments.new.new_assignment')
-    end
-  end
-
-  def assign_button_text(creating_case)
-    if creating_case
-      t('button.create_and_assign')
     else
-      t('button.assign')
+      t('assignments.new.assignment')
     end
   end
 end
