ecb7d70573acd04d3d7e39dc48d57f383e1cacac
Misha Gorodnitzky <misaka@pobox.com>
2017-06-21 15:19:35 +0100

    CT-745 stylistic fix set_global_env before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, :set_user, except: [:ping, :healthcheck]
-  before_action :set_global_nav, if: -> { !current_user.nil? }
+  before_action :set_global_nav, if: -> { current_user.present? }
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
