9874c422ffa66ecf9724709a95700c976f60b735
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-06-27 16:20:41 +0100

    CT-803 Fix bug where not all attachments converted to PDF
    This was caused by the PDF job firing off before the transaction to save the
    CaseAttachment records was complete - most commonly seen when a lot of files
    are uploaded in one go.  This fix removes the error handling from the Job, and
    allows sidekiq to reschedule the job in it's own time.  This is recommended behaviour
    for ActiveJob using sidekiq.

--- app/jobs/pdf_maker_job.rb
+++ app/jobs/pdf_maker_job.rb
@@ -2,19 +2,8 @@ class PdfMakerJob < ApplicationJob
 
   queue_as :pdf_maker
 
-
-  def self.perform_with_delay(attachment_id, retry_count)
-    set(wait: Settings.s3_upload_retry_delay_minutes.minutes).perform_later(attachment_id, retry_count)
-  end
-
   def perform(attachment_id, retry_count = 0)
-    begin
-      attachment = CaseAttachment.find attachment_id
-      attachment.make_preview(retry_count)
-    rescue StandardError => err
-      message = "PdfMakerJob Error creating Preview for attachment #{attachment_id}"
-      Rails.logger.error "#{message}: #{err.class} - #{err.message}"
-      attachment.preview_key = nil unless attachment.nil?
-    end
+    attachment = CaseAttachment.find attachment_id
+    attachment.make_preview(retry_count)
   end
 end
--- spec/jobs/pdf_maker_job_spec.rb
+++ spec/jobs/pdf_maker_job_spec.rb
@@ -27,56 +27,8 @@ describe PdfMakerJob, type: :job do
       perform_enqueued_jobs { described_class.perform_later(123) }
     end
 
-    it 'handles no ActiveRecord::RecordNotFound error' do
-      allow(CaseAttachment).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
-      errmsg = 'PdfMakerJob Error creating Preview for attachment 123: ActiveRecord::RecordNotFound - ActiveRecord::RecordNotFound'
-      expect(Rails.logger).to receive(:error).with(errmsg)
-
-      described_class.perform_now(123)
-    end
-
-    context 'excepition' do
-      it 'logs the error and sets the preview_key to nil' do
-        expect(@attachment).to receive(:make_preview).and_raise(RuntimeError, 'Some specific error')
-        expect(Rails.logger).to receive(:error).with("PdfMakerJob Error creating Preview for attachment 123: RuntimeError - Some specific error")
-        expect(@attachment).to receive(:preview_key=).with(nil)
-        described_class.perform_now(123)
-      end
-
-    end
   end
 
-
-  describe '.perform_with_delay' do
-
-    before(:each) do
-      described_class.perform_with_delay(8766, 2)
-    end
-
-    let(:jobs) { queue_adapter.enqueued_jobs }
-    let(:job) { jobs.first }
-
-    it 'queues a job of the right class' do
-      expect(job[:job]).to eq PdfMakerJob
-    end
-
-    it 'queues a job with the specified arguments' do
-      expect(job[:args]).to eq [8766, 2]
-    end
-
-    it 'queues the job on the correct queue' do
-      expect(job[:queue]).to eq 'correspondence_tool_staff_pdf_maker'
-    end
-
-    it 'queues it for execution between 4 and 5 minutes from now' do
-      execution_time = Time.at(job[:at])
-      expect(time_between(execution_time, 4.minutes.from_now, 5.minutes.from_now)).to be true
-    end
-
-    def time_between(time_to_test, lower_limit, upper_limit)
-      time_to_test > lower_limit && time_to_test < upper_limit
-    end
-  end
 
   after do
     clear_enqueued_jobs
