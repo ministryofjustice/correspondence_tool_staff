dd5a6991f55ae8d23d8ccde2a85d15df7241df46
Misha Gorodnitzky <misaka@pobox.com>
2017-07-19 19:24:06 +0100

    CT-811 make arguments clearer in CaseFlagForClearanceService

--- app/services/case_flag_for_clearance_service.rb
+++ app/services/case_flag_for_clearance_service.rb
@@ -13,9 +13,11 @@ class CaseFlagForClearanceService
     return @result unless validate_case_is_unflagged
 
     if @team.dacu_disclosure?
-      assign_approver(@user, @team, @team)
+      assign_approver target_user: @user, target_team: @team,
+                      acting_team: @team
     elsif @team.press_office? || @team.private_office?
-      assign_and_accept_approver(@user, @team, @team)
+      assign_and_accept_approver target_team: @team, target_user: @user,
+                                 acting_team: @team
       @dts.associated_teams(for_team: @team).each do |associated|
         associate_team(associated[:team], associated[:user])
       end
@@ -35,27 +37,32 @@ class CaseFlagForClearanceService
     end
   end
 
-  def assign_approver(user, managing_team, team)
-    @case.state_machine.flag_for_clearance! user,
-                                            managing_team,
-                                            team
-    @case.approving_teams << team
+  def assign_approver(target_user:, target_team:, acting_team:)
+    @case.state_machine.flag_for_clearance! target_user,
+                                            acting_team,
+                                            target_team
+    @case.approving_teams << target_team
   end
 
-  def assign_and_accept_approver(user, managing_team, team)
-    @case.state_machine.take_on_for_approval!(user, managing_team, team)
-    @case.approving_teams << team
+  def assign_and_accept_approver(target_user:, target_team:, acting_team:)
+    @case.state_machine.take_on_for_approval! target_user,
+                                              acting_team,
+                                              target_team
+    @case.approving_teams << target_team
     @case.reload
-    team_assignment = @case.approver_assignments.for_team(team).last
-    team_assignment.update!(state: 'accepted', user_id: user.id)
+    team_assignment = @case.approver_assignments.for_team(target_team).last
+    team_assignment.update!(state: 'accepted', user_id: target_user.id)
   end
 
   def associate_team(associate_team, associate_user)
     if @case.approver_assignments.where(team: associate_team).blank?
       if associate_user
-        assign_and_accept_approver(associate_user, @team, associate_team)
+        assign_and_accept_approver target_team: associate_team,
+                                   target_user: associate_user,
+                                   acting_team: @team
       else
-        assign_approver(@user, @team, associate_team)
+        assign_approver target_team: associate_team, target_user: @user,
+                        acting_team: @team
       end
     end
   end
