346903170833c480d1578b72306e60d17be34895
Misha Gorodnitzky <misaka@pobox.com>
2017-07-19 15:53:55 +0100

    CT-806 test case shows up on open cases page for private office test

--- spec/features/cases/foi/case_approvals/private_office_approvals_spec.rb
+++ spec/features/cases/foi/case_approvals/private_office_approvals_spec.rb
@@ -30,10 +30,19 @@ feature 'cases requiring clearance by press office' do
     expect(case_row.number).to have_text case_available_for_taking_on.number
     case_row.actions.take_on_case.click
     expect(case_row.actions.success_message).to have_text 'Case taken on'
+
     case_row.number.click
     expect(cases_show_page.case_history.entries.first)
       .to have_text('Preston Offman Take on for approval')
     expect(cases_show_page.case_history.entries[1])
       .to have_text("#{private_officer.full_name} Flag for clearance")
+
+    _case_not_for_private_office_open_cases = create :case_being_drafted,
+                                                     :flagged_accepted,
+                                                     :press_office
+    open_cases_page.load
+    expect(open_cases_page.case_list.first.number)
+      .to have_text case_available_for_taking_on.number
+    expect(open_cases_page.case_list.count).to eq 1
   end
 end
