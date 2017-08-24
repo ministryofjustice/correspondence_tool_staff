c48fb1b4b4c3c53875fc89c5bb3a48c2e04c91d6
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-08-24 11:56:35 +0100

    CT-995 Fix bug in upload and approve
    This mod fixes the bug which meant that cases which were flagged for approval
    by Press Office weren't being advance to pending press office approval state
    if Dacu Disclosure used the event upload and approve

--- spec/support/case_printer.rb
+++ spec/support/case_printer.rb
@@ -2,20 +2,26 @@ class CasePrinter
 
   def initialize(case_id)
     @case = Case.find case_id
+    @lines = []
   end
 
   def print
-    puts "Case id: #{@case.id}   from #{@case.name}    title: #{@case.message}"
-    puts "Assignments:"
+    generate_lines
+    @lines.each { |l| puts l }
+  end
+
+  def generate_lines
+    @lines <<  "Case id: #{@case.id}   from #{@case.name}    title: #{@case.message}"
+    @lines <<   "Assignments:"
     @case.assignments.each do |a|
-      puts sprintf("    id: %-5d  role: %-12s state: %-12s team: %-5d %s", a.id, a.role, a.state, a.team.id, a.team.name)
+      @lines <<   sprintf("    id: %-5d  role: %-12s state: %-12s team: %-5d %s", a.id, a.role, a.state, a.team.id, a.team.name)
       # puts "    id: #{a.id}  state #{a.state}  team: #{a.team.id}: #{a.team.name}  user: #{a.user_id}: #{a.user&.full_name} role: #{a.role}"
     end
     puts "Transitions:"
     @case.transitions.each do |t|
-      puts sprintf("    id: %-5d   event: %s", t.id, t.event)
-      puts sprintf("        acting team: %-5s %-20s acting user: %-5s %s", t.acting_team_id, t.acting_team&.name, t.acting_user_id, t.acting_user&.full_name)
-      puts sprintf("        target team: %-5s %-20s target user: %-5s %s", t.target_team_id, t.target_team&.name, t.target_user_id, t.target_user&.full_name)
+      @lines <<   sprintf("    id: %-5d   event: %s", t.id, t.event)
+      @lines <<   sprintf("        acting team: %-5s %-20s acting user: %-5s %s", t.acting_team_id, t.acting_team&.name, t.acting_user_id, t.acting_user&.full_name)
+      @lines <<   sprintf("        target team: %-5s %-20s target user: %-5s %s", t.target_team_id, t.target_team&.name, t.target_user_id, t.target_user&.full_name)
     end
 
   end
