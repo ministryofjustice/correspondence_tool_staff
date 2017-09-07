0ae61c4994215f80ccdd73d518ed650dfb59170c
Misha Gorodnitzky <misaka@pobox.com>
2017-09-07 14:50:30 +0100

    Fix convert team role property methods names

--- db/migrate/20170831091142_convert_team_role_property_to_attribute.rb
+++ db/migrate/20170831091142_convert_team_role_property_to_attribute.rb
@@ -2,19 +2,19 @@ class ConvertTeamRolePropertyToAttribute < ActiveRecord::Migration[5.0]
   def up
     teams = BusinessUnit.all
     teams.each do |team|
-      convert_property_to_attribute(team)
+      convert_role_to_attribute(team)
     end
   end
 
   def down
     teams = BusinessUnit.all
     teams.each do |team|
-      convert_role_to_attribute(team)
+      convert_role_to_property(team)
     end
   end
 
   private
-  def convert_property_to_attribute(team)
+  def convert_role_to_attribute(team)
     property = team.properties.where(key: 'role').first
     role = property&.value || 'responder'
     team.role = role
@@ -22,7 +22,7 @@ class ConvertTeamRolePropertyToAttribute < ActiveRecord::Migration[5.0]
     property.destroy
   end
 
-  def convert_role_to_attribute(team)
+  def convert_role_to_property(team)
     team.properties.where(key: 'role').map(&:destroy)
     team.properties << TeamProperty.new(key: 'role', value: team.role)
     team.save!
