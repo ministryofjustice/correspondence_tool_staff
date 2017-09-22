d63fe19e549211e135f4ad383ab5541877498582
Alistair Laing <aliuk2012@users.noreply.github.com>
2017-09-22 09:22:29 +0100

    CT-1035 Pluralize the copy on re-assign user page

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,7 +3,7 @@ class UserDecorator < Draper::Decorator
 
   def full_name_with_optional_load_html
     if self.teams.include?(BusinessUnit.dacu_disclosure)
-      "#{full_name} <strong>(#{cases.opened.count} open cases)</strong>".html_safe
+      "#{full_name} <strong>(#{ h.pluralize(cases.opened.count,'open case','open cases')})</strong>".html_safe
     else
       full_name
     end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe CaseDecorator, type: :model do
-  let(:disclosure)    { find_or_create(:team_dacu_disclosure) }
-  let(:responder)     { create(:responder).decorate }
-  let(:sds)           { disclosure.approvers.first.decorate }
-  let(:accepted_case) { create(:accepted_case, :flagged_accepted,
-                               approver: sds,
-                               approving_team: disclosure) }
+  let(:disclosure)           { find_or_create(:team_dacu_disclosure) }
+  let(:responder)            { create(:responder).decorate }
+  let(:sds)                  { disclosure.approvers.first.decorate }
+  let(:accepted_case)        { create(:accepted_case, :flagged_accepted,
+                                      approver: sds,
+                                      approving_team: disclosure) }
+  let(:another_accepted_case){ create(:accepted_case, :flagged_accepted,
+                                      approver: sds,
+                                      approving_team: disclosure) }
 
   describe :full_name_with_optional_load_html do
     it 'returns the users full name if they are not on disclosure team' do
@@ -14,10 +17,19 @@ describe CaseDecorator, type: :model do
         .to eq responder.full_name
     end
 
-    it 'returns full name with case load info if not disclosure' do
+    it 'returns full name with case load info if disclosure' do
       accepted_case
       expect(sds.full_name_with_optional_load_html)
-        .to eq "#{sds.full_name} <strong>(1 open cases)</strong>"
+        .to eq "#{sds.full_name} <strong>(1 open case)</strong>"
     end
+
+    it 'returns full name with case load info in plural form if disclosure' do
+      accepted_case
+      another_accepted_case
+      expect(sds.full_name_with_optional_load_html)
+        .to eq "#{sds.full_name} <strong>(2 open cases)</strong>"
+    end
+
+
   end
 end
