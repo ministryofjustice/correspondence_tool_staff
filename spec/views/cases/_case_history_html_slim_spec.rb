af0db8f4a7e50f093d245e0de57296270e857705
Alistair Laing <aliuk2012@users.noreply.github.com>
2017-10-05 12:14:05 +0100

    CT-927 Move case history section out to a partial
    As the case history section will now be presented on the case details
    page and the edit assignments page. It would make sense to break it
    out into a partial.
    
    This will change again when we move case history details into the
    case messages section

---
+++ spec/views/cases/_case_history_html_slim_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'cases/case_history.html.slim', type: :view do
+
+  it 'displays the section heading' do
+    first  = double CaseTransitionDecorator,
+                    action_date: (DateTime.now + 10.days).strftime('%d %b %Y %H:%M'),
+                    user_name: 'Tom Smith',
+                    user_team: 'Managing Team 1',
+                    event_and_detail: 'Created new case'
+
+    transitions = [first]
+
+    assign(:case_transitions, transitions)
+    render partial: 'cases/case_history.html.slim',
+              locals:{ case_details: first}
+
+    partial = case_history_section(rendered)
+
+    expect(partial.section_heading.text).to eq 'Case history'
+  end
+
+  it 'displays date, user, team and event details' do
+    first  = double CaseTransitionDecorator,
+                    action_date: (DateTime.now + 10.days).strftime('%d %b %Y<br>%H:%M'),
+                    user_name: 'Tom Smith',
+                    user_team: 'Managing Team 1',
+                    event_and_detail: 'Created new case'
+
+    transitions = [first]
+
+    assign(:case_transitions, transitions)
+    render partial: 'cases/case_history.html.slim',
+              locals:{ case_details: first}
+
+    partial = case_history_section(rendered)
+
+    expect(partial.rows.first.action_date.text).to eq first.action_date
+    expect(partial.rows.first.user.text).to eq first.user_name
+    expect(partial.rows.first.team.text).to eq first.user_team
+    expect(partial.rows.first.details.text).to eq first.event_and_detail
+
+  end
+end
