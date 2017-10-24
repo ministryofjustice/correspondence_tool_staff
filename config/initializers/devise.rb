ec4a7437571b36f9169acfbcdfd3d036201dd857
Alistair Laing <aliuk2012@users.noreply.github.com>
2017-10-24 09:08:12 +0100

    CT-1139 increase session out out

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -158,7 +158,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = Rails.env.development? ? 12.day : 30.minutes
+  config.timeout_in = Rails.env.development? ? 12.day : 60.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
