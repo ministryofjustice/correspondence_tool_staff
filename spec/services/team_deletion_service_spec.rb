eda49cc9aa8166d0076610195a2b3fd81dfdbcbe
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2017-11-03 14:28:00 +0000

    CT-1106 tests the team deletion service with active children

--- spec/services/team_deletion_service_spec.rb
+++ spec/services/team_deletion_service_spec.rb
@@ -3,24 +3,38 @@ require 'rails_helper'
 describe TeamDeletionService do
 
   describe '#call' do
-    let(:team)          { create :responding_team }
-    let(:responder)     { team.responders.first }
+    let(:dir)           { create :dacu_directorate }
     let(:service)       { TeamDeletionService.new(params) }
     let(:params) do
       {
-        id: team.id
+        id: dir.id
       }
     end
+    context 'child team is not active' do
+      let!(:bu) { create(:deactivated_team,
+                          directorate: dir) }
+      it 'updates the team name' do
+        service.call
+        expect(dir.reload.name).to include "DEACTIVATED", dir.name
+      end
 
-    it 'updates the team name' do
-      service.call
-      expect(team.reload.name).to include "DEACTIVATED", team.name
+      it 'updates the deleted_at column' do
+        service.call
+        expect(dir.reload.deleted_at).not_to be nil
+      end
     end
-    
-    it 'updates the deleted_at column' do
-      expect(team.reload.deleted_at).to be nil
-      service.call
-      expect(team.reload.deleted_at).not_to be nil
+
+    context 'child team is active' do
+      let!(:bu) { create(:team_dacu,
+                          directorate: dir) }
+      it 'does not change the name' do
+        service.call
+        expect(dir.reload.name).not_to include "DEACTIVATED"
+      end
+      it 'does not update the deleted_at column' do
+        service.call
+        expect(dir.reload.deleted_at).to be nil
+      end
     end
   end
 end
