68d52c78c0fb25558988a8e48083fe7b2baf7dbb
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2017-11-06 15:56:54 +0000

    CT-1106 fixes direct method, updates tests

--- spec/features/admin/deactivating_teams_spec.rb
+++ spec/features/admin/deactivating_teams_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 feature 'deactivating users' do
   given!(:manager)         { create :manager }
   given!(:dir)             { create :dacu_directorate }
+  given!(:active_dir)      { create :dacu_directorate, name: "directorate"}
+  let!(:business_unit)     { create :business_unit, directorate: active_dir }
+
 
   scenario 'manager deactivates a team with no active children' do
     login_as manager
@@ -10,6 +13,15 @@ feature 'deactivating users' do
     teams_show_page.load(id: dir.id)
     teams_show_page.deactivate_team_link.click
 
-    expect(teams_show_page.flash_notice.text).to eq 'This team has now been deactivated'
+    expect(teams_show_page.flash_notice.text).to eq I18n.t('teams.destroyed')
+  end
+
+  scenario 'manager deactivates a team with no active children' do
+    login_as manager
+
+    teams_show_page.load(id: active_dir.id)
+    teams_show_page.deactivate_team_link.click
+
+    expect(teams_show_page.flash_alert.text).to eq I18n.t('teams.error')
   end
 end
