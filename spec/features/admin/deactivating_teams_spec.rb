7522c88fa71d94da2ede976ec73cefdb6b63db83
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2017-11-10 11:33:34 +0000

    CT-1106 comments out team deactivation links
    As we do not have a method to deactivate business units the directorates and business groups cannot be deactivated we have therefore removed the link to deactivate business groups and directorates to avoid user confusion.
    
    Once we have the functionality to deactivate business units we can uncomment this code and use it again.

--- spec/features/admin/deactivating_teams_spec.rb
+++ spec/features/admin/deactivating_teams_spec.rb
@@ -1,27 +1,27 @@
-require 'rails_helper'
-
-feature 'deactivating users' do
-  given(:manager)         { create :manager }
-  given(:dir)             { create :dacu_directorate }
-  given(:active_dir)      { create :dacu_directorate, name: "directorate"}
-  let!(:business_unit)    { create :business_unit, directorate: active_dir }
-
-
-  scenario 'manager deactivates a team with no active children' do
-    login_as manager
-
-    teams_show_page.load(id: dir.id)
-    teams_show_page.deactivate_team_link.click
-
-    expect(teams_show_page.flash_notice.text).to eq I18n.t('teams.destroyed')
-  end
-
-  scenario 'manager deactivates a team with no active children' do
-    login_as manager
-
-    teams_show_page.load(id: active_dir.id)
-    teams_show_page.deactivate_team_link.click
-
-    expect(teams_show_page.flash_alert.text).to eq I18n.t('teams.error')
-  end
-end
+# require 'rails_helper'
+#
+# feature 'deactivating users' do
+#   given(:manager)         { create :manager }
+#   given(:dir)             { create :dacu_directorate }
+#   given(:active_dir)      { create :dacu_directorate, name: "directorate"}
+#   let!(:business_unit)    { create :business_unit, directorate: active_dir }
+#
+#
+#   scenario 'manager deactivates a team with no active children' do
+#     login_as manager
+#
+#     teams_show_page.load(id: dir.id)
+#     teams_show_page.deactivate_team_link.click
+#
+#     expect(teams_show_page.flash_notice.text).to eq I18n.t('teams.destroyed')
+#   end
+#
+#   scenario 'manager deactivates a team with no active children' do
+#     login_as manager
+#
+#     teams_show_page.load(id: active_dir.id)
+#     teams_show_page.deactivate_team_link.click
+#
+#     expect(teams_show_page.flash_alert.text).to eq I18n.t('teams.error')
+#   end
+# end
