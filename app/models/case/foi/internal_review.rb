25db8582d91bcb84dd1b40f6c96441bc5248c1c0
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2017-11-29 10:11:33 +0000

    CT-1289 moves time reviews and comp reviews into internal review

--- app/models/case/foi/internal_review.rb
+++ app/models/case/foi/internal_review.rb
@@ -1,42 +1,22 @@
-# == Schema Information
-#
-# Table name: cases
-#
-#  id                   :integer          not null, primary key
-#  name                 :string
-#  email                :string
-#  message              :text
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  category_id          :integer
-#  received_date        :date
-#  postal_address       :string
-#  subject              :string
-#  properties           :jsonb
-#  requester_type       :enum
-#  number               :string           not null
-#  date_responded       :date
-#  outcome_id           :integer
-#  refusal_reason_id    :integer
-#  current_state        :string
-#  last_transitioned_at :datetime
-#  delivery_method      :enum
-#  workflow             :string
-#  deleted?             :boolean          default(FALSE)
-#  info_held_status_id  :integer
-#  type                 :string           default("Case")
-#  appeal_outcome_id    :integer
-#
-
 class Case::FOI::InternalReview < Case
 
   belongs_to :appeal_outcome, class_name: CaseClosure::AppealOutcome
 
-  def appeal_outcome_name=(name)
-    self.appeal_outcome = CaseClosure::AppealOutcome.by_name(name)
+  def check_is_flagged
+    unless current_state.in?([nil, 'unassigned'])
+      errors.add(:base, 'Internal reviews must be flagged for clearance') unless flagged?
+    end
+  end
+
+  def is_internal_review?
+    true
   end
 
   def appeal_outcome_name
     appeal_outcome&.name
+  end
+
+  def appeal_outcome_name=(name)
+    self.appeal_outcome = CaseClosure::AppealOutcome.by_name(name)
   end
 end
