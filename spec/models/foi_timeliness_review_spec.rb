255cad03468c4466a4ef1063273984ca68b9cc0b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-11-29 14:44:17 +0000

    Fix typo

--- spec/models/foi_timeliness_review_spec.rb
+++ spec/models/foi_timeliness_review_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe FoiTimelinessReview, type: :model do
 
-  let(:time_review) { create :foi_tiap meliness_review}
+  let(:time_review) { create :foi_timeliness_review}
 
   describe 'has a factory' do
     it 'that produces a valid object by default' do
