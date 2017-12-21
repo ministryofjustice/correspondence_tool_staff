18caa94fb139684ddb49c67e306c8b36c3580066
Misha Gorodnitzky <misaka@pobox.com>
2017-12-21 17:31:40 +0000

    CT-1234 Re-generate reports when existing reports aren't current

--- spec/controllers/stats_controller/download_spec.rb
+++ spec/controllers/stats_controller/download_spec.rb
@@ -28,11 +28,58 @@ RSpec.describe StatsController, type: :controller do
     end
 
     context 'there is no already-generated report' do
-      let!(:report)     { build :r003_report }
+      let(:report_type) { find_or_create :r003_report_type }
+
+      it 'generates a new report' do
+        existing_count = Report.count
+        get :download, params: { id: report_type.id }
+        expect(Report.count).to eq existing_count + 1
+        new_report = Report.last
+        expect(new_report.report_type).to eq report_type
+      end
+
+      it 'sends the generated report as an attachment' do
+        get :download, params: { id: report_type.id }
+        new_report = Report.last
+        expect(response.body).to eq new_report.report_data
+        expect(response.headers['Content-Disposition'])
+          .to eq 'attachment; filename="r003_business_unit_performance_report.csv"'
+      end
+    end
+
+    context 'there is an old already-generated report' do
       let(:report_type) { find_or_create :r003_report_type }
 
       before do
-        allow(Report).to receive(:new).and_return(report)
+        create :r003_report, created_at: 1.days.ago
+      end
+
+      it 'generates a new report' do
+        existing_report = Report.where(report_type_id: report_type.id).last
+        get :download, params: { id: report_type.id }
+        new_report = Report.last
+        expect(new_report.report_type).to eq report_type
+        expect(new_report.id).not_to eq existing_report.id
+        expect(new_report.created_at).to be > existing_report.created_at
+      end
+
+      it 'sends the generated report as an attachment' do
+        get :download, params: { id: report_type.id }
+        new_report = Report.last
+        expect(response.body).to eq new_report.report_data
+        expect(response.headers['Content-Disposition'])
+          .to eq 'attachment; filename="r003_business_unit_performance_report.csv"'
+      end
+    end
+
+    context 'there is no scheduled job for the given report' do
+      let(:report_type) { find_or_create :r003_report_type }
+
+      before do
+        allow(YAML).to receive(:load_file).and_return <<~EOYML
+          ---
+          :schedule:
+          EOYML
       end
 
       it 'generates a new report' do
@@ -41,13 +88,12 @@ RSpec.describe StatsController, type: :controller do
         expect(Report.count).to eq existing_count + 1
         new_report = Report.last
         expect(new_report.report_type).to eq report_type
-        expect(Report).to have_received(:new)
-                            .with({report_type_id: report_type.id})
       end
 
       it 'sends the generated report as an attachment' do
         get :download, params: { id: report_type.id }
-        expect(response.body).to eq report.report_data
+        new_report = Report.last
+        expect(response.body).to eq new_report.report_data
         expect(response.headers['Content-Disposition'])
           .to eq 'attachment; filename="r003_business_unit_performance_report.csv"'
       end
