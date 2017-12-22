98b842fbeb7eeaa2c734a9b3ec65daaf93dc37c4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-12-22 16:20:56 +0000

    CT-1423 Add validations for non-blank SARS fields

--- spec/models/case/sar_spec.rb
+++ spec/models/case/sar_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe Case::SAR do
 
-  context 'validates that SAR-specific fields are blank' do
+  context 'validates that SAR-specific fields are not blank' do
     it 'is not valid' do
 
-      kase = build :case, subject_full_name: 'Stephen Richards', subject_type: 'Offender', third_party: false
+      kase = build :sar_case, subject_full_name: nil, subject_type: nil, third_party: nil
+      ap kase
+
       expect(kase).not_to be_valid
-      expect(kase.errors[:subject_full_name]).to eq(['must be blank'])
-      expect(kase.errors[:subject_type]).to eq(['must be blank'])
-      expect(kase.errors[:third_party]).to eq(['must be blank'])
+      expect(kase.errors[:subject_full_name]).to eq(["can't be blank"])
+      expect(kase.errors[:subject_type]).to eq(["can't be blank"])
+      expect(kase.errors[:third_party]).to eq(["can't be blank"])
     end
-
   end
-
 
 end
