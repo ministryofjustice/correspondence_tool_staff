ae6f6009aa6182e9eacb09fe12eb2ec0a04cc389
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-12-29 11:09:40 +0000

    CT-1423 Remove unused parameters passed to Capybara warnings

--- spec/views/cases/_linked_cases_html_slim_spec.rb
+++ spec/views/cases/_linked_cases_html_slim_spec.rb
@@ -46,7 +46,7 @@ describe 'cases/linked_cases.html.slim', type: :view do
 
       main_case.linked_cases.each_with_index do | linked_case, index|
         row = partial.linked_records[index]
-        expect(row.link).to have_link("Link to case #{ linked_case.number}", href: case_path(linked_case.id))
+        expect(row.link).to have_link("Link to case #{linked_case.number}", href: case_path(linked_case.id))
         expect(row.case_type.text).to eq 'FOI '
         expect(row.request.text)
             .to eq "#{ linked_case.subject } #{ linked_case.name }"
