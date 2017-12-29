59cf542199e982e13226c3c9871be440cd760d26
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-12-29 10:33:27 +0000

    CT-1423 Add validation for subject type

--- app/models/case/sar.rb
+++ app/models/case/sar.rb
@@ -1,5 +1,9 @@
 class Case::SAR < Case
 
-  validates :subject_full_name, :subject_type, :third_party, presence: true
+  VALID_SUBJECT_TYPES = %w{ offender staff member_of_the_public }
+
+  validates :subject_full_name, presence: true
+  validates :third_party, inclusion: {in: [ true, false ], message: "can't be blank" }
+  validates :subject_type, inclusion: { in: VALID_SUBJECT_TYPES, message: 'is not a valid subject type' }
 
 end
