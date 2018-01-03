0c1b6ced5de9442017cf6c1da0a4d5b82e81482e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-01-03 16:45:20 +0000

    CT-1459 Fix admin case creator to create FOI cases instead of Base

--- lib/cts/cases/create.rb
+++ lib/cts/cases/create.rb
@@ -76,7 +76,7 @@ module CTS::Cases
                       else
                         0.business_days.after(4.business_days.ago)
                       end
-      Case::Base.new(
+      Case::FOI.new(
         name:            name,
         email:           options.fetch(:email, Faker::Internet.email(name)),
         category:        foi,
