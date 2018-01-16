31b77ca8c8dd5844f561fc7f0e074d3ef92ef8f6
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-01-16 12:03:14 +0000

    CT-1499 Add extended timeout in end to end test

--- spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
+++ spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
@@ -1,7 +1,7 @@
 def take_on_case_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.take_on_case.click
-  row.actions.wait_until_take_on_case_invisible
+  row.actions.wait_until_take_on_case_invisible(5)
   expect(row.actions.success_message.text)
     .to eq "Case taken on Undo taking on of case #{kase.number}"
 end
