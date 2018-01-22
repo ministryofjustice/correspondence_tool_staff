0467d8a67ace72afc79fbc5a3e78b28e04bf3741
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-01-22 17:07:52 +0000

    CT-1179 overrides password controller methods

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,14 +1,26 @@
 class PasswordsController < Devise::PasswordsController
+
+  include ApplicationHelper
+
+  def create
+    super
+    DeviseMailer.user_does_not_exist(@user.email).deliver_later
+  end
+
+  def new
+    super
+  end
+
+  def update
+    super
+  end
+
+  def edit
+    super
+  end
+
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
     new_user_session_path
   end
-
-  # def new
-  #   super
-  # end
-
-  # def create
-  #   super
-  # end
 end
