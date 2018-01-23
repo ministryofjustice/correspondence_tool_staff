f55f6da23a6842bdc22ffdd91023d95c1a7a58fa
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-01-23 09:32:40 +0000

    changes required for service to work with updated assets

--- spec/javascripts/modules/ShowHideContentSpec.js
+++
@@ -1,8 +0,0 @@
-describe('Modules.ShowHideContent.js', function() {
-  it('should exist with expected constructures', function() {
-    expect(moj.Modules.ShowHideContent.init).toBeDefined();
-    expect(moj.Modules.ShowHideContent.cacheEls).toBeDefined();
-    expect(moj.Modules.ShowHideContent.bindEvents).toBeDefined();
-    expect(moj.Modules.ShowHideContent.elementSelector).toBeDefined();
-  });
-});
