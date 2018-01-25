d1fb90773925ee3f65b3ed25d8ff74f105ea0193
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-01-25 10:23:00 +0000

    CT-1525 Add Kilo Map report

--- db/seeders/report_type_seeder.rb
+++ db/seeders/report_type_seeder.rb
@@ -7,6 +7,7 @@ class ReportTypeSeeder
     ReportType.find_or_create_by!(abbr:'R003', full_name: 'Business unit report', class_name: 'Stats::R003BusinessUnitPerformanceReport' , custom_report: true, seq_id: 200)
     ReportType.find_or_create_by!(abbr:'R004', full_name: 'Cabinet Office report', class_name: 'Stats::R004CabinetOfficeReport', custom_report: false, seq_id: 400)
     ReportType.find_or_create_by!(abbr:'R005', full_name: 'Monthly report', class_name: 'Stats::R005MonthlyPerformanceReport',custom_report: true, seq_id: 300)
+    ReportType.find_or_create_by!(abbr:'R006', full_name: 'KILO map', class_name: 'Stats::R006KiloMap', custom_report: false, seq_id: 9999)
   end
 end
 
