487590bf607a20e01d907b74302e3dcdd6076c32
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-02-07 15:11:37 +0000

    CT-1522 extend capybara timeout

--- spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
+++ spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
@@ -1,7 +1,8 @@
 def take_on_case_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.take_on_case.click
-  row.actions.wait_until_take_on_case_invisible(5)
+  sleep 1
+  row.actions.wait_until_take_on_case_invisible(10)
   expect(row.actions.success_message.text)
     .to eq "Case taken on Undo taking on of case #{kase.number}"
 end
@@ -9,6 +10,6 @@ end
 def undo_taking_case_on_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.undo_assign_link.click
-  row.actions.wait_until_take_on_case_visible
-  row.actions.wait_until_success_message_invisible
+  row.actions.wait_until_take_on_case_visible(10)
+  row.actions.wait_until_success_message_invisible(10)
 end
