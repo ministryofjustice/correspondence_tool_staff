96a910b251d0f01a048aea7278d6b3831df9b0f3
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-02-09 15:42:18 +0000

    CT-1510 fixes show polcies

--- app/policies/case/sar_policy.rb
+++ app/policies/case/sar_policy.rb
@@ -14,18 +14,12 @@ class Case::SARPolicy < Case::BasePolicy
       check(:responding_team_is_linked_to_case)
   end
 
-  check :responding_team_is_linked_to_case do
-    self.case.linked_cases.detect do |kase|
-      kase.responding_team.in? user.responding_teams
-    end
-  end
-
-
   def new_case_link?
     clear_failed_checks
     check_can_trigger_event(:link_a_case) &&
       check_user_is_a_manager_for_case
-    end
+  end
+
   def can_close_case?
     clear_failed_checks
     check_user_is_a_responder_for_case && check_case_is_in_attachable_state
@@ -33,5 +27,15 @@ class Case::SARPolicy < Case::BasePolicy
 
   def can_add_attachment_to_flagged_and_unflagged_cases?
     false
+  end
+
+  check :responding_team_is_linked_to_case do
+    self.case.linked_cases.detect do |kase|
+      kase.responding_team.in? user.responding_teams
+    end
+  end
+
+  check :user_is_a_responder_for_case do
+    user.responding_teams.include?(self.case.responding_team)
   end
 end
