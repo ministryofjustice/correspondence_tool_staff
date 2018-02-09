e929c1913f08a6da3337cbd2d5a02d04391c3686
Misha Gorodnitzky <misaka@pobox.com>
2018-02-09 15:41:31 +0000

    CT-1553 remove GQs from correspondence type seeding

--- db/seeders/correspondence_type_seeder.rb
+++ db/seeders/correspondence_type_seeder.rb
@@ -8,12 +8,6 @@ class CorrespondenceTypeSeeder
                                           internal_time_limit: 10,
                                           external_time_limit: 20,
                                           deadline_calculator_class: 'BusinessDays'
-    CorrespondenceType.find_or_create_by! name: 'General enquiry',
-                                          abbreviation: 'GQ',
-                                          escalation_time_limit: 0,
-                                          internal_time_limit: 10,
-                                          external_time_limit: 15,
-                                          deadline_calculator_class: 'BusinessDays'
     CorrespondenceType.find_or_create_by! name: 'Subject Access Request',
                                           abbreviation: 'SAR',
                                           escalation_time_limit: 0,
