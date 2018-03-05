e6c325e4b74be7d118de92dc43dd8560979436db
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-03-05 15:03:14 +0000

    CT-983 Spec for DeviseMailer#unlock_instructions

--- spec/mailers/devise_mailer_spec.rb
+++ spec/mailers/devise_mailer_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe DeviseMailer, type: :mailer do
       expect(mail.to).to include user.email
     end
   end
+
+  describe 'unlock_instructions' do
+    let(:user) { create :user, full_name: 'Someone' }
+    let(:mail) { described_class.unlock_instructions(user, 'nEAanath7ath7at8aWF') }
+
+    it 'sets the template' do
+      expect(mail.govuk_notify_template)
+          .to eq '9910545b-1b46-41d1-a5e3-f8b31ff450c0'
+    end
+
+    it 'personalises the email' do
+      expect(mail.govuk_notify_personalisation)
+          .to eq({
+                     email_subject: 'Your CMS user account has been locked',
+                     user_full_name: 'Someone',
+                     user_unlock_url:
+                         'http://localhost:3000/users/unlock?unlock_token=nEAanath7ath7at8aWF'
+                 })
+    end
+
+    it 'sets the To address of the email' do
+      expect(mail.to).to include user.email
+    end
+  end
 end
