4daee446229bbacf45ffacae757ab550b16096ea
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-03-08 12:20:41 +0000

    CT-1568 Fixed extend_for_pit and specs

---
+++ app/policies/predicates.rb
@@ -0,0 +1,27 @@
+class Predicates
+  def initialize(user:, kase:)
+    @user = user
+    @kase = kase
+  end
+
+  def responder_is_member_of_assigned_team?
+    if @kase.responding_team
+      @kase.responding_team.users.include?(@user)
+    else
+      false
+    end
+  end
+
+  def notify_responder_message_received
+    NotifyResponderService.new(@kase, 'Message received').call
+  end
+  
+  def user_is_assigned_disclosure_specialist?
+    @kase.assignments.with_teams(BusinessUnit.dacu_disclosure).for_user(@user).present?
+  end
+
+
+end
+
+
+
