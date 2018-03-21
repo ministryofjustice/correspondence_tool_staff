bba8bc8775ebc366e4a9f78fa18d2696bbb99085
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-03-21 15:45:33 +0000

    CT-1612 update migration

--- db/migrate/20180321094200_populate_full_approval_workflow.rb
+++ db/migrate/20180321094200_populate_full_approval_workflow.rb
@@ -1,7 +1,7 @@
 class PopulateFullApprovalWorkflow < ActiveRecord::Migration[5.0]
   def up
     Case::Base.find_each do |kase|
-      if kase.flagged_for_disclosure_specialist_clearance?
+      if kase.flagged_for_press_office_clearance?
         kase.update(workflow: 'full_approval')
       end
     end
