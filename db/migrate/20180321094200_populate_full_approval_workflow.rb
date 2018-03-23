4d2a9f5f6bc4d2659e8fb13003bb8eb8b2afc916
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-03-23 15:10:55 +0000

    CT-1612 Make migrations usable even if class methods are removed

--- db/migrate/20180321094200_populate_full_approval_workflow.rb
+++ db/migrate/20180321094200_populate_full_approval_workflow.rb
@@ -1,6 +1,28 @@
 class PopulateFullApprovalWorkflow < ActiveRecord::Migration[5.0]
+
+  class MigrateableCase < ActiveRecord::Base
+
+    self.table_name = :cases
+
+    has_many :approver_assignments,
+             -> { approving },
+             class_name: 'Assignment',
+             foreign_key: :case_id
+
+    has_many :approving_teams,
+             -> { where("state != 'rejected'") },
+             class_name: BusinessUnit,
+             through: :approver_assignments,
+             source: :team
+
+    def flagged_for_press_office_clearance?
+      approving_teams.include?(BusinessUnit.press_office)
+    end
+
+  end
+
   def up
-    Case::Base.find_each do |kase|
+    MigrateableCase.find_each do |kase|
       if kase.flagged_for_press_office_clearance?
         kase.update(workflow: 'full_approval')
       end
