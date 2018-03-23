23eb0460cbd1ea4de1b820efa415c65070a4d5af
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-03-23 15:30:26 +0000

    CT-1612 remove duplicate clone

--- app/state_machines/configurable_state_machine/machine.rb
+++ app/state_machines/configurable_state_machine/machine.rb
@@ -194,7 +194,7 @@ module ConfigurableStateMachine
           to_state = find_destination_state(event_config: event_config, user: user)
           to_workflow = find_destination_workflow(event_config: event_config, user: user)
           CaseTransition.unset_most_recent(@kase)
-          write_transition(event: event, to_state: to_state, to_workflow: to_workflow, params: params.clone)
+          write_transition(event: event, to_state: to_state, to_workflow: to_workflow, params: params)
           @kase.update!(current_state: to_state, workflow: to_workflow)
           execute_after_transition_method(event_config: event_config, user: user)
         end
