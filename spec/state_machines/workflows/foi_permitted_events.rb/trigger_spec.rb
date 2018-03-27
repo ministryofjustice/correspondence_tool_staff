1965ef32aa459225a07e48048a2da3a851679d82
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-03-27 12:19:50 +0100

    CT-1642 remove_linked_case event tests

--- spec/state_machines/workflows/foi_permitted_events.rb/trigger_spec.rb
+++ spec/state_machines/workflows/foi_permitted_events.rb/trigger_spec.rb
@@ -158,7 +158,7 @@ describe Case::FOI::StandardStateMachine do
             k = create :case, :flagged, :dacu_disclosure
 
             expect(k.current_state).to eq 'unassigned'
-            expect(k.state_machine.permitted_events(responder.id)).to eq [:link_a_case]
+            expect(k.state_machine.permitted_events(responder.id)).to eq [:link_a_case, :remove_linked_case]
           end
         end
 
