4175c392c1f12eb40ad5fec21262b2d6aaf4cf37
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-04-09 09:19:08 +0100

    CT-1615 remove old code

--- spec/state_machines/cases/foi_state_machine_spec.rb
+++ spec/state_machines/cases/foi_state_machine_spec.rb
@@ -904,33 +904,6 @@ RSpec.describe Case::FOI::StandardStateMachine, type: :model do
     end
   end
 
-  describe '#notify_kilo_case_is_ready_to_send' do
-    let(:approved_case)   { create :approved_case, :flagged }
-    let(:kase)            { create :case }
-    let!(:service) do
-      double(NotifyResponderService, call: true).tap do |svc|
-        allow(NotifyResponderService).to receive(:new).and_return(svc)
-      end
-    end
-
-    # context 'case state is awaiting_dispatch' do
-    #   it 'calls the service' do
-    #     approved_case.state_machine.notify_kilo_case_is_ready_to_send(approved_case)
-    #     expect(NotifyResponderService)
-    #       .to have_received(:new).with(approved_case)
-    #     expect(service).to have_received(:call)
-    #   end
-    # end
-
-    context 'case state is not awaiting_dispatch' do
-      # it 'does not calls the service' do
-      #   new_case.state_machine.notify_kilo_case_is_ready_to_send(new_case)
-      #   expect(NotifyResponderService)
-      #     .not_to have_received(:new).with(new_case)
-      #   expect(service).not_to have_received(:call)
-      # end
-    end
-  end
 
   describe '#request_further_clearance' do
     let(:accepted_case) { create :accepted_case }
