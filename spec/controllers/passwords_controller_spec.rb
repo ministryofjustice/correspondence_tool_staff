b7eb3091a543bb01fe17f54e993e65ad20e3928f
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-04-12 09:56:37 +0100

    Ct-1669 password controller test for passwords

--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -3,19 +3,32 @@ require 'devise'
 
 RSpec.describe PasswordsController, type: :controller do
   describe 'POST create' do
-    let(:user) { create :user}
+    let(:mailer) { double ActionNotificationsMailer, account_not_active}
+    let!(:user)  { create :user }
     let!(:deactivated_user) { create :deactivated_user }
     let(:params){
       {
-      email: user.email
+        user:{
+          email: user.email
+        }
       }
     }
+
+    before do
+      allow(ActionNotificationsMailer).to receive_message_chain(:account_not_active,
+                                                     :deliver_later)
+    end
 
     context 'user is registered' do
       it 'redirect and shows message' do
         @request.env["devise.mapping"] = Devise.mappings[:user]
 
         post :create, params: params
+
+        expect(ActionNotificationsMailer).to have_received(:account_not_active)
+                                      .with user.email
+        expect(mailer).to receive(:deliver_later).exactly(1)
+
         expect(flash[:notice]).to eq "If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes."
       end
     end
@@ -26,6 +39,10 @@ RSpec.describe PasswordsController, type: :controller do
         params[:email] = deactivated_user.email
 
         post :create, params: params
+        expect(ActionNotificationsMailer).to have_received(:account_not_active)
+                                      .with deactivated_user.email
+        expect(mailer).to receive(:deliver_later).exactly(1)
+
         expect(flash[:notice]).to eq "If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes."
       end
     end
@@ -36,6 +53,10 @@ RSpec.describe PasswordsController, type: :controller do
         params[:email] = Faker::Internet.email
 
         post :create, params: params
+        expect(ActionNotificationsMailer).to have_received(:account_not_active)
+                                      .with params[:email]
+        expect(mailer).to receive(:deliver_later).exactly(1)
+
         expect(flash[:notice]).to eq "If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes."
       end
     end
