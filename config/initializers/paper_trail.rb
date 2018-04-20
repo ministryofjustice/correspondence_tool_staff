f6846d82bcda128553c033ee77306dad8220e56d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-04-20 16:45:43 +0100

    CT-1550 remove commented-out lines

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,6 +1,2 @@
 PaperTrail.config.track_associations = false
-# PaperTrail.serializer = PaperTrail::Serializers::JSON
 PaperTrail.serializer = CtsPapertrailSerializer
-
-# require File.join(Rails.root, 'lib', 'cts_papertrail_serializer')
-# PaperTrail.serializer = Papertrail::Serializers::CTSPapertrailSerializer
