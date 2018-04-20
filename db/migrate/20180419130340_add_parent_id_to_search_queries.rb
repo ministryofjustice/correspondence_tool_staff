d2eeffba279ba8067681b34140e53780f6a24b8a
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-04-20 14:10:49 +0100

    CT-1672 Add create_enum back into migration

--- db/migrate/20180419130340_add_parent_id_to_search_queries.rb
+++ db/migrate/20180419130340_add_parent_id_to_search_queries.rb
@@ -1,4 +1,7 @@
 class AddParentIdToSearchQueries < ActiveRecord::Migration[5.0]
+
+  create_enum :search_query_type, 'search', 'filter'
+
   def change
     add_column :search_queries, :parent_id, :integer
     add_column :search_queries, :query_type, :search_query_type, null: false, default: :search
