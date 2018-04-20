8945ea966835009d7801e8ae8219d5ec6a85cce3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-04-20 14:07:55 +0100

    CT-1550 Fix bug in papertrail versioning
    Full notes in readme, but there were two issues with regenerating
    an old version from a papertrail versions record.
    
    One was thet JSONB fields were not properly recreated - this has been
    fixed by writing a custom serializer
    
    The socond was that gov_uk_date fields were not being recreated properly.
    The readme gives a work-around tothe this issue.

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,1 +1,6 @@
 PaperTrail.config.track_associations = false
+# PaperTrail.serializer = PaperTrail::Serializers::JSON
+PaperTrail.serializer = CtsPapertrailSerializer
+
+# require File.join(Rails.root, 'lib', 'cts_papertrail_serializer')
+# PaperTrail.serializer = Papertrail::Serializers::CTSPapertrailSerializer
