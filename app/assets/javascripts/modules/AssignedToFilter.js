857055ced3443908252464e7958eb1da4e66b53b
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-05-03 10:41:50 +0100

    CT-1671 refactors the javascript used for assigned to filters

--- app/assets/javascripts/modules/AssignedToFilter.js
+++ app/assets/javascripts/modules/AssignedToFilter.js
@@ -2,37 +2,36 @@ moj.Modules.AssignedToFilter = {
 
     init: function () {
 
-        if ($('#search_query_business_unit_name_filter').length === 0 ){
-            return;
-        }
-        var module = this;
+      var module = this;
 
-        $('#search_query_business_unit_name_filter').on('keyup', function(){
-            if(this.value !== '') {
-                $('.js-all-business-units').show();
-                module.getMatchingBusinessUnits(this.value);
-            } else {
-                $('.js-all-business-units').hide();
-            }
-        }).trigger('keyup')
+      $('#search_query_business_unit_name_filter').on('keyup', function(){
+        if(this.value !== '') {
+          $('.js-all-business-units').show();
+          module.getMatchingBusinessUnits(this.value);
+        } else {
+          $('.js-all-business-units').hide();
+        }
+      }).trigger('keyup')
     },
 
 
     getMatchingBusinessUnits: function (searchFor){
-        $('.js-all-business-units').find('.multiple-choice')
-            .each(function( index, element ) {
-                var $elem = $(this);
-                var labelText = $elem.text().replace(/[^a-z0-9+]+/gi, '');
-                var cleanedTerm = searchFor.replace(/[^a-z0-9+]+/gi, '');
-                var check = new RegExp(cleanedTerm, 'gi')
+      $('.js-all-business-units').find('.multiple-choice')
+        .each(function( index, element ) {
+          var $elem = $(this);
+          // Remove any character that is not alphanumeric
+          var labelText = $elem.text().replace(/[^a-z0-9+]+/gi, '');
+          // Remove any character that is not alphanumeric
+          var cleanedTerm = searchFor.replace(/[^a-z0-9+]+/gi, '');
 
-                if (check.test(labelText)){
-                    $elem.show();
-                }else{
-                    $elem.hide();
-                    $elem.find(':checkbox').prop('checked', false);
-                }
-            })
+          // if checkbox label test contains any part of the search term
+          if (labelText.search(cleanedTerm, 'gi') > -1){
+              $elem.show();
+          }else{
+              $elem.hide();
+              $elem.find(':checkbox').prop('checked', false);
+          }
+        })
     }
 
 }
