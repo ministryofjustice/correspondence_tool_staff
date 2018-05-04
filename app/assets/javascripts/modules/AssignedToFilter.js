6618b3b0a7b6bb859865435141a56f3bfabec76c
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-05-04 14:44:33 +0100

    CT-1671 display message for no results

--- app/assets/javascripts/modules/AssignedToFilter.js
+++ app/assets/javascripts/modules/AssignedToFilter.js
@@ -9,31 +9,39 @@ moj.Modules.AssignedToFilter = {
           $('.js-all-business-units').show();
           module.getMatchingBusinessUnits(this.value);
         } else {
-          $('.js-all-business-units').hide();
+          $('.js-all-business-units, .js-no-results').hide();
+
         }
       }).trigger('keyup')
     },
 
 
-    getMatchingBusinessUnits: function (searchFor){
+    getMatchingBusinessUnits: function (searchFor) {
       $('.js-all-business-units').find('.multiple-choice')
-        .each(function( index, element ) {
-          var $elem = $(this);
-          // Remove any character that is not alphanumeric
-          var labelText = $elem.text().replace(/[^a-z0-9+]+/gi, '');
-          // Remove any character that is not alphanumeric
-          var cleanedTerm = searchFor.replace(/[^a-z0-9+]+/gi, '');
-          var check = new RegExp(cleanedTerm, 'gi');
+      .each(function (index, element) {
+        var $elem = $(this);
+        // Remove any character that is not alphanumeric
+        var labelText = $elem.text().replace(/[^a-z0-9+]+/gi, '');
+        // Remove any character that is not alphanumeric
+        var cleanedTerm = searchFor.replace(/[^a-z0-9+]+/gi, '');
+        var check = new RegExp(cleanedTerm, 'gi');
 
 
-          // if checkbox label test contains any part of the search term
-          if (check.test(labelText)){
-            $elem.show();
-          }else{
-            $elem.hide();
-            $elem.find(':checkbox').prop('checked', false);
-          }
-        })
+        // if checkbox label test contains any part of the search term
+        if (check.test(labelText)) {
+          $elem.show();
+        } else {
+          $elem.hide();
+          $elem.find(':checkbox').prop('checked', false);
+        }
+      });
+
+      if ($('.js-all-business-units').find('.multiple-choice').filter(':visible').length > 0){
+        $('.js-no-results').hide();
+      } else {
+        $('.js-no-results').show();
+        $('.js-all-business-units').hide();
+      }
     }
 
 }
