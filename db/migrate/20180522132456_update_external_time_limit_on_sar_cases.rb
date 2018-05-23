bd7bffd64708bd29b0b3ee204af912bb9956ea89
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-05-23 10:23:34 +0100

    CT-1515 removes calendar days from migration

--- db/migrate/20180522132456_update_external_time_limit_on_sar_cases.rb
+++ db/migrate/20180522132456_update_external_time_limit_on_sar_cases.rb
@@ -6,7 +6,6 @@ class UpdateExternalTimeLimitOnSarCases < ActiveRecord::Migration[5.0]
   end
   def down
     CorrespondenceType.find_by(abbreviation: 'SAR')
-                      &.update deadline_calculator_class: 'CalendarDays',
-                               external_time_limit: 40
+                      &.update external_time_limit: 40
   end
 end
