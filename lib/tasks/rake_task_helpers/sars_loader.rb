e210f0ed603f0be0fad71763b9ff448aedf44f34
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-05-23 09:08:40 +0100

    CT-1552 Update Business Units requirings SARS responding rights
    This introduces two new rake tasks:
    
     * rake kilo_map:load_sars[<filename>] Reads the CSV file <filename> and updates
       those business units marked as requiring SARS rights.  The file is currently
       in lib/assets
    
     * rake kilo_map:audit_corr_types Prints a list of business units and what
       correspondence types they are authorised to respond to
    
    After deployment to production, the following command must be run manually:
    
       rake kilo_map:load_sars[lib/assets/kilo_map.csv]

---
+++ lib/tasks/rake_task_helpers/sars_loader.rb
@@ -0,0 +1,50 @@
+require 'csv'
+
+# This class is used for loading a list of teams into the dev database from the R006_kilo_map CSV
+# file (useful for testing scripts to assign correpsondence types to certain business units only)
+#
+class SarsLoader
+
+  def initialize(filename)
+    @filename = filename
+    @foi = CorrespondenceType.find_by(abbreviation: 'FOI')
+    @sar = CorrespondenceType.find_by(abbreviation: 'SAR')
+  end
+
+  def run
+    CSV.foreach(@filename) do |row|
+      bg, bu, sars = row
+
+      next if bg == 'Business group'   # header row
+      if bg.present?
+        process_business_group(bg)
+      elsif bu.present? && sars.present?
+        process_business_unit(bu)
+      end
+    end
+  end
+
+
+  private
+
+  def process_business_group(bg)
+    @bg = BusinessGroup.find_by(name: bg)
+    if @bg.nil?
+      puts ">>>>>>>>>> ERROR <<<<<<<<<<<< Unable to find BG #{@bg.name}"
+    end
+  end
+
+
+  def process_business_unit(bu)
+    bus = @bg.business_units.where("teams.name LIKE ?", "%#{bu}%")
+    if bus.empty?
+      puts ">>>>>>>>>> ERROR <<<<<<<<<<<< Unable to find BU #{bu} under BG #{@bg.name}"
+    elsif bus.size > 1
+      puts ">>>>>>>>>> ERROR <<<<<<<<<<<< Mulitple business units found with name matchng %#{bu}% under BG #{@bg.name}"
+    else
+      bu = bus.first
+      bu.update(correspondence_type_ids: [ @foi.id, @sar.id ] )
+      bu.save!
+    end
+  end
+end
