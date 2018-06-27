b5c19cc7a80ed4c6526aed9eac41a89b1f45e70e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-06-27 09:01:29 +0100

    CT-1334 experimenting with sleeps to get tests working

--- db/structure.sql
+++ db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.6
--- Dumped by pg_dump version 9.5.6
+-- Dumped from database version 9.5.10
+-- Dumped by pg_dump version 9.5.10
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -1613,9 +1613,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180522132456'),
 ('20180524132031'),
 ('20180613141421'),
-('20180621093054'),
 ('20180621094208'),
-('20180622153909'),
-('20180625160408');
+('20180622153909');
 
 
--- spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
+++ spec/support/features/steps/cases/taking_on_and_escalating_case_steps.rb
@@ -1,7 +1,8 @@
 def take_on_case_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.take_on_case.click
-  row.actions.wait_until_take_on_case_invisible(60)
+  sleep 1
+  row.actions.wait_until_take_on_case_invisible(10)
   expect(row.actions).to have_undo_assign_link
   expect(row.actions.success_message)
     .to have_text "Case taken on Undo taking on of case #{kase.number}"
@@ -11,6 +12,7 @@ end
 def undo_taking_case_on_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.undo_assign_link.click
+  sleep 1
   row.actions.wait_until_undo_assign_link_invisible(10)
   expect(row.actions).to have_take_on_case
   expect(row.actions).to have_no_success_message
@@ -20,7 +22,8 @@ end
 def de_escalate_case_step(kase:)
   row = incoming_cases_page.row_for_case_number(kase.number)
   row.actions.de_escalate_link.click
-  row.actions.wait_until_de_escalate_link_invisible(20)
+  sleep 1
+  row.actions.wait_until_de_escalate_link_invisible(10)
   expect(row.actions).to have_undo_de_escalate_link
   expect(row.actions).to have_no_take_on_case
   expect(row.actions).to have_text 'Case de-escalated Undo'
