8dc4d19f219a0e0dd31376488514476eca83b7b7
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-06-28 11:05:55 +0100

    CT-1753 change flash message after creating a case

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -152,9 +152,11 @@ class CasesController < ApplicationController
     @case = service.case
     case service.result
     when :case_created
+      flash[:notice] = "#{@case.type_abbreviation} case created<br/> Case number: #{@case.number}".html_safe
       redirect_to case_path @case
     when :assign_responder
       flash[:creating_case] = true
+      flash[:notice] = "#{@case.type_abbreviation} case created<br/> Case number: #{@case.number}".html_safe
       redirect_to new_case_assignment_path @case
     else # including :error
       @case.type = @case.type.demodulize
