a973989371dc8ad17fdfa216c4d5192bca251c2d
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-07-16 11:32:09 +0100

    CT-1823 add test for upload and approve ico case:

--- spec/features/cases/ico/ico_approval_spec.rb
+++ spec/features/cases/ico/ico_approval_spec.rb
@@ -14,7 +14,7 @@ feature 'cases requiring clearance by disclosure specialist' do
   given(:responder)                   { responding_team.users.first }
 
 
-  scenario 'taking_on, undoing and de-escalating a case as a disclosure specialist', js: true do
+  scenario 'taking_on and undoing a case as a disclosure specialist', js: true do
     kase = create :accepted_ico_foi_case, :flagged,
                   approving_team: team_dacu_disclosure
 
@@ -64,6 +64,23 @@ feature 'cases requiring clearance by disclosure specialist' do
 
     cases_show_page.load(id: kase.id)
     expect(cases_show_page.case_status.details.copy.text).to eq 'Draft in progress'
+    expect(cases_show_page.case_status.details.who_its_with.text).to eq kase.responding_team.name
+  end
+
+  scenario 'upload a response and approve', js: true do
+    kase = create :pending_dacu_clearance_ico_foi_case, approver: disclosure_specialist
+
+    login_as disclosure_specialist
+    cases_show_page.load(id: kase.id)
+    cases_show_page.actions.upload_approve.click
+
+    expect(cases_new_response_upload_page).to be_displayed
+    expect_any_instance_of(CasesController).to receive(:upload_responses)
+    cases_new_response_upload_page.upload_response_button.click
+    upload_and_approve_response_as_dacu_disclosure_specialist(kase.reload, disclosure_specialist)
+
+    cases_show_page.load(id: kase.id)
+    expect(cases_show_page.case_status.details.copy.text).to eq 'Ready to send'
     expect(cases_show_page.case_status.details.who_its_with.text).to eq kase.responding_team.name
   end
 end
