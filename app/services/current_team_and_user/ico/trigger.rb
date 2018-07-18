8623acf1ac973727eb16c7c0cb2665884027dde0
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-07-18 10:02:06 +0100

    CT-1800 add new ready to send to ico state

--- app/services/current_team_and_user/ico/trigger.rb
+++ app/services/current_team_and_user/ico/trigger.rb
@@ -1,6 +1,10 @@
 module CurrentTeamAndUser
   module ICO
     class Trigger < ::CurrentTeamAndUser::Base
+      def awaiting_dispatch_for_ico
+        @team = @case.responding_team
+        @user = @case.responder
+      end
     end
   end
 end
