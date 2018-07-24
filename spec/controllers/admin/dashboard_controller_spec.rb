323ff71a0acba7dccddf3c7a75594499fe9319a3
Misha Gorodnitzky <misaka@pobox.com>
2018-07-24 20:24:12 +0100

    Move dashboards spec to admin/

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe DashboardController do
+describe Admin::DashboardController do
   let(:admin)         { create :admin }
   let!(:search_query) { create :search_query}
 
@@ -11,7 +11,7 @@ describe DashboardController do
       get :search_queries
     end
     it 'renders the index view' do
-      expect(request.path).to eq('/dashboard/search_queries')
+      expect(request.path).to eq('/admin/dashboard/search_queries')
     end
 
     it 'has search queries' do
