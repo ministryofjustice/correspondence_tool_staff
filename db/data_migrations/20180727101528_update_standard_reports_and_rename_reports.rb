0a80cc5cd7031deda24b2782cfd5afc4368105bd
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-07-27 12:35:12 +0100

    CT-1768 Removes non-standard reports and renames some reports

---
+++ db/data_migrations/20180727101528_update_standard_reports_and_rename_reports.rb
@@ -0,0 +1,32 @@
+class UpdateStandardReportsAndRenameReports < ActiveRecord::DataMigration
+  def up
+    attrs = {
+        'R002'  => {
+            full_name: 'Appeals report',
+            standard_report: false
+        },
+        'R003'  => {
+            standard_report: false,
+        },
+        'R006'  => {
+            standard_report: false
+        },
+        'R102'  => {
+            standard_report: false,
+            full_name: 'Appeals performance report'
+        },
+        'R103'  => {
+            standard_report: false,
+            full_name: 'Business unit report'
+        },
+        'R105'  => {
+            standard_report: true,
+        }
+    }
+
+    attrs.each do |report_abbreviation, new_values|
+      rt = ReportType.find_by!(abbr: report_abbreviation)
+      rt.update!(new_values)
+    end
+  end
+end
