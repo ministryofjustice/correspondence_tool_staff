9566565af8d38da1dde7d856e5a79bfccf923b7f
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-07-30 16:18:40 +0100

    fix broken db migration as result of adding default_report_types

--- db/migrate/20180126120726_change_kilo_map_name.rb
+++ db/migrate/20180126120726_change_kilo_map_name.rb
@@ -1,4 +1,7 @@
 class ChangeKiloMapName < ActiveRecord::Migration[5.0]
+  class ReportType < ActiveRecord::Base
+  end
+
   def up
     rt = ReportType.find_by_abbr!('R006')
     rt.full_name = "Business unit map"
