ec8bbe074fb155fd36eca0af34e86908c0fba1e3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-08-03 23:24:09 +0100

    CT-1784 back end case closure service revisited

--- app/services/case_closure_service.rb
+++ app/services/case_closure_service.rb
@@ -1,32 +1,22 @@
 class CaseClosureService
 
-  def initialize(kase, params)
+  attr_reader :result, :flash_message
+
+  def initialize(kase, user, params)
     @kase = kase
+    @user = user
     @params = params
+    @result = nil
   end
 
   def call
-    #
-    #   @case.prepare_for_close
-    #   close_params = process_closure_params(@case.type_abbreviation)
-    #   if @case.update(close_params)
-    #     @case.close(current_user)
-    #     set_permitted_events
-    #     flash[:notice] = t('notices.case_closed')
-    #     redirect_to case_path(@case)
-    #   else
-    #     set_permitted_events
-    #     render :close
-    #   end
-    # end
-
-    @case.prepare_for_close
-    if @case.update(@params)
-      @flash = I18n.t('notices.case_closed')
+    @kase.prepare_for_close
+    if @kase.update(@params)
+      @kase.close(@user)
+      @flash_message = I18n.t('notices.case_closed')
+      @result = :ok
     else
-    self
+      @result = :error
+    end
   end
-
-
-
 end
