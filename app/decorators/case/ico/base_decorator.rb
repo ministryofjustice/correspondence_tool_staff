1e0c860d5055128505f914c05b7ff1f29a01158c
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-08-06 13:01:04 +0100

    CT-1784 temporary add the fields needed for the page

--- app/decorators/case/ico/base_decorator.rb
+++ app/decorators/case/ico/base_decorator.rb
@@ -2,4 +2,9 @@ class Case::ICO::BaseDecorator < Case::BaseDecorator
 
   attr_accessor :original_case_number, :related_case_number
 
+  attr_accessor :ico_decision, :date_ico_decision_received
+  attr_accessor :date_ico_decision_received_dd
+  attr_accessor :date_ico_decision_received_mm
+  attr_accessor :date_ico_decision_received_yyyy
+
 end
