c9cea0e99e68c5f148940820432b03730dcea835
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-08-08 21:04:55 +0100

    fixes existing codeclimate issues raised for JS files

--- app/assets/javascripts/modules/ExemptionFilter.js
+++ app/assets/javascripts/modules/ExemptionFilter.js
@@ -1,25 +1,25 @@
 moj.Modules.ExemptionFilter = {
-    init: function() {
-        $('.exemption-most-used').on('click', ':checkbox', function(){
-            var $elem = $(this);
-            var $allExemptionCheckbox = $('.exemption-all #search_query_exemption_ids_' + $elem.val());
+  init: function() {
+    $('.exemption-most-used').on('click', ':checkbox', function(){
+      var $elem = $(this);
+      var $allExemptionCheckbox = $('.exemption-all #search_query_exemption_ids_' + $elem.val());
 
-            if ($elem.is(':checked')) {
-                $allExemptionCheckbox.prop('checked',true)
-            } else {
-                $allExemptionCheckbox.prop('checked', false)
-            }
-        });
+      if ($elem.is(':checked')) {
+        $allExemptionCheckbox.prop('checked',true)
+      } else {
+        $allExemptionCheckbox.prop('checked', false)
+      }
+    });
 
-        $('.exemption-all').on('click', ':checkbox', function(){
-            var $elem = $(this);
-            var $commonUsedCheckbox = $('.exemption-most-used #search_query_common_exemption_ids_' + $elem.val());
+    $('.exemption-all').on('click', ':checkbox', function(){
+      var $elem = $(this);
+      var $commonUsedCheckbox = $('.exemption-most-used #search_query_common_exemption_ids_' + $elem.val());
 
-            if ($elem.is(':checked')) {
-                $commonUsedCheckbox.prop('checked',true)
-            } else {
-                $commonUsedCheckbox.prop('checked', false)
-            }
-        })
-    }
+      if ($elem.is(':checked')) {
+        $commonUsedCheckbox.prop('checked',true);
+      } else {
+        $commonUsedCheckbox.prop('checked', false);
+      }
+    })
+  }
 };
