3569c00813c2e78df5ef90bd7017572c01a03f3c
Misha Gorodnitzky <misaka@pobox.com>
2018-08-31 13:59:27 +0100

    CT-1848 add draft deadline to ICO Overturned SAR display

--- app/decorators/case/overturned_ico/sar_decorator.rb
+++ app/decorators/case/overturned_ico/sar_decorator.rb
@@ -1,7 +1,10 @@
 class Case::OverturnedICO::SARDecorator < Case::BaseDecorator
 
+  def internal_deadline
+    I18n.l(object.internal_deadline, format: :default)
+  end
+
   def original_case_description
     "ICO appeal (SAR) #{object.original_ico_appeal.number}"
   end
-
 end
