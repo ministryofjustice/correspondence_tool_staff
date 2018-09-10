9b14b162fb543fb4c30ee0770cfc6f2a2c2f8c99
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-09-10 17:19:39 +0100

    CT-1919 Case::Base subtypes specify which state machine config to use
    Previously, each Case::Base sub-class used a state machine config based on the type abbreviation.
    That meant a separate state machine config for each subclass.  This PR implements a #state_machine
    method on Case::Base which is used to determine which state machine config to load.  This is set to use the
    type bbreviation, but is overridden in Case::OverturnedICO::SAR to use the sar state machine config.

--- spec/state_machines/workflows/sar_permitted_events/sar_trigger_spec.rb
+++ spec/state_machines/workflows/sar_permitted_events/sar_trigger_spec.rb
@@ -52,6 +52,7 @@ describe ConfigurableStateMachine::Machine do
           k = create :pending_dacu_clearance_sar, :flagged_accepted
           expect(k.current_state).to eq 'pending_dacu_clearance'
           expect(k.state_machine.permitted_events(manager.id)).to eq [:add_message_to_case,
+                                                                      :assign_to_new_team,
                                                                       :destroy_case,
                                                                       :edit_case,
                                                                       :link_a_case,
