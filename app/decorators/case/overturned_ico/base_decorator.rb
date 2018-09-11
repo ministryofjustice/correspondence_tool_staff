799cceb0199810d7064a43e350373318a9d40450
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-09-11 15:07:45 +0100

    CT-1848 move view logic out to decorator

--- app/decorators/case/overturned_ico/base_decorator.rb
+++ app/decorators/case/overturned_ico/base_decorator.rb
@@ -3,4 +3,23 @@ class Case::OverturnedICO::BaseDecorator < Case::BaseDecorator
   def internal_deadline
     I18n.l(object.internal_deadline, format: :default)
   end
+
+  def formatted_date_ico_decision_received
+    I18n.l(object.date_ico_decision_received, format: :default)
+  end
+
+  def ico_decision_summary
+    Slim::Template.new do
+      <<~EOHTML
+      p
+        strong
+          = "MoJ's decision has been #{object.ico_decision} by the ICO "
+        = "on #{ self.formatted_date_ico_decision_received }"
+      
+      - if #{object.ico_decision_comment.present?}
+        p
+          = "#{object.ico_decision_comment}"
+      EOHTML
+    end.render.html_safe
+  end
 end
