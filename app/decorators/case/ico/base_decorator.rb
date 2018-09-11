fe8ea561f0162211f6f15f9b2f95790be3741f69
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-09-11 15:07:45 +0100

    CT-1848 move view logic out to decorator

--- app/decorators/case/ico/base_decorator.rb
+++ app/decorators/case/ico/base_decorator.rb
@@ -2,7 +2,23 @@ class Case::ICO::BaseDecorator < Case::BaseDecorator
 
   attr_accessor :original_case_number, :related_case_number
 
-  def date_decision_received
+  def formatted_date_ico_decision_received
     I18n.l(object.date_ico_decision_received, format: :default)
   end
+
+  def ico_decision_summary
+    Slim::Template.new do
+      <<~EOHTML
+      p
+        strong
+          = "MoJ's decision has been #{object.ico_decision} by the ICO "
+        = "on #{ self.formatted_date_ico_decision_received }"
+      
+      - if #{object.ico_decision_comment.present?}
+        p
+          = " #{object.ico_decision_comment}"
+      EOHTML
+    end.render.html_safe
+  end
+
 end
--- spec/decorators/case/ico/base_decorator_spec.rb
+++ spec/decorators/case/ico/base_decorator_spec.rb
@@ -1,16 +1,34 @@
 require "rails_helper"
 
 describe Case::ICO::BaseDecorator do
-  let(:closed_ico_foi_case) { (create :closed_ico_foi_case).decorate }
+  let(:closed_ico_foi_case)        { (create :closed_ico_foi_case).decorate }
+  let(:closed_overturned_foi_case) { (create :closed_ico_foi_case,
+                                      :overturned_by_ico).decorate }
+
 
   it 'instantiates the correct decorator' do
     expect(Case::ICO::Base.new.decorate).to be_instance_of Case::ICO::BaseDecorator
   end
 
-  describe '#date_decision_received' do
+  describe '#formatted_date_ico_decision_received' do
     it 'returns a formated date' do
       closed_ico_foi_case.object.date_ico_decision_received = Date.new(2017, 8, 13)
-      expect(closed_ico_foi_case.date_decision_received).to eq '13 Aug 2017'
+      expect(closed_ico_foi_case.formatted_date_ico_decision_received).to eq '13 Aug 2017'
+    end
+  end
+
+  describe '#ico_decision_summary' do
+    it 'returns a just the summary' do
+      closed_ico_foi_case.object.date_ico_decision_received = Date.new(2017, 8, 13)
+      expect(closed_ico_foi_case.ico_decision_summary)
+          .to eq '<p><strong>MoJ&#39;s decision has been upheld by the ICO </strong>on 13 Aug 2017</p>'
+    end
+
+    it 'returns summary and comment' do
+      closed_overturned_foi_case.object.date_ico_decision_received = Date.new(2017, 8, 13)
+      closed_overturned_foi_case.ico_decision_comment = 'Today is a good day'
+      expect(closed_overturned_foi_case.ico_decision_summary)
+          .to eq "<p><strong>MoJ&#39;s decision has been overturned by the ICO </strong>on 13 Aug 2017</p><p> Today is a good day</p>"
     end
   end
 
