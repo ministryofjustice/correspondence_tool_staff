a05f15ab82b91ce7dd4be12c65c1a43a8694a06d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-09-13 16:29:08 +0100

    CT-1929 Fix ICO SAR update closure details bug
    Updating the closure reasons of a closed ICO appeal for SAR
    cases crashed if no ICO decision files were uploaded.  This bug
    has been fixed.

--- spec/controllers/cases_controller/edit_closure_spec.rb
+++ spec/controllers/cases_controller/edit_closure_spec.rb
@@ -2,7 +2,30 @@ require "rails_helper"
 
 describe CasesController do
   describe 'GET edit_closure' do
-    let(:kase)    { create :closed_sar }
+    context 'SAR case' do
+      let(:kase)    { create :closed_sar }
+      let(:manager) { create :disclosure_bmt_user }
+
+      before do
+        sign_in manager
+      end
+
+      it 'authorises with update_closure? policy' do
+        expect{
+          get :edit_closure, params: { id: kase.id }
+        }.to require_permission(:update_closure?)
+               .with_args(manager, kase)
+      end
+
+      it 'renders the close page' do
+        get :edit_closure, params: { id: kase.id }
+        expect(response).to render_template :edit_closure
+      end
+    end
+  end
+
+  describe 'ICO appeal for SAR case' do
+    let(:kase)    { create :closed_ico_sar_case }
     let(:manager) { create :disclosure_bmt_user }
 
     before do
@@ -13,7 +36,7 @@ describe CasesController do
       expect{
         get :edit_closure, params: { id: kase.id }
       }.to require_permission(:update_closure?)
-             .with_args(manager, kase)
+               .with_args(manager, kase)
     end
 
     it 'renders the close page' do
