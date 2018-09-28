c7c71072faf9ff04e36e07b86dfa50af386cf588
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-09-28 12:18:00 +0100

    CT-1937 Fix code climate issues

--- app/services/case_linking_service.rb
+++ app/services/case_linking_service.rb
@@ -14,42 +14,26 @@ class CaseLinkingService
 
   def create
     ActiveRecord::Base.transaction do
+      begin
 
-      # By using a LinkedCase object here we can retrieve validation errors
-      # from the models
-      @case_link = LinkedCase.new(
-        linked_case_number: @link_case_number.to_s&.strip
-      )
-
-      #Create the links
-      @case.related_case_links << @case_link
-
-      if @case.valid?
-        @linked_case = @case_link.linked_case
-        # Log event in both cases
-        acting_team = acting_team_for_case_and_user
-        @case.state_machine.link_a_case!(acting_user: @user,
-                                         acting_team: acting_team,
-                                         linked_case_id: @linked_case.id)
-        @linked_case.state_machine.link_a_case!(acting_user: @user,
-                                                acting_team: acting_team,
-                                                linked_case_id: @case.id)
-        @result = :ok
-      else
-        add_case_link_errors_to_case(@case_link, @case)
-        @result = :validation_error
+        # By using a LinkedCase object here we can retrieve validation errors
+        # from the models
+        @case_link = LinkedCase.new(
+            linked_case_number: @link_case_number.to_s&.strip
+        )
+        create_links
+        @result
+      rescue CaseLinkingError => err
+        @case.errors.add(:linked_case_number, err.message)
+        @error = err
+        @result = :error
+      rescue => err
+        Rails.logger.error err.to_s
+        Rails.logger.error err.backtrace.join("\n\t")
+        @error = err
+        @result = :error
       end
     end
-    @result
-  rescue CaseLinkingError => err
-    @case.errors.add(:linked_case_number, err.message)
-    @error = err
-    @result = :error
-  rescue => err
-    Rails.logger.error err.to_s
-    Rails.logger.error err.backtrace.join("\n\t")
-    @error = err
-    @result = :error
   end
 
   def add_case_link_errors_to_case(case_link, kase)
@@ -92,6 +76,26 @@ class CaseLinkingService
   end
 
   private
+
+  def create_links
+    @case.related_case_links << @case_link
+
+    if @case.valid?
+      @linked_case = @case_link.linked_case
+      # Log event in both cases
+      acting_team = acting_team_for_case_and_user
+      @case.state_machine.link_a_case!(acting_user: @user,
+                                       acting_team: acting_team,
+                                       linked_case_id: @linked_case.id)
+      @linked_case.state_machine.link_a_case!(acting_user: @user,
+                                              acting_team: acting_team,
+                                              linked_case_id: @case.id)
+      @result = :ok
+    else
+      add_case_link_errors_to_case(@case_link, @case)
+      @result = :validation_error
+    end
+  end
 
   def acting_team_for_case_and_user
     if @case.sar? || @case.overturned_ico_sar?
