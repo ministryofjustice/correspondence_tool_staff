07b7d976255fac6f44704d733945fdc7e7b43490
Misha Gorodnitzky <misaka@pobox.com>
2018-10-01 10:23:03 +0100

    CT-1901 refactor create_ico_overturned_*_params

--- app/controllers/concerns/overturned_ico_params.rb
+++ app/controllers/concerns/overturned_ico_params.rb
@@ -2,19 +2,21 @@ module OverturnedICOParams
   extend ActiveSupport::Concern
 
   def create_ico_overturned_foi_params
-    params.require(:case_overturned_foi).permit(
-      :original_ico_appeal_id,
-      :original_case_id,
-      :reply_method,
-      :email,
-      :postal_address,
-      :received_date_dd, :received_date_mm, :received_date_yyyy,
-      :external_deadline_dd, :external_deadline_mm, :external_deadline_yyyy,
-    )
+    params
+      .require(:case_overturned_foi)
+      .permit(create_overturned_ico_params_list)
   end
 
   def create_ico_overturned_sar_params
-    params.require(:case_overturned_sar).permit(
+    params
+      .require(:case_overturned_sar)
+      .permit(create_overturned_ico_params_list)
+  end
+
+  private
+
+  def create_overturned_ico_params_list
+    [
       :original_ico_appeal_id,
       :original_case_id,
       :reply_method,
@@ -22,7 +24,7 @@ module OverturnedICOParams
       :postal_address,
       :received_date_dd, :received_date_mm, :received_date_yyyy,
       :external_deadline_dd, :external_deadline_mm, :external_deadline_yyyy,
-    )
+    ]
   end
 
   def respond_overturned_params
