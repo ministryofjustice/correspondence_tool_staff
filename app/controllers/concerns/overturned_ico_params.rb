6f084f38140111ea50a4de345f50a8455e21b519
Misha Gorodnitzky <misaka@pobox.com>
2018-10-01 23:01:15 +0100

    CT-1901 remove original_case_id and received_date from ot ico params

--- app/controllers/concerns/overturned_ico_params.rb
+++ app/controllers/concerns/overturned_ico_params.rb
@@ -28,11 +28,9 @@ module OverturnedICOParams
   def create_overturned_ico_params_list
     [
       :original_ico_appeal_id,
-      # :original_case_id,
       :reply_method,
       :email,
       :postal_address,
-      # :received_date_dd, :received_date_mm, :received_date_yyyy,
       :external_deadline_dd, :external_deadline_mm, :external_deadline_yyyy,
     ]
   end
