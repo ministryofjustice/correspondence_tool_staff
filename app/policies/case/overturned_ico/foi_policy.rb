965e55294643a08087537f90c3af60321a7e2884
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-10-05 17:15:07 +0100

    CT-1965 Hide request further clearance message
    When we go live with non-trigger OVT FOI, there will be a period
    in which trigger OVT FOI will not be available, so we should suppress
    the Request further clearance link.
    
    This is done by testing the overturned_trigger_fois FeatureSet,
    and only enabling that action in the policy if the feature set
    is enabled.
    
    Once triggeer OVT FOIs have been rolled out, the request_furnter_clearance?
    method in polocies/case/overturned_ico/foi_policy.rb can be removed, and let the
    superclass handle the policy.

--- app/policies/case/overturned_ico/foi_policy.rb
+++ app/policies/case/overturned_ico/foi_policy.rb
@@ -4,4 +4,17 @@ class Case::OverturnedICO::FOIPolicy < Case::FOI::StandardPolicy
     FeatureSet.ico.enabled? && @user.manager?
   end
 
+
+  # this method only needs to exist here until such time as we have
+  # trigger overturned sars enabled, at which time we can delete it here
+  # and it will use the base policy
+  #
+  def request_further_clearance?
+    clear_failed_checks
+
+    FeatureSet.overturned_trigger_fois.enabled? &&
+      check_user_is_a_manager_for_case &&
+          check_can_trigger_event(:request_further_clearance)
+  end
+
 end
