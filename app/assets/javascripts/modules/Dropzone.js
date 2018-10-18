2a0eb4e8676f79b84a00e3e46bbe94b09bf9412c
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-10-18 11:18:09 +0100

    CT-1621 WIP handling and checking file status

--- app/assets/javascripts/modules/Dropzone.js
+++ app/assets/javascripts/modules/Dropzone.js
@@ -1,11 +1,24 @@
 var Dropzone;
 
 moj.Modules.Dropzone = {
-  removeElement : function (elements){
-                    while(elements.length > 0){
-                        elements[0].parentNode.removeChild(elements[0]);
-                    }
-                  },
+  removeElement: function(elements) {
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+  },
+  //Store the files completed
+  ctCompletedFiles: [],
+  // Adds error message and styling
+  addErrorToFilePreview: function(file, errorMessage) {
+    var errorWrapper = file.previewElement.getElementsByClassName('dz-error-message')[0];
+
+    // Adds red border and red text on error
+    errorWrapper.classList.add('error-message');
+    file.previewElement.getElementsByClassName('dz-details')[0].classList.add('form-group-error');
+
+    // Display error message
+    errorWrapper.getElementsByTagName('span')[0].innerHTML= errorMessage;
+  },
   $target: {},
   init: function() {
     this.$target = $('.dropzone');
@@ -35,26 +48,30 @@ moj.Modules.Dropzone = {
         }
       },
       error: function(file, response) {
-        var errorWrapper = file.previewElement.getElementsByClassName('dz-error-message')[0];
         var removeLinkContainer = file.previewElement.getElementsByClassName('dz-remove-link');
         var removeLink = file.previewElement.getElementsByClassName('dz-remove');
 
-        // Adds red border and red text on error
-        //file.previewElement.classList.add('form-group-error');
-        errorWrapper.classList.add('error-message');
-        file.previewElement.getElementsByClassName('dz-filename')[0].classList.add('form-group-error');
-        // Display error message
-        errorWrapper.getElementsByTagName('span')[0].innerHTML= response;
+        //Add error styling and messaging
+        this.addErrorToFilePreview(file,response);
 
         ////Remove "remove" link
-        moj.Modules.Dropzone.removeElement(removeLinkContainer);
-        moj.Modules.Dropzone.removeElement(removeLink);
+        this.removeElement(removeLinkContainer);
+        this.removeElement(removeLink);
+      },
+      processing: function() {
+        $('.button').prop('disabled', true);
       },
       success : function(file, response) {
         // extract key and generate URL from response
         var responseDoc = $.parseXML(response);
         var $response   = $(responseDoc);
         var key = $response.find('Key').text();
+        var recordToCheck = {case_attachment: {key: key, type: 'response'} };
+        // create hidden field that will be sent to the server
+        var input = $('<input />', { class: 'case-uploaded-files'
+          , type:'hidden'
+          , name: fileInputName
+          , value: key });
 
         file.previewElement.classList.add('dz-success');
 
@@ -62,34 +79,9 @@ moj.Modules.Dropzone = {
           $(file.previewElement).find('.dz-upload').css('width', '100%');
         }
 
-        // create hidden field
-        var input = $('<input />', { class: 'case-uploaded-files'
-          , type:'hidden'
-          , name: fileInputName
-          , value: key });
-
         $(file.previewElement).append(input);
 
-        // Process the file
-        $.post(this.options.processFileUrl,
-          { case_attachment: {key: key, type: 'response'}  },
-          function(data){
-            var currentState = ''
-            uploadedFile = data
-
-            function checkState () {
-              $.get(uploadedFile.path, function(data){
-                console.log('checking')
-                console.log(data)
-                if (data.state !== 'virus_scan_passed' && data.state !== 'virus_scan_failed') {
-                  setTimeout(checkState, 1000);
-                }
-              })
-            }
-
-            checkState()
-            console.log (uploadedFile)
-          })
+        moj.Modules.Dropzone.CheckForVirus(this, file, recordToCheck);
       },
       removedfile : function(file) {
         // server-side will take care of cleaning up uploads dir
@@ -98,6 +90,68 @@ moj.Modules.Dropzone = {
     });
   }
 };
+
+moj.Modules.Dropzone.CheckForVirus = function (dropzone, file, recordToCheck) {
+  // Process the temporay uploaded file (virus scan)
+  var module = this;
+
+  $(file.previewElement).find('.dz-scanning').show().html('Scanning file for known viruses...');
+  $(file.previewElement).find('.dz-remove').hide();
+
+  $.post(dropzone.options.processFileUrl, recordToCheck,function(data){
+    var uploadedFile = data;
+    var checkStatusTimeOut = '';
+
+    function checkState () {
+      // It throws 404 Not Found error (if virus found) but thats OK.
+      // What happens is the record in the DB is deleted so it cant be found
+      $.get(uploadedFile.path, function(data){
+        switch (data.state){
+        case 'virus_scan_passed':
+          console.log('pass');
+          module.VirusTestPassed(file);
+          module.allScanningFinished();
+          break;
+        case 'virus_scan_failed':
+          module.VirusTestFailed(file, checkStatusTimeOut);
+          module.allScanningFinished();
+          break;
+        default:
+          checkStatusTimeOut = setTimeout(checkState, 1000);
+        }
+      });
+    }
+    checkState();
+  });
+};
+
+moj.Modules.Dropzone.VirusTestPassed = function (file) {
+  file.previewTemplate.setAttribute('virus-result', 'passed');
+  this.ctCompletedFiles.push(file);
+  $(file.previewElement).find('.dz-upload').css('width', '100%');
+  $(file.previewElement).find('.dz-scanning').hide();
+  $(file.previewElement).find('.dz-remove').show();
+};
+
+
+moj.Modules.Dropzone.VirusTestFailed = function (file, checkStatusTimeOut) {
+  var errMsg = 'Virus found. We can\'t upload this file';
+
+  //Add error styling and messaging
+  this.addErrorToFilePreview(file,errMsg);
+
+  $(file.previewTemplate).find('.case-uploaded-files, .dz-remove').remove();
+  this.ctCompletedFiles.push(file);
+  file.previewTemplate.setAttribute('virus-result', 'failed');
+  $(file.previewElement).find('.dz-scanning').hide();
+  clearTimeout(checkStatusTimeOut);
+};
+
+moj.Modules.Dropzone.allScanningFinished = function() {
+  if(this.ctCompletedFiles.length === $('.dz-preview').length){
+    $('.button').prop('disabled', false);
+  }
+}
 
 moj.Modules.ExternalLinks = {
   init: function() {
