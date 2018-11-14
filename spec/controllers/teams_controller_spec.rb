ca573c1ac7d8bbcbef2b409ec9717cb4d75948fc
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-11-14 12:04:50 +0000

    CT-2025 More spec amendments for new factories

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -69,27 +69,29 @@ RSpec.describe TeamsController, type: :controller do
 
       context 'viewing a business group' do
         it 'has no active users' do
-          get :show, params: { id: @bg.id }
+          get :show, params: { id: bg.id }
           expect(assigns(:active_users)).to be_empty
         end
       end
 
       context 'viewing a business unit' do
         before(:each) do
-          @user_1 = create :user, responding_teams: [business_unit]
-          @user_2 = create :user, responding_teams: [business_unit]
+          # @user_1 = create :user, responding_teams: [business_unit]
+          # @user_2 = create :user, responding_teams: [business_unit]
+          @user_1 = find_or_create :foi_responder
+          @user_2 = create :foi_responder, identifier: 'foi responder 2'
         end
 
         it 'assigns active users' do
           get :show, params: { id: business_unit.id }
-          expect(assigns(:active_users)).to eq [ @user_1, @user_2 ]
+          expect(assigns(:active_users)).to match_array [ @user_1, @user_2 ]
         end
 
         it 'calls UserActiveCaseCountService to populate case_counts' do
           counts = {@user_1.id => 34, @user_2.id => 6 }
           service = double UserActiveCaseCountService
           expect(UserActiveCaseCountService).to receive(:new).and_return(service)
-          expect(service).to receive(:case_counts_by_user).with([ @user_1, @user_2]).and_return(counts)
+          expect(service).to receive(:case_counts_by_user).with([ @user_2, @user_1]).and_return(counts)
 
           get :show, params: { id: business_unit.id }
           expect(assigns(:case_counts)).to eq counts
