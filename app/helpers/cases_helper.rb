3620168274762de2942978ad92ab90387f57917a
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2018-11-30 16:28:19 +0000

    CT-2027 removed commented out code and dump structure with correct version

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -91,11 +91,6 @@ module CasesHelper
               close_case_path(@case),
               id: 'action--close-case',
               class: 'button', method: :get
-    # when :respond_and_close
-    #   link_to I18n.t('common.case.close'),
-    #           respond_and_close_case_path(@case),
-    #           id: 'action--close-case',
-    #           class: 'button', method: :get
     when :progress_for_clearance
       link_to I18n.t('common.case.progress_for_clearance'),
               progress_for_clearance_case_path(@case),
