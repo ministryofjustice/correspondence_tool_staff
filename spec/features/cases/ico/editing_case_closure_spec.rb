d1bbb03d820ba9b886033c7dba525bd137fc3b21
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2019-02-08 10:09:21 +0000

    CT-2118 fix timecop errors

--- spec/features/cases/ico/editing_case_closure_spec.rb
+++ spec/features/cases/ico/editing_case_closure_spec.rb
@@ -4,24 +4,26 @@ feature 'editing case closure information' do
   given(:manager) { find_or_create :disclosure_bmt_user }
 
   scenario 'bmt changes ico decision to overturned', js: true do
-    Timecop.freeze(11.days.ago)
-    kase = create :closed_ico_foi_case
-    Timecop.return
-    login_as manager
-    cases_show_page.load(id: kase.id)
-    edit_ico_case_closure_step(kase: kase,
-                               decision_received_date: 10.business_days.ago,
-                               ico_decision: 'overturned')
+    Timecop.freeze(11.days.ago) do
+      kase = create :closed_ico_foi_case
+      Timecop.return
+      login_as manager
+      cases_show_page.load(id: kase.id)
+      edit_ico_case_closure_step(kase: kase,
+                                 decision_received_date: 10.business_days.ago,
+                                 ico_decision: 'overturned')
+    end
   end
 
   scenario 'bmt changes ico decision to upheld', js: true do
-    Timecop.freeze(11.days.ago)
-    kase = create :closed_ico_foi_case, :overturned_by_ico, created_at: 11.days.ago
-    Timecop.return
-    login_as manager
-    cases_show_page.load(id: kase.id)
-    edit_ico_case_closure_step(kase: kase,
-                               decision_received_date: 10.business_days.ago,
-                               ico_decision: 'upheld')
+    Timecop.freeze(11.days.ago) do
+      kase = create :closed_ico_foi_case, :overturned_by_ico, created_at: 11.days.ago
+      Timecop.return
+      login_as manager
+      cases_show_page.load(id: kase.id)
+      edit_ico_case_closure_step(kase: kase,
+                                 decision_received_date: 10.business_days.ago,
+                                 ico_decision: 'upheld')
+    end                           
   end
 end
--- spec/views/stats/index_html_slim_spec.rb
+++ spec/views/stats/index_html_slim_spec.rb
@@ -1,79 +1,80 @@
 require 'rails_helper'
 
 describe 'stats/index.html.slim', type: :view do
-  let(:report_1)      { build_stubbed :report_type, :r004 }
-  let(:report_2)      { build_stubbed :report_type, :r005 }
+  Timecop.freeze(Time.local(2019, 2, 8)) do
+    let(:report_1)      { build_stubbed :report_type, :r004 }
+    let(:report_2)      { build_stubbed :report_type, :r005 }
 
-  let(:report_3)      { build_stubbed :r105_report_type}
-  let(:report_4)      { instance_double ReportType,
-                                        id: 2,
-                                        full_name: "Report 4" }
-  let(:foi_reports)   { [ report_1, report_2 ]  }
-  let(:sar_reports)   { [ report_3 ]  }
-  let(:today)         { Date.today.strftime(Settings.default_date_format) }
-  let(:year_start)    { Date.new(2019, 1, 1).strftime(Settings.default_date_format) }
+    let(:report_3)      { build_stubbed :r105_report_type}
+    let(:report_4)      { instance_double ReportType,
+                                          id: 2,
+                                          full_name: "Report 4" }
+    let(:foi_reports)   { [ report_1, report_2 ]  }
+    let(:sar_reports)   { [ report_3 ]  }
+    let(:end_date)      { Date.today.strftime(Settings.default_date_format) }
+    let(:start_date)    { Date.new(2019, 1, 1).strftime(Settings.default_date_format) }
 
-  it 'has a heading' do
-    assign(:foi_reports, foi_reports)
-    assign(:sar_reports, sar_reports)
-    render
-    stats_index_page.load(rendered)
+    it 'has a heading' do
+      assign(:foi_reports, foi_reports)
+      assign(:sar_reports, sar_reports)
+      render
+      stats_index_page.load(rendered)
 
-    page = stats_index_page
-    expect(page.page_heading.heading.text).to eq "Performance reports"
-    expect(page.page_heading).to have_no_sub_heading
-  end
+      page = stats_index_page
+      expect(page.page_heading.heading.text).to eq "Performance reports"
+      expect(page.page_heading).to have_no_sub_heading
+    end
 
 
-  context 'displaying list of reports per type' do
-    context 'FOI' do
-      it 'has a list of FOI reports' do
-        assign(:foi_reports, foi_reports)
-        assign(:sar_reports, sar_reports)
-        render
-        stats_index_page.load(rendered)
+    context 'displaying list of reports per type' do
+      context 'FOI' do
+        it 'has a list of FOI reports' do
+          assign(:foi_reports, foi_reports)
+          assign(:sar_reports, sar_reports)
+          render
+          stats_index_page.load(rendered)
 
-        page = stats_index_page
+          page = stats_index_page
 
-        expect(page.foi.type_name.text).to eq 'Standard FOI reports'
+          expect(page.foi.type_name.text).to eq 'Standard FOI reports'
 
-        page.foi.reports.each_with_index  do | report, index |
-          expect(report.name.text).to eq foi_reports[index].full_name
-          if index == 0
-            expect(report.description.text)
-                .to eq "Includes performance data about how we are meeting statutory deadlines and how we are using exemptions."
-          else
-            expect(report.description.text)
-                .to eq 'Includes performance data about FOI requests we received and responded to from the beginning of the year by month.'
+          page.foi.reports.each_with_index  do | report, index |
+            expect(report.name.text).to eq foi_reports[index].full_name
+            if index == 0
+              expect(report.description.text)
+                  .to eq "Includes performance data about how we are meeting statutory deadlines and how we are using exemptions."
+            else
+              expect(report.description.text)
+                  .to eq 'Includes performance data about FOI requests we received and responded to from the beginning of the year by month.'
+            end
+
+            expect(report.report_period.text).to eq "Reporting period: #{start_date} to #{end_date}"
+            expect(report.download).to have_link("Download report - #{foi_reports[index].full_name}",  href: stats_download_path(id: foi_reports[index].id))
           end
-
-          expect(report.report_period.text).to eq "Reporting period: #{year_start} to #{today}"
-          expect(report.download).to have_link("Download report - #{foi_reports[index].full_name}",  href: stats_download_path(id: foi_reports[index].id))
         end
       end
-    end
 
-    context 'SAR' do
-      it 'has a list of SAR reports' do
-        assign(:foi_reports, foi_reports)
-        assign(:sar_reports, sar_reports)
-        render
-        stats_index_page.load(rendered)
+      context 'SAR' do
+        it 'has a list of SAR reports' do
+          assign(:foi_reports, foi_reports)
+          assign(:sar_reports, sar_reports)
+          render
+          stats_index_page.load(rendered)
 
-        page = stats_index_page
+          page = stats_index_page
 
-        expect(page.sar.type_name.text).to eq 'Standard SAR reports'
+          expect(page.sar.type_name.text).to eq 'Standard SAR reports'
 
-        page.sar.reports.each_with_index  do | report, index |
-          expect(report.name.text).to eq sar_reports[index].full_name
-          expect(report.description.text)
-              .to eq "Includes performance data about SAR requests we received and responded to from the beginning of the year by month."
+          page.sar.reports.each_with_index  do | report, index |
+            expect(report.name.text).to eq sar_reports[index].full_name
+            expect(report.description.text)
+                .to eq "Includes performance data about SAR requests we received and responded to from the beginning of the year by month."
 
-          expect(report.report_period.text).to eq "Reporting period: #{year_start} to #{today}"
-          expect(report.download).to have_link("Download report - #{sar_reports[index].full_name}",  href: stats_download_path(id: sar_reports[index].id))
+            expect(report.report_period.text).to eq "Reporting period: #{start_date} to #{end_date}"
+            expect(report.download).to have_link("Download report - #{sar_reports[index].full_name}",  href: stats_download_path(id: sar_reports[index].id))
+          end
         end
       end
     end
   end
-
 end
