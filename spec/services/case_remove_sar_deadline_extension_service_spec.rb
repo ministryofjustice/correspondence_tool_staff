a2b404f4fe6aa265fcec0b2f90a5d6bbcc730802
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-02-12 18:38:32 +0000

    CT-2109 Add CaseRemoveSARDeadlineExtensionService spec

---
+++ spec/services/case_remove_sar_deadline_extension_service_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+describe CaseRemoveSARDeadlineExtensionService do
+  let(:team_dacu)           { find_or_create :team_disclosure_bmt }
+  let(:manager)             { find_or_create :disclosure_bmt_user }
+  let!(:sar_case)           { create(:extended_deadline_sar) }
+  let!(:initial_deadline)   { sar_case.initial_deadline }
+  let!(:max_extension)      { Settings.sar_extension_limit.to_i }
+
+  before do
+    allow(sar_case.state_machine).to receive(:remove_sar_deadline_extension!)
+  end
+
+  describe '#initialize' do
+    subject { new_service(manager, sar_case) }
+
+    it { expect(sar_case.external_deadline).not_to eq initial_deadline }
+    it { expect(subject.result).to eq :incomplete }
+  end
+
+  describe '#call' do
+    context 'with expected params' do
+      subject! { new_service(manager, sar_case).call }
+
+      it { is_expected.to eq :ok }
+      it 'creates new SAR extension removal transition' do
+        expect(sar_case.state_machine)
+          .to have_received(:remove_sar_deadline_extension!)
+          .with(
+            acting_user: manager,
+            acting_team: team_dacu
+          )
+      end
+
+      it 'resets SAR deadline date' do
+        expect(sar_case.external_deadline).to eq initial_deadline
+      end
+    end
+
+    context 'exception' do
+      let!(:service) { new_service(manager, sar_case) }
+
+      it 'rolls-back changes' do
+        allow(sar_case).to receive(:reset_deadline!).and_throw(ArgumentError)
+
+        expect { service.call }.to raise_error(ArgumentError)
+        expect(service.result).to eq :error
+
+        transitions = sar_case.transitions.where(
+          event: 'remove_sar_deadline_extension'
+        )
+
+        expect(transitions.any?).to be false
+      end
+    end
+  end
+
+  private
+
+  def new_service(user, kase)
+    CaseRemoveSARDeadlineExtensionService.new(
+      user,
+      kase
+    )
+  end
+end
