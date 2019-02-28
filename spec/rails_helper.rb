b77b36e8970766b87a5355e5f90956506cbdfa61
stephen dicks <stephen.dicks@digital.justice.gov.uk>
2019-02-28 11:24:03 +0000

    CT-2131 add extra sections to simplecov report

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,13 @@
 if ENV['COVERAGE'].present?
   require 'simplecov'
-  SimpleCov.start 'rails'
+  SimpleCov.start 'rails' do
+    add_group "Services", "app/services"
+    add_group "Policies", "app/policies"
+    add_group "Decorators", "app/decorators"
+    add_group "Validators", "app/validators"
+    # application doesn't use action cable
+    add_filter '/app/channels/'
+  end
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
