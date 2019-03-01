f3f29e727a479e891bbd7f6bc3ad86ef84fabd14
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2019-03-01 11:52:42 +0000

    CT-2113 humanise outputs for csv

--- app/services/csv_exporter.rb
+++ app/services/csv_exporter.rb
@@ -11,14 +11,14 @@ class CSVExporter
       'Responder',
       'Date received',
       'Internal deadline',
-      'External_deadline',
+      'External deadline',
       'Date responded',
       'Date compliant draft uploaded',
-      'Workflow',
+      'Trigger',
       'Name',
       'Requester type',
       'Message',
-      'Info_held',
+      'Info held',
       'Outcome',
       'Refusal reason',
       'Exemptions',
@@ -41,34 +41,32 @@ class CSVExporter
   def to_csv #rubocop:disable Metrics/MethodLength,Metrics/CyclomaticComplexity
     begin
       [
-          @kase.number,
-          @kase.decorate.pretty_type,
-          @kase.current_state,
-          @kase.responding_team&.name,
-          @kase.responder&.full_name,
-          @kase.received_date&.to_s,
-          @kase.internal_deadline&.to_s,
-          @kase.external_deadline&.to_s,
-          @kase.date_responded&.to_s,
-          @kase.date_draft_compliant&.to_s,
-          @kase.workflow,
-          @kase.name,
-          @kase.requester_type,
-          dequote_and_truncate(@kase.message),
-          @kase.info_held_status&.name,
-          @kase.outcome&.name,
-          @kase.refusal_reason&.name,
-          @kase.exemptions.map{ |x| CaseClosure::Exemption.section_number_from_id(x.abbreviation) }.join(','),
-          @kase.postal_address,
-          @kase.email,
-          @kase.appeal_outcome&.name,
-          @kase.respond_to?(:third_party) ? @kase.third_party : nil,
-          @kase.respond_to?(:reply_method) ? @kase.reply_method : nil,
-          @kase.respond_to?(:subject_type) ? @kase.subject_type : nil,
-          @kase.respond_to?(:subject_full_name) ? @kase.subject_full_name : nil,
-          @kase.decorate.late_team_name,
-          kase_extended?(@kase) ? 'Yes' : 'No',
-          extension_count(@kase),
+        @kase.number,
+        @kase.decorate.pretty_type,
+        I18n.t("state.#{@kase.current_state}").downcase,
+        @kase.responding_team&.name,
+        @kase.responder&.full_name,
+        @kase.received_date&.to_s,
+        @kase.flagged? ? @kase.internal_deadline&.to_s : nil,
+        @kase.external_deadline&.to_s,
+        @kase.date_responded&.to_s,
+        @kase.date_draft_compliant&.to_s,
+        @kase.flagged? ? 'Yes' : nil,
+        @kase.name,
+        @kase.sar? ? nil : @kase.requester_type.humanize,
+        dequote_and_truncate(@kase.message),
+        @kase.info_held_status&.name,
+        @kase.outcome&.name,
+        @kase.refusal_reason&.name,
+        @kase.exemptions.map{ |x| CaseClosure::Exemption.section_number_from_id(x.abbreviation) }.join(','),
+        @kase.postal_address,
+        @kase.email,
+        @kase.appeal_outcome&.name,
+        @kase.respond_to?(:third_party) ? humanize_boolean(@kase.third_party) : nil,
+        @kase.respond_to?(:reply_method) ? @kase.reply_method.humanize : nil,
+        @kase.respond_to?(:subject_type) ? @kase.subject_type.humanize : nil,
+        @kase.respond_to?(:subject_full_name) ? @kase.subject_full_name : nil,
+        @kase.decorate.late_team_name
       ]
     rescue => err
       raise CSVExporterError.new("Error encountered formatting case id #{@kase.id} as CSV:\nOriginal error: #{err.class} #{err.message}")
@@ -104,5 +102,9 @@ class CSVExporter
 
   def dequote_and_truncate(text)
     text.tr('"', '').tr("'", '')[0..4000]
+  end
+
+  def humanize_boolean(boolean)
+    boolean ? 'Yes' : nil
   end
 end
--- spec/services/csv_exporter_spec.rb
+++ spec/services/csv_exporter_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe CSVExporter do
 
+<<<<<<< HEAD
   let(:late_team) { create :responding_team, name: 'Transport for London' }
   let(:late_foi_case) { create :closed_case, :fully_refused_exempt_s40,
                           :late,
@@ -25,6 +26,20 @@ describe CSVExporter do
                                    email: 'dave@moj.com',
                                    message: 'foi message',
                                    postal_address: nil }
+=======
+  let(:foi_case)          { create :closed_case, :fully_refused_exempt_s40,
+                                   name: 'FOI Case name',
+                                   email: 'dave@moj.com',
+                                   message: 'foi message',
+                                   postal_address: nil }
+  let(:sar_case)          { create :closed_sar,
+                                   name: 'SAR case name',
+                                   postal_address: "2 High Street\nAnytown\nAY2 4FF",
+                                   subject: 'Full details required',
+                                   email: 'theresa@moj.com',
+                                   message: 'my SAR message',
+                                   subject_full_name: 'Theresa Cant' }
+>>>>>>> CT-2113 humanise outputs for csv
 
   context 'late FOI' do
     it 'returns an array of fields' do
@@ -37,14 +52,22 @@ describe CSVExporter do
                              'closed',
                              'FOI Responding Team',
                              'foi responding user',
+<<<<<<< HEAD
                              '2018-08-17',
                              '2018-09-03',
                              '2018-09-17',
                              '2018-09-28',
                              '2018-09-28',
                              'standard',
+=======
+                             '2018-08-30',
+                             nil,
+                             '2018-09-27',
+                             '2018-09-25',
+                             nil,
+>>>>>>> CT-2113 humanise outputs for csv
                              'FOI Case name',
-                             'member_of_the_public',
+                             'Member of the public',
                              'foi message',
                              'Yes',
                              'Refused fully',
@@ -106,11 +129,12 @@ describe CSVExporter do
                               'SAR Responding Team',
                               'sar responding user',
                               '2018-08-30',
-                              '2018-09-09',
+                              nil,
                               '2018-09-29',
                               '2018-09-25',
                               nil,
                               'standard',
+                              nil,
                               'SAR case name',
                               nil,
                               'my SAR message',
@@ -121,6 +145,7 @@ describe CSVExporter do
                               "2 High Street\nAnytown\nAY2 4FF",
                               'theresa@moj.com',
                               nil,
+<<<<<<< HEAD
                               false,
                               'send_by_email',
                               'offender',
@@ -128,6 +153,13 @@ describe CSVExporter do
                               'N/A',
                               'No',
                               0
+                              'N/A'
+=======
+                              nil,
+                              'Send by email',
+                              'Offender',
+                              'Theresa Cant'
+>>>>>>> CT-2113 humanise outputs for csv
                           ]
       end
     end
