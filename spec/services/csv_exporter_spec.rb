a55fffef620b8e7041b186f0d87d53aaed297693
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2019-03-07 11:17:57 +0000

    CT-2113 retain draft timeliness value in csv spec

--- spec/services/csv_exporter_spec.rb
+++ spec/services/csv_exporter_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 describe CSVExporter do
-
-<<<<<<< HEAD
   let(:late_team) { create :responding_team, name: 'Transport for London' }
   let(:late_foi_case) { create :closed_case, :fully_refused_exempt_s40,
                           :late,
+                          :flagged,
                           name: 'FOI Case name',
                           email: 'dave@moj.com',
                           message: 'foi message',
@@ -26,20 +25,6 @@ describe CSVExporter do
                                    email: 'dave@moj.com',
                                    message: 'foi message',
                                    postal_address: nil }
-=======
-  let(:foi_case)          { create :closed_case, :fully_refused_exempt_s40,
-                                   name: 'FOI Case name',
-                                   email: 'dave@moj.com',
-                                   message: 'foi message',
-                                   postal_address: nil }
-  let(:sar_case)          { create :closed_sar,
-                                   name: 'SAR case name',
-                                   postal_address: "2 High Street\nAnytown\nAY2 4FF",
-                                   subject: 'Full details required',
-                                   email: 'theresa@moj.com',
-                                   message: 'my SAR message',
-                                   subject_full_name: 'Theresa Cant' }
->>>>>>> CT-2113 humanise outputs for csv
 
   context 'late FOI' do
     it 'returns an array of fields' do
@@ -52,20 +37,16 @@ describe CSVExporter do
                              'closed',
                              'FOI Responding Team',
                              'foi responding user',
-<<<<<<< HEAD
                              '2018-08-17',
                              '2018-09-03',
                              '2018-09-17',
                              '2018-09-28',
-                             '2018-09-28',
                              'standard',
-=======
                              '2018-08-30',
                              nil,
                              '2018-09-27',
                              '2018-09-25',
                              nil,
->>>>>>> CT-2113 humanise outputs for csv
                              'FOI Case name',
                              'Member of the public',
                              'foi message',
@@ -80,10 +61,8 @@ describe CSVExporter do
                              nil,
                              nil,
                              nil,
-                             late_team.name,
-                             'No',
-                             0
-                          ]
+                             late_team.name
+                         ]
       end
     end
   end
@@ -133,8 +112,6 @@ describe CSVExporter do
                               '2018-09-29',
                               '2018-09-25',
                               nil,
-                              'standard',
-                              nil,
                               'SAR case name',
                               nil,
                               'my SAR message',
@@ -145,21 +122,11 @@ describe CSVExporter do
                               "2 High Street\nAnytown\nAY2 4FF",
                               'theresa@moj.com',
                               nil,
-<<<<<<< HEAD
                               false,
                               'send_by_email',
                               'offender',
                               'Theresa Cant',
-                              'N/A',
-                              'No',
-                              0
                               'N/A'
-=======
-                              nil,
-                              'Send by email',
-                              'Offender',
-                              'Theresa Cant'
->>>>>>> CT-2113 humanise outputs for csv
                           ]
       end
     end
