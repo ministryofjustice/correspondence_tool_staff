2dc8abf922269e1ba6827c771f7b63081f546c79
stephen dicks <stephen.dicks@digital.justice.gov.uk>
2019-03-11 12:07:00 +0000

    CT-2112 remove manual transaction handling

--- app/services/case_deletion_service.rb
+++ app/services/case_deletion_service.rb
@@ -9,13 +9,11 @@ class CaseDeletionService
   # TODO - work out whether to catch all and log (which we forgot to do here)
   # or stay with this more idiomatic pattern (and maybe remove the transaction)
   def call
-    ActiveRecord::Base.transaction do
-      if @kase.update(@update_parameters.merge(deleted: true))
-        @kase.state_machine.destroy_case!(acting_user: @user, acting_team: @kase.managing_team)
-        :ok
-      else
-        :error
-      end
+    if @kase.update(@update_parameters.merge(deleted: true))
+      @kase.state_machine.destroy_case!(acting_user: @user, acting_team: @kase.managing_team)
+      :ok
+    else
+      :error
     end
   end
 end
