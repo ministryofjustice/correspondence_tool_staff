d8efa6a707204727893c629349d35ed3172af83c
stephen dicks <stephen.dicks@digital.justice.gov.uk>
2019-03-15 11:58:44 +0000

    CT-2113 improve test to cope with difficult merges

--- spec/services/csv_exporter_spec.rb
+++ spec/services/csv_exporter_spec.rb
@@ -31,36 +31,37 @@ describe CSVExporter do
       Timecop.freeze Time.local(2018, 10, 1, 13, 21, 33) do
         csv = CSVExporter.new(late_foi_case).to_csv
         expect(csv.size).to eq(CSVExporter::CSV_COLUMN_HEADINGS.size)
-        expect(csv).to eq [
-                             '180817001',
-                             'FOI',
-                             'closed',
-                             'FOI Responding Team',
-                             'foi responding user',
-                             '2018-08-17',
-                             '2018-09-03',
-                             '2018-09-17',
-                             '2018-09-28',
-                             '2018-09-28',
-                             'Yes',
-                             'FOI Case name',
-                             'Member of the public',
-                             'foi message',
-                             'Yes',
-                             'Refused fully',
-                             nil,
-                             's40',
-                             nil,
-                             'dave@moj.com',
-                             nil,
-                             nil,
-                             nil,
-                             nil,
-                             nil,
-                             late_team.name,
-                             'No',
-                             0
-                         ]
+        expect(CSVExporter::CSV_COLUMN_HEADINGS.zip(csv).to_h)
+          .to eq({
+                   'Number' => '180817001',
+                   'Case type' => 'FOI',
+                   'Current state' => 'closed',
+                   'Responding team' => 'FOI Responding Team',
+                   'Responder' => 'foi responding user',
+                   'Date received' => '2018-08-17',
+                   'Internal deadline' => '2018-09-03',
+                   'External deadline' => '2018-09-17',
+                   'Date responded' => '2018-09-28',
+                   'Date compliant draft uploaded' => '2018-09-28',
+                   'Trigger' => 'Yes',
+                   'Name' => 'FOI Case name',
+                   'Requester type' => 'Member of the public',
+                   'Message' => 'foi message',
+                   'Info held' => 'Yes',
+                   'Outcome' => 'Refused fully',
+                   'Refusal reason' => nil,
+                   'Exemptions' => 's40',
+                   'Postal address' => nil,
+                   'Email' => 'dave@moj.com',
+                   'Appeal outcome' => nil,
+                   'Third party' => nil,
+                   'Reply method' => nil,
+                   'SAR Subject type' => nil,
+                   'SAR Subject full name' => nil,
+                   'Business unit responsible for late response' => late_team.name,
+                   'Extended' => 'No',
+                   'Extension Count' => 0
+                 })
       end
     end
   end
@@ -68,7 +69,7 @@ describe CSVExporter do
   context 'extended' do
     let(:csv_data) do
       Timecop.freeze Time.local(2018, 10, 1, 13, 21, 33) do
-        CSVExporter.new(kase).to_csv[26..27]
+        CSVExporter::CSV_COLUMN_HEADINGS.zip(CSVExporter.new(kase).to_csv).to_h
       end
     end
 
@@ -79,7 +80,11 @@ describe CSVExporter do
                           message: 'foi message',
                           postal_address: nil }
       it 'marks case as having an extended count of 1' do
-        expect(csv_data).to eq ['Yes', 1]
+        # noinspection RubyResolve
+        expect(csv_data).to include(
+                              { 'Extended' => 'Yes',
+                                'Extension Count' => 1
+                              })
       end
     end
 
@@ -89,7 +94,11 @@ describe CSVExporter do
                           subject_full_name: 'Theresa Cant' }
 
       it 'marks an extended SAR having an extended count of 1' do
-        expect(csv_data).to eq ['Yes', 1]
+        # noinspection RubyResolve
+        expect(csv_data).to include({
+                                      'Extended' => 'Yes',
+                                      'Extension Count' => 1
+                                    })
       end
     end
   end
@@ -99,36 +108,37 @@ describe CSVExporter do
       Timecop.freeze Time.local(2018, 10, 1, 13, 21, 33) do
         csv = CSVExporter.new(sar_case).to_csv
         expect(csv.size).to eq(CSVExporter::CSV_COLUMN_HEADINGS.size)
-        expect(csv).to eq [
-                              '180830001',
-                              'SAR',
-                              'closed',
-                              'SAR Responding Team',
-                              'sar responding user',
-                              '2018-08-30',
-                              nil,
-                              '2018-09-29',
-                              '2018-09-25',
-                              nil,
-                              nil,
-                              'SAR case name',
-                              nil,
-                              'my SAR message',
-                              nil,
-                              nil,
-                              nil,
-                              '',
-                              "2 High Street\nAnytown\nAY2 4FF",
-                              'theresa@moj.com',
-                              nil,
-                              nil,
-                              'Send by email',
-                              'Offender',
-                              'Theresa Cant',
-                              'N/A',
-                              'No',
-                              0,
-        ]
+        expect(CSVExporter::CSV_COLUMN_HEADINGS.zip(csv).to_h)
+          .to eq({
+                   'Number' => '180830001',
+                   'Case type' => 'SAR',
+                   'Current state' => 'closed',
+                   'Responding team' => 'SAR Responding Team',
+                   'Responder' => 'sar responding user',
+                   'Date received' => '2018-08-30',
+                   'Internal deadline' => nil,
+                   'External deadline' => '2018-09-29',
+                   'Date responded' => '2018-09-25',
+                   'Date compliant draft uploaded' => nil,
+                   'Trigger' => nil,
+                   'Name' => 'SAR case name',
+                   'Requester type' => nil,
+                   'Message' => 'my SAR message',
+                   'Info held' => nil,
+                   'Outcome' => nil,
+                   'Refusal reason' => nil,
+                   'Exemptions' => '',
+                   'Postal address' => "2 High Street\nAnytown\nAY2 4FF",
+                   'Email' => 'theresa@moj.com',
+                   'Appeal outcome' => nil,
+                   'Third party' => nil,
+                   'Reply method' => 'Send by email',
+                   'SAR Subject type' => 'Offender',
+                   'SAR Subject full name' => 'Theresa Cant',
+                   'Business unit responsible for late response' => 'N/A',
+                   'Extended' => 'No',
+                   'Extension Count' => 0})
+
       end
     end
   end
