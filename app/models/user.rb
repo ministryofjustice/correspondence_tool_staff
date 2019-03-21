cbd4d8f73a725fe6a3c279f203e2ce4cfec1ba78
stephen dicks <stephen.dicks@digital.justice.gov.uk>
2019-03-21 13:35:20 +0000

    CT-2144 added comment about having the 'teams' collection eager-loaded by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,15 +32,18 @@ class User < ApplicationRecord
 
   # Most uses of User are in authentication - so preload team_roles and teams
   # so that we don't issue lots of DB queries when constantly asking what a user can do.
-  default_scope do  includes(:team_roles,
-                             :responding_team_roles,
-                             :responding_teams,
-                             :approving_team_roles,
-                             :approving_team,
-                             :managing_team_roles,
-                             :managing_teams,
-                             :teams,
-  )
+  #
+  # Needing teams in this list (along with managing_teams, approving_team and responding_teams)
+  # implies that we could have better implementations of the above based on the 'teams' collection
+  default_scope do
+    includes(:team_roles,
+             :responding_team_roles,
+             :responding_teams,
+             :approving_team_roles,
+             :approving_team,
+             :managing_team_roles,
+             :managing_teams,
+             :teams)
   end
 
   has_many :cases, through: :assignments
