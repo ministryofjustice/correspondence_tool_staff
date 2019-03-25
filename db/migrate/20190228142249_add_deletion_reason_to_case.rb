0b46b2fcceb98d295730e8594833f06cb3884c99
stephen dicks <stephen.dicks@digital.justice.gov.uk>
2019-03-25 08:26:04 +0000

    CT-2169 fix up migration

--- db/migrate/20190228142249_add_deletion_reason_to_case.rb
+++ db/migrate/20190228142249_add_deletion_reason_to_case.rb
@@ -1,7 +1,11 @@
 class AddDeletionReasonToCase < ActiveRecord::Migration[5.0]
-  def up
+  def  change
     change_table :cases do |t|
-      t.string :reason_for_deletion, null: false, default: 'Unspecified'
+      t.string :reason_for_deletion, null: true
     end
+    Case::Base.find_each do |kase|
+      kase.update!(reason_for_deletion: 'Unspecified')
+    end
+    change_column_null(:cases, :reason_for_deletion, false)
   end
 end
