45646e5f9b22307582908624f7d3e5b8e784faa9
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-04-30 21:56:08 +0100

    CT-2207 Update CasesStatsController to use simple Array instead of Hash

--- app/services/stats/cases_stats_collector.rb
+++ app/services/stats/cases_stats_collector.rb
@@ -4,8 +4,7 @@ module Stats
   # StatsController due to the way Closed Cases require output
   # of an unknown number of rows and field calculations are handled
   # by the CSVExporter class, which is also used to generate
-  # All Open Cases and My Open Cases. Consumers of this class are to
-  # treat it like a StatsController
+  # All Open Cases and My Open Cases.
   class CasesStatsCollector
     attr_reader :stats # Duck-type StatsController#stats
 
@@ -13,16 +12,15 @@ module Stats
     # values set for @stats will be consistent
     def initialize
       @columns_list = CSVExporter::CSV_COLUMN_HEADINGS
-      @stats = {}
+      @stats = []
     end
 
     def add(kase)
-      @stats[kase.number] = CSVExporter.new(kase).to_csv
+      @stats << CSVExporter.new(kase).to_csv
     end
 
     # Called by BaseReport#to_csv to create actual spreadsheet data
-    # duck-type StatsCollector#to_csv therefore ignores
-    # +first_column_header+ and +superheadings+ parameters
+    # Duck-type StatsCollector#to_csv
     def to_csv(*)
       CaseStatsEnumerator.new(
         @stats,
@@ -40,12 +38,10 @@ module Stats
 
       # Lazy enumerator as per Stats::StatsCollector::StatsEnumerator
       def each
-        @_stats ||= @stats.to_enum
-
         yield @columns_list
 
-        @_stats.each do |row|
-          yield row.second
+        @stats.each do |row|
+          yield row
         end
       end
     end
