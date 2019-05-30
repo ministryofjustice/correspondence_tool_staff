5e25f39b1859a21f6dbf7c44108dc38a5dabf536
digitalali-moj <javid.ali@digital.justice.gov.uk>
2019-05-30 16:05:18 +0100

    CT-2290 WIP Add fields to forms

--- app/models/case/sar/offender.rb
+++ app/models/case/sar/offender.rb
@@ -9,6 +9,12 @@ class Case::SAR::Offender < Case::SAR::Standard
   validates :prison_number, presence: true
 
   jsonb_accessor :properties,
-                 prison_number: :string
+                  prison_number: :string,
+                  subject_aliases: :string,
+                  previous_case_numbers: :string,
+                  other_subject_ids: :string,
+                  date_of_birth_dd: :string,
+                  date_of_birth_mm: :string,
+                  date_of_birth_yyyy: :string
 
 end
