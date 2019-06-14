46b6ba47b8aa79cb1c1d3e645511f74382872382
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-06-14 15:01:42 +0100

    CT-2236 Amend pit extension controller tests

---
+++ spec/controllers/cases/pit_extensions_controller/execute_extend_for_pit_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+describe Cases::PitExtensionsController, type: :controller do
+  describe '#create' do
+    let(:service) { double(CaseExtendForPITService, call: :ok) }
+    let(:manager) { find_or_create :disclosure_bmt_user }
+
+    let(:case_being_drafted) {
+      create :case_being_drafted, :flagged_accepted
+    }
+
+    let(:post_params) {
+      {
+        case_id: case_being_drafted.id,
+        case: {
+          extension_deadline_yyyy: '2017',
+          extension_deadline_mm: '02',
+          extension_deadline_dd: '10',
+          reason_for_extending:  'need more time',
+        }
+      }
+    }
+
+    before do
+      allow(CaseExtendForPITService).to receive(:new).and_return(service)
+      sign_in manager
+    end
+
+    it 'authorizes' do
+      expect { post :create, params: post_params }
+        .to require_permission(:extend_for_pit?).with_args(
+          manager,
+          case_being_drafted
+        )
+    end
+
+    context 'with valid params' do
+      before do
+        post :create, params: post_params
+      end
+
+      it 'calls the CaseExtendForPitService' do
+        expect(CaseExtendForPITService)
+          .to have_received(:new).with(
+            manager,
+            case_being_drafted,
+            Date.new(2017, 2, 10),
+            'need more time'
+          )
+        expect(service).to have_received(:call)
+      end
+
+      it 'notifies the user of the success' do
+        expect(request.flash[:notice]).to eq 'Case extended for Public Interest Test (PIT)'
+      end
+
+      it 'redirects to case details' do
+        expect(:result).to redirect_to(case_path(case_being_drafted.id))
+      end
+    end
+
+    context 'on service error' do
+      let(:service) { double(CaseExtendForPITService, call: :error) }
+
+      before do
+        post :create, params: post_params
+      end
+
+      it 'notifies the user of the failure' do
+        expect(flash[:alert]).to eq "Unable to perform PIT extension on case #{case_being_drafted.number}"
+        expect(:result).to redirect_to(case_path(case_being_drafted.id))
+      end
+    end
+  end
+end
