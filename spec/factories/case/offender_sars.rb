09ac89da1361b963dc17dfc55e8fff9a1a75ce0b
digitalali-moj <javid.ali@digital.justice.gov.uk>
2019-06-21 12:07:01 +0100

    CT-2325 Adjustments after cherry-pick

---
+++ spec/factories/case/offender_sars.rb
@@ -0,0 +1,73 @@
+# == Schema Information
+#
+# Table name: cases
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  email                :string
+#  message              :text
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  received_date        :date
+#  postal_address       :string
+#  subject              :string
+#  properties           :jsonb
+#  requester_type       :enum
+#  number               :string           not null
+#  date_responded       :date
+#  outcome_id           :integer
+#  refusal_reason_id    :integer
+#  current_state        :string
+#  last_transitioned_at :datetime
+#  delivery_method      :enum
+#  workflow             :string
+#  deleted              :boolean          default(FALSE)
+#  info_held_status_id  :integer
+#  type                 :string
+#  appeal_outcome_id    :integer
+#  dirty                :boolean          default(FALSE)
+#
+
+FactoryBot.define do
+
+  factory :offender_sar_case, class: Case::SAR::Offender do
+    transient do
+      creation_time       { 4.business_days.ago }
+      identifier          { "new offender sar case" }
+      managing_team       { find_or_create :team_dacu }
+      manager             { managing_team.managers.first }
+      responding_team     { find_or_create :sar_responding_team }
+      responder           { responding_team.responders.first }
+      flag_for_disclosure { nil }
+      approving_team      { find_or_create :team_disclosure }
+      approver            { approving_team.approvers.first }
+      i_am_deleted        { false }
+    end
+
+    trait :third_party do
+      third_party { true }
+      third_party_relationship { 'Aunt' }
+    end
+
+    current_state                 { 'unassigned' }
+    sequence(:name)               { |n| "#{identifier} name #{n}" }
+    email                         { Faker::Internet.email(identifier) }
+    reply_method                  { 'send_by_email' }
+    sequence(:subject)            { |n| "#{identifier} subject #{n}" }
+
+
+    sequence(:message)            { |n| "#{identifier} message #{n}" }
+    received_date                 { Time.zone.today.to_s }
+    sequence(:postal_address)     { |n| "#{identifier} postal address #{n}" }
+    sequence(:subject_full_name)  { |n| "Subject #{n}" }
+    sequence(:subject_aliases)    { |n| "#{identifier} subject alias #{n}" }
+    previous_case_numbers         { "54321" }
+    prison_number                 { "123465" }
+    date_of_birth                 { "Time.zone.today.to_s" }
+    subject_type                  { 'offender' }
+    third_party                   { false }
+    created_at                    { creation_time }
+    creator                       { create(:user, :orphan) }
+
+  end
+end
