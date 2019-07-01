f52c5788245a62fb934a9d69a20f59ac06ccce6f
Javid <javid.ali@digital.justice.gov.uk>
2019-07-01 11:17:21 +0100

    Ct 2325 testing 1 4 pages offender (#823)
    * CT-2325 Refactor tests for offender SARS 1-4 but untested due to blocker
    
    * CT-2325 Adjustments after cherry-pick
    
    * CT-2325 Complete initial tests for Offender Sar pages 1-4
    
    * CT-2325 Remove useless comments as per feedback

--- spec/site_prism/page_objects/pages/cases/new/osar_page_requester_details.rb
+++ spec/site_prism/page_objects/pages/cases/new/osar_page_requester_details.rb
@@ -12,15 +12,35 @@ module PageObjects
           section :page_heading,
                   PageObjects::Sections::PageHeadingSection, '.page-heading'
 
-          element :subject_full_name, '#case_sar_subject_full_name'
+          element :email_address, '#offender_sar_case_form_email'
+
+          element :postal_address, '#offender_sar_case_form_postal_address'
+
+          element :name, 'offender_sar_case_form_name'
+
+          element :third_party_relationship, '#offender_sar_case_form_third_party_relationship'
 
           element :submit_button, '.button'
 
-          # def set_received_date(received_date)
-          #   date_received_day.set(received_date.day)
-          #   date_received_month.set(received_date.month)
-          #   date_received_year.set(received_date.year)
-          # end
+          def fill_in_case_details(params={})
+            kase = FactoryBot.build :offender_sar_case, params
+
+            if kase.third_party?
+              choose('offender_sar_case_form_third_party_true', visible: false)
+              requester_full_name.set kase.name
+              third_party_relationship.set kase.third_party_relationship
+            else
+              choose('offender_sar_case_form_third_party_false', visible: false)
+            end
+
+            if kase.send_by_email?
+              choose('offender_sar_case_form_reply_method_send_by_email', visible: false)
+              email_address.set kase.email
+            elsif kase.send_by_post?
+              choose('offender_sar_case_form_reply_method_send_by_postase.email', visible: false)
+              postal_address.set kase.postal_address
+            end
+          end
         end
       end
     end
--- spec/support/features/steps/cases/create_steps.rb
+++ spec/support/features/steps/cases/create_steps.rb
@@ -93,31 +93,21 @@ def create_offender_sar_case_step(params={})
   click_on "Continue"
   expect(cases_new_offender_sar_requester_details_page).to be_displayed
 
-  choose('offender_sar_case_form_third_party_true', visible: false)
-  fill_in :offender_sar_case_form_name, with: "John Ali"
-  fill_in :offender_sar_case_form_third_party_relationship, with: "Father"
-
-  choose('offender_sar_case_form_reply_method_send_by_email', visible: false)
-  fill_in :offender_sar_case_form_email, with: "bob@example.com"
-
-  choose('offender_sar_case_form_reply_method_send_by_post', visible: false)
-  fill_in :offender_sar_case_form_postal_address, with: "66A Eltham Road, Nottingham, NG2 5AA"
+  cases_new_offender_sar_requester_details_page.fill_in_case_details(params)
   click_on "Continue"
   expect(cases_new_offender_sar_requested_info_page).to be_displayed
 
-
-  fill_in :offender_sar_case_form_message, with: "Offender Sar Case, urgent, needs to be looked at, please! like now? while we're young, today!"
+  cases_new_offender_sar_requested_info_page.fill_in_case_details(params)
   click_on "Continue"
   expect(cases_new_offender_sar_date_received_page).to be_displayed
 
-  fill_in :offender_sar_case_form_received_date_dd, with: "21"
-  fill_in :offender_sar_case_form_received_date_mm, with: "05"
-  fill_in :offender_sar_case_form_received_date_yyyy, with: "2019"
-  click_on "Continue"
+  cases_new_offender_sar_date_received_page.fill_in_case_details(params)
 
+  click_on "Continue"
   # commented out next expectation while team assignment issue is getting fixed
   # expect(cases_show_page).to be_displayed
   # expect(cases_show_page).to have_content "Case created successfully"
+
 end
 
 def create_overturned_ico_case_step(params={})
