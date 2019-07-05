36752b6453f6ccb52e8ada6f3c8e95120dddcc89
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-07-05 15:06:43 +0100

    CT-2236 Clear all report types before report_type_spec
    - Rake task to make testing parallel issues locally slightly easier

--- spec/models/report_type_spec.rb
+++ spec/models/report_type_spec.rb
@@ -17,13 +17,16 @@
 require 'rails_helper'
 
 RSpec.describe ReportType, type: :model do
-  before(:each) do
-    ReportType.destroy_all
-  end
+  after(:all) { ReportType.delete_all }
 
   it { should have_many(:reports) }
 
   describe 'custom scope' do
+
+    before do
+      ReportType.destroy_all
+    end
+
     it 'returns only closed cases in most recently closed first' do
       create :report_type
       custom_report_1 = create :report_type, custom_report: true
@@ -33,6 +36,11 @@ RSpec.describe ReportType, type: :model do
   end
 
   describe 'standard scope' do
+
+    before do
+      ReportType.destroy_all
+    end
+
     it 'returns only closed cases in most recently closed first' do
       create :report_type
       custom_report_1 = create :report_type, standard_report: true
@@ -42,6 +50,11 @@ RSpec.describe ReportType, type: :model do
   end
 
   describe 'foi scope' do
+
+    before do
+      ReportType.destroy_all
+    end
+
     it 'returns only reports associated with fois' do
       create :report_type
       custom_report_1 = create :report_type, foi: true
@@ -53,6 +66,11 @@ RSpec.describe ReportType, type: :model do
   end
 
   describe 'sar scope' do
+
+    before do
+      ReportType.destroy_all
+    end
+
     it 'returns only reports associated with sars' do
       create :report_type
       custom_report_1 = create :report_type, sar: true
@@ -65,6 +83,7 @@ RSpec.describe ReportType, type: :model do
 
   describe '#class_constant' do
     it 'returns the report class name as a constant' do
+      r003 = create :report_type, :r003
       expect(r003.class_constant)
         .to eq Stats::R003BusinessUnitPerformanceReport
     end
@@ -72,6 +91,7 @@ RSpec.describe ReportType, type: :model do
 
   describe '#filename' do
     it 'formats the class name into a filename' do
+      r003 = find_or_create :report_type, :r003
       expect(r003.filename('csv')).to eq 'r003_business_unit_performance_report.csv'
     end
   end
@@ -95,12 +115,14 @@ RSpec.describe ReportType, type: :model do
 
     context '#file_extension' do
       it 'assumes csv only if concrete class does not support xlsx' do
+        r006 = create :report_type, :r006
         expect(r006.file_extension).to eq 'csv'
       end
     end
 
     context '#description' do
       it 'returns concrete class description' do
+        r003 = create :report_type, :r003
         expect(r003.description)
           .to eq Stats::R003BusinessUnitPerformanceReport.description
       end
