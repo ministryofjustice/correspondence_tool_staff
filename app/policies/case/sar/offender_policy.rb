427d156fad4f962f24e7e0d5403daeb9c3966c1a
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-07-10 17:47:07 +0100

    CT-2236 Allow all logged in users to view Offender SARS during testing

--- app/policies/case/sar/offender_policy.rb
+++ app/policies/case/sar/offender_policy.rb
@@ -1,2 +1,6 @@
 class Case::SAR::OffenderPolicy < Case::SAR::StandardPolicy
+  # @todo (mseedat-moj): Allow all users to view Offender SAR during MVP dev
+  def show?
+    clear_failed_checks
+  end
 end
