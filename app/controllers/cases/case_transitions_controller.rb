692992ad8814a3dc54ecf65f2e5d12769e46ff20
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-07-19 17:37:11 +0100

    wip - "undefined method `valid?' for nil:NilClass" when calling `case.state_machine.mark_as_waiting_for_data`

--- app/controllers/cases/case_transitions_controller.rb
+++ app/controllers/cases/case_transitions_controller.rb
@@ -2,8 +2,9 @@ module Cases
   class CaseTransitionsController < CasesController
     def create
       @case = Case::Base.find(params[:case_id])
-      # authorize @case
+      authorize @case
 
+      @case.state_machine.mark_as_waiting_for_data!(acting_user: current_user, acting_team: current_user.managing_teams.first)
     end
   end
 end
--- app/policies/case/sar/offender_policy.rb
+++ app/policies/case/sar/offender_policy.rb
@@ -1,2 +1,7 @@
 class Case::SAR::OffenderPolicy < Case::SAR::StandardPolicy
+  def create?
+    clear_failed_checks
+    check_can_trigger_event(:mark_as_waiting_for_data) &&
+      check_user_is_a_manager_for_case
+  end
 end
