f4ba758567717c2c9f928a1485e4c34ac86ad28a
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-07-24 13:58:49 +0100

    wip - button not appearing

--- app/controllers/cases/case_transitions_controller.rb
+++ app/controllers/cases/case_transitions_controller.rb
@@ -4,7 +4,7 @@ module Cases
       @case = Case::Base.find(params[:case_id])
       authorize @case
 
-      if @case.state_machine.mark_as_next_state!(acting_user: current_user, acting_team: current_user.managing_teams.first)
+      if @case.state_machine.mark_as_waiting_for_data!(acting_user: current_user, acting_team: current_user.managing_teams.first)
         redirect_to(case_path(@case))
       end
     end
