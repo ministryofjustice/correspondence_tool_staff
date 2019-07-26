ae3baf089bc3ce2dc09b6c189f35eacdecdba687
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-07-26 17:29:28 +0100

    CT-2238 Dry up the controller spec using a shared example

--- app/controllers/cases/case_transitions_controller.rb
+++ app/controllers/cases/case_transitions_controller.rb
@@ -52,6 +52,8 @@ module Cases
     # this method is here to fix an issue with the gov_uk_date_fields
     # where the validation fails since the internal list of instance
     # variables lacks the date_of_birth field from the json properties
+    #     NoMethodError: undefined method `valid?' for nil:NilClass
+    #     ./app/state_machines/configurable_state_machine/machine.rb:256
     def set_date_of_birth_instance_var
       @case.date_of_birth = @case.date_of_birth
     end
--- spec/controllers/cases/case_transitions_controller_spec.rb
+++ spec/controllers/cases/case_transitions_controller_spec.rb
@@ -1,36 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cases::CaseTransitionsController, type: :controller do
-  describe "#mark_as_waiting_for_data" do
-    before do
-      sign_in manager
-    end
-    let(:manager) { find_or_create :branston_user }
-    let(:offender_sar) { create(:offender_sar_case).decorate }
-    let(:params) {{ id: offender_sar.id }}
-
-    it 'sets @case' do
-      patch :mark_as_waiting_for_data, params: params
-
-      expect(assigns(:case)).to eq offender_sar
-    end
-
-    it 'authorizes' do
-      expect {
-        patch :mark_as_waiting_for_data, params: params
-      }.to require_permission(:mark_as_waiting_for_data?)
-        .with_args(manager, offender_sar)
-    end
-
-    it 'flashes a notification' do
-      patch :mark_as_waiting_for_data, params: params
-      expect(flash[:notice])
-        .to eq 'Case updated'
-    end
-
-    it 'redirects to case details page' do
-      patch :mark_as_waiting_for_data, params: params
-      expect(response).to redirect_to(case_path(offender_sar))
-    end
-  end
+  it_behaves_like 'edit offender sar spec', :offender_sar_case, :mark_as_waiting_for_data
+  it_behaves_like 'edit offender sar spec', :waiting_for_data_offender_sar, :mark_as_ready_for_vetting
+  it_behaves_like 'edit offender sar spec', :ready_for_vetting_offender_sar, :mark_as_vetting_in_progress
+  it_behaves_like 'edit offender sar spec', :vetting_in_progress_offender_sar, :mark_as_ready_to_copy
+  it_behaves_like 'edit offender sar spec', :ready_to_copy_offender_sar, :mark_as_ready_to_dispatch
+  # it_behaves_like 'edit offender sar spec', :ready_to_dispatch_offender_sar, :mark_as_closed
 end
