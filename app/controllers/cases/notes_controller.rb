358a4e48338d7f3b0c9b9928603fdb680f223f70
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-07-29 14:49:55 +0100

    CT-2257 Add note to case

---
+++ app/controllers/cases/notes_controller.rb
@@ -0,0 +1,52 @@
+module Cases
+  class NotesController < ApplicationController
+    before_action :set_case, except: []
+    before_action :set_date_of_birth_instance_var, except: []
+
+    def create
+      begin
+        @case.state_machine.add_note_to_case!(
+          acting_user: current_user,
+          acting_team: case_team,
+          message: params[:case][:message_text]
+        )
+      rescue ActiveRecord::RecordInvalid => err
+        if err.record.errors.include?(:message)
+          flash[:case_errors] = { message_text: err.record.errors[:message] }
+        end
+      ensure
+        redirect_to case_path(@case, anchor: 'case-history')
+      end
+    end
+
+    private
+
+    # need to sort with manager first so that if we are both manager and something else, we don't
+    # try to execute the action with our lower authority (which might fail)
+    def case_team
+      User.sort_teams_by_roles(case_teams).first
+    end
+
+    def case_teams
+      if current_user.teams_for_case(@case).any?
+        current_user.teams_for_case(@case)
+      else
+        current_user.teams
+      end
+    end
+
+    def set_case
+      @case = Case::Base.find(params[:case_id])
+      authorize(@case, :can_add_note_to_case?)
+    end
+
+    # this method is here to fix an issue with the gov_uk_date_fields
+    # where the validation fails since the internal list of instance
+    # variables lacks the date_of_birth field from the json properties
+    #     NoMethodError: undefined method `valid?' for nil:NilClass
+    #     ./app/state_machines/configurable_state_machine/machine.rb:256
+    def set_date_of_birth_instance_var
+      @case.date_of_birth = @case.date_of_birth
+    end
+  end
+end
