5e59316914498f54c563dd341d641333dafa0666
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-07-31 11:09:45 +0100

    CT-2390 Add System section to dashboard showing useful app info

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -27,12 +27,29 @@ describe Admin::DashboardController do
       get :search_queries
     end
 
-    it 'renders the index view' do
+    it 'renders the search_queries view' do
       expect(request.path).to eq('/admin/dashboard/search_queries')
     end
 
     it 'has search queries' do
       expect(subject.queries).to eq [search_query]
+    end
+  end
+
+  describe '#system' do
+    before do
+      sign_in admin
+      get :system
+    end
+
+    it 'renders the system view' do
+      expect(request.path).to eq('/admin/dashboard/system')
+    end
+
+    it 'has Git version SHA' do
+      git_sha = assigns(:version)
+      expect(git_sha).to be_present
+      expect(git_sha).to match /^[0-9a-f]{40}$/
     end
   end
 end
