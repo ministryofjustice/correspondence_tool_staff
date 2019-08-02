dcf26a1915d6fd903f9d7fa9aa4462884836fd40
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-08-02 16:09:17 +0100

    CT-2257 move govuk date fix into its own concern

--- app/controllers/cases/notes_controller.rb
+++ app/controllers/cases/notes_controller.rb
@@ -1,5 +1,7 @@
 module Cases
   class NotesController < ApplicationController
+    include GovUKDateFixes
+
     before_action :set_case, :set_date_of_birth
 
     def create
@@ -37,15 +39,6 @@ module Cases
     def set_case
       @case = Case::Base.find(params[:case_id])
       authorize(@case, :can_add_note_to_case?)
-    end
-
-    # this method is here to fix an issue with the gov_uk_date_fields
-    # where the validation fails since the internal list of instance
-    # variables lacks the date_of_birth field from the json properties
-    #     NoMethodError: undefined method `valid?' for nil:NilClass
-    #     ./app/state_machines/configurable_state_machine/machine.rb:256
-    def set_date_of_birth
-      @case.date_of_birth = @case.date_of_birth
     end
   end
 end
