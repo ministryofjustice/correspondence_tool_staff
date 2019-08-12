12894ecc21f7aaa043ff0b721f56eab387463adc
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-08-12 14:35:31 +0100

    CT-2192 Remove space causing github diff change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   # get run.
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-  
+
   # # Many RSpec users commonly either run the entire suite or an individual
   # # file, and it's useful to allow more verbose output when running an
   # # individual spec file.
