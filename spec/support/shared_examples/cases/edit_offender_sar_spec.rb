dca1921fb2062f36b9d12207b33e23097f7538d8
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-08-28 13:49:52 +0100

    CT-2444 Simplify Offender SAR factory and related tests

--- spec/support/shared_examples/cases/edit_offender_sar_spec.rb
+++ spec/support/shared_examples/cases/edit_offender_sar_spec.rb
@@ -1,36 +1,44 @@
 require 'rails_helper'
 
-RSpec.shared_examples 'edit offender sar spec' do |offender_sar_case, method|
-  describe "mark methods" do
-    before do
-      sign_in manager
-    end
-    let(:manager) { find_or_create :branston_user }
-    let(:offender_sar) { create(offender_sar_case).decorate }
-    let(:params) {{ id: offender_sar.id, transition_name: method }}
+RSpec.shared_examples 'edit offender sar spec' do |current_state, event_name|
+  def offender_sar_case(with_trait:)
+    create :offender_sar_case, with_trait.to_sym
+  end
 
-    it 'sets @case' do
-      patch :transition, params: params
+  let(:manager) { find_or_create :branston_user }
+  let(:offender_sar) { (offender_sar_case with_trait: current_state).decorate }
+  let(:params) {{ id: offender_sar.id, transition_name: event_name }}
 
-      expect(assigns(:case)).to eq offender_sar
-    end
+  before do
+    sign_in manager
+  end
 
+  context '#transition' do
     it 'authorizes' do
-      expect {
+      expect { patch :transition, params: params }
+        .to require_permission('transition?').with_args(manager, offender_sar)
+    end
+
+    context 'updates Offender SAR state' do
+      before do
         patch :transition, params: params
-      }.to require_permission("transition?")
-        .with_args(manager, offender_sar)
-    end
+      end
 
-    it 'flashes a notification' do
-      patch :transition, params: params
-      expect(flash[:notice])
-        .to eq 'Case updated'
-    end
+      it 'sets case' do
+        expect(assigns(:case)).to eq offender_sar
+      end
 
-    it 'redirects to case details page' do
-      patch :transition, params: params
-      expect(response).to redirect_to(case_path(offender_sar))
+      it 'flashes a notification' do
+        expect(controller.flash[:notice]).to eq 'Case updated'
+      end
+
+      it 'redirects to case details page' do
+        expect(response).to redirect_to(case_path(offender_sar))
+      end
+
+      it 'changes case state' do
+        expect(offender_sar.current_state).not_to eq current_state
+      end
     end
   end
 end
