753aca1542b84f4fab010a3936e67d95fab36e71
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-08-29 23:33:37 +0100

    CT-2261 Coalesce database changes into single migration

--- db/migrate/20190823160424_add_data_received_to_data_request.rb
+++
@@ -1,6 +0,0 @@
-class AddDataReceivedToDataRequest < ActiveRecord::Migration[5.0]
-  def change
-    add_column :data_requests, :date_received, :date, null: true
-    add_column :data_requests, :num_pages, :integer, default: 0, null: false
-  end
-end
