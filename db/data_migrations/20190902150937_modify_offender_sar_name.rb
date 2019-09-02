657b2924710993142ebfd2e50e146893c3973531
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-09-02 17:33:56 +0100

    CT-2396 added data migration to rename Offender SAR

---
+++ db/data_migrations/20190902150937_modify_offender_sar_name.rb
@@ -0,0 +1,9 @@
+class ModifyOffenderSarName < ActiveRecord::DataMigration
+  def up
+    CorrespondenceType.where(abbreviation: "OFFENDER_SAR").update(name: "Offender subject access request")
+  end
+
+  def down
+    CorrespondenceType.where(abbreviation: "OFFENDER_SAR").update(name: "Offender SAR (OFFENDER)")
+  end
+end
