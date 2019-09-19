58ff949c679b51669ce53e0e31ef32cf0374cf45
Javid <javid.ali@digital.justice.gov.uk>
2019-09-19 10:55:11 +0100

    CT-2398 Add keys for subject type radios and change apostrophe to… (#918)
    * CT-2398 WIP add keys for subject type radios and change apostrophe to straight
    
    * CT-2398 Add hyphen to case show page where it says Ex offender
    
    * CT-2398 Replace gsub with tr as per Rubocop
    
    * Trigger rebuild
    
    * Trigger rebuild

--- app/decorators/case/sar/offender_decorator.rb
+++ app/decorators/case/sar/offender_decorator.rb
@@ -5,7 +5,7 @@ class Case::SAR::OffenderDecorator < Case::BaseDecorator
   end
 
   def subject_type_display
-    object.subject_type.humanize
+    object.subject_type.humanize.tr(' ', '-')
   end
 
   def third_party_display
