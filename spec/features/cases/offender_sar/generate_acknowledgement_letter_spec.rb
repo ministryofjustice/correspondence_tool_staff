411f70b79bfc71da1b3d1b04e6d2c8393ea4cc24
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-09-27 17:35:58 +0100

    CT-2423 Managers can generate a letter after a case is created and when waiting for data

--- spec/features/cases/offender_sar/generate_acknowledgement_letter_spec.rb
+++ spec/features/cases/offender_sar/generate_acknowledgement_letter_spec.rb
@@ -3,27 +3,38 @@ require 'rails_helper'
 feature 'Generate an acknowledgement letter by a manager' do
   given(:manager)           { find_or_create :branston_user }
   given(:managing_team)     { create :managing_team, managers: [manager] }
-  given(:offender_sar_case) { create(:offender_sar_case, :waiting_for_data, name: "Bob").decorate }
   given!(:letter_template)   { find_or_create :letter_template }
+  given(:offender_sar_case) { create(:offender_sar_case).decorate }
+  given(:offender_sar_case_waiting) { create(:offender_sar_case, :waiting_for_data, name: "Bob").decorate }
 
   background do
     find_or_create :team_branston
     login_as manager
   end
 
-  scenario 'creating a case that does not need clearance', :js do
-    cases_show_page.load(id: offender_sar_case.id)
+  context 'manager can choose a template and view the rendered letter' do
+    scenario 'when the case has just been created' do
+      cases_show_page.load(id: offender_sar_case.id)
+      expect(cases_show_page).to be_displayed
+      expect(cases_show_page).to have_content "Data to be requested"
+      expect(cases_show_page).to have_content "Send acknowledgement letter"
+    end
 
-    expect(cases_show_page).to be_displayed
-    expect(cases_show_page).to have_content "Mark as ready for vetting"
-    click_on "Send acknowledgement letter"
+    scenario 'and when a case is in "waiting for data" status', :js do
+      cases_show_page.load(id: offender_sar_case_waiting.id)
 
-    expect(cases_new_letter_page).to be_displayed
+      expect(cases_show_page).to be_displayed
+      expect(cases_show_page).to have_content "Mark as ready for vetting"
+      expect(cases_show_page).to have_content "Send acknowledgement letter"
+      click_on "Send acknowledgement letter"
 
-    cases_new_letter_page.new_letter.first_option.click
-    click_on "Continue"
+      expect(cases_new_letter_page).to be_displayed
 
-    expect(cases_show_letter_page).to be_displayed
-    expect(cases_show_letter_page).to have_content "Thank you for your offender subject access request, Bob"
+      cases_new_letter_page.new_letter.first_option.click
+      click_on "Continue"
+
+      expect(cases_show_letter_page).to be_displayed
+      expect(cases_show_letter_page).to have_content "Thank you for your offender subject access request, Bob"
+    end
   end
 end
