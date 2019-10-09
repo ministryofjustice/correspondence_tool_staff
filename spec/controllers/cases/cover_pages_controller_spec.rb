aae761659e767088c158a4955c0ac63097eee129
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-10-09 14:49:22 +0100

    CT-2494 generate cover page (#931)
    Manager can generate printable cover page from case in "Waiting for data" state, containing the list of data requests, space to write in vetting status, and final deadline

---
+++ spec/controllers/cases/cover_pages_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Cases::CoverPagesController, type: :controller do
+  let(:manager) { find_or_create :branston_user }
+  let(:offender_sar_case) { create :offender_sar_case }
+
+  before do
+    sign_in manager
+  end
+
+  describe '#show' do
+    before do
+      get :show, params: { case_id: offender_sar_case.id }
+    end
+
+    it 'sets @case' do
+      expect(assigns(:case)).to eq offender_sar_case
+    end
+  end
+end
