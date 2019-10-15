41c1a4984ed5c4cacf565938ed3e8613ffa208cb
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-10-15 10:55:08 +0100

    CT-2425 Alter tests for readability & formatting

--- spec/features/common/homepage_navigation.spec.rb
+++ spec/features/common/homepage_navigation.spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "Top level global navigation" do
+feature 'top level global navigation' do
   let(:responder)                 { find_or_create(:foi_responder) }
   let(:manager)                   { create(:manager)  }
   let(:managing_team)             { create :managing_team, managers: [manager] }
@@ -18,15 +18,11 @@ feature "Top level global navigation" do
       login_as manager
     end
 
-    scenario "Home page should have navigation" do
+    scenario 'case pages should have nav entries for all pages' do
       open_cases_page.load
-      expect(open_cases_page).to have_homepage_navigation
       expect(open_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/open'
-    end
 
-    scenario "case pages should have nav entries for all pages" do
-      open_cases_page.load
       nav_links = open_cases_page.homepage_navigation.all_links
       expect(nav_links.count).to eq 3
       expect(nav_links[0]).to have_text('All open cases')
@@ -40,82 +36,72 @@ feature "Top level global navigation" do
       login_as disclosure_specialist
     end
 
-    scenario "incoming case pages has nav entries" do
+    scenario 'incoming case pages has nav entries' do
       incoming_cases_page.load
       expect(incoming_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/incoming'
+
       nav_links = incoming_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
 
-    scenario "open all cases page has nav entries" do
+    scenario 'open all cases page has nav entries' do
       open_cases_page.load
       expect(open_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/open'
+
       nav_links = open_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
 
-    scenario "open my cases page has nav entries" do
+    scenario 'open my cases page has nav entries' do
       my_open_cases_page.load
       expect(open_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/my_open/in_time'
+
       nav_links = open_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
 
-    scenario "open closed cases page has nav entries" do
+    scenario 'open closed cases page has nav entries' do
       closed_cases_page.load
       expect(open_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/closed'
+
       nav_links = open_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
   end
 
-  context 'as a disclosure specialist / bmt' do
+  context 'as a disclosure specialist bmt' do
     background do
       login_as disclosure_specialist_bmt
     end
 
-    scenario "incoming case pages has nav entries" do
+    scenario 'incoming case pages has nav entries' do
       incoming_cases_page.load
       expect(incoming_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/incoming'
+
       nav_links = incoming_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
 
-    scenario "open in-time page has nav entries" do
+    scenario 'open in-time page has nav entries' do
       open_cases_page.load
       expect(open_cases_page.homepage_navigation.active_link[:href])
         .to eq '/cases/open'
+
       nav_links = open_cases_page.homepage_navigation.all_links
-      expect(nav_links.count).to eq 4
-      expect(nav_links[0]).to have_text('New cases')
-      expect(nav_links[1]).to have_text('All open cases')
-      expect(nav_links[2]).to have_text('My open cases')
-      expect(nav_links[3]).to have_text('Closed cases')
+      has_all_nav_links?(nav_links)
     end
+  end
+
+  def has_all_nav_links?(nav_links)
+    expect(nav_links.count).to eq 4
+    expect(nav_links[0]).to have_text('New cases')
+    expect(nav_links[1]).to have_text('All open cases')
+    expect(nav_links[2]).to have_text('My open cases')
+    expect(nav_links[3]).to have_text('Closed cases')
   end
 end
