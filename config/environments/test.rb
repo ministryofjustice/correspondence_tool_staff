c350db4558c5e1d0957975eae1824487d0283dea
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-10-29 22:07:43 +0000

    CT-2450 Disable unhelpful Bullet checking for STI association

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,15 @@ Rails.application.configure do
     Bullet.add_whitelist :type => :n_plus_one_query,
                          :class_name => 'BusinessUnit',
                          :association => :correspondence_types
+    Bullet.add_whitelist :type => :n_plus_one_query,
+                         :class_name => 'BusinessGroup',
+                         :association => :moved_to_unit
+    Bullet.add_whitelist :type => :n_plus_one_query,
+                         :class_name => 'Directorate',
+                         :association => :moved_to_unit
+    Bullet.add_whitelist :type => :n_plus_one_query,
+                         :class_name => 'BusinessUnit',
+                         :association => :moved_to_unit
 
     # Enable this to track down most of the N+1 query issues
     Bullet.raise         = true # raise an error if n+1 query occurs
