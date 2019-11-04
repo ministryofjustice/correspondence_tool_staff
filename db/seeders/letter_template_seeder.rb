e78b4dbfbf6f66fe7efda2f26ec3c7752cadac76
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2019-11-04 23:26:41 +0000

    Update letter template seeder `rake db:seed:letter_templates`

--- db/seeders/letter_template_seeder.rb
+++ db/seeders/letter_template_seeder.rb
@@ -9,35 +9,19 @@ class LetterTemplateSeeder
                 abbreviation: 'solicitor-acknowledgement',
                 template_type: 'acknowledgement',
                 body: <<~EOF
-                  <%= values.name %>
-                  <%= values.third_party_company_name %>
-                  <%= values.postal_address %>
-
-                  Your Reference: <%= values.third_party_reference %>
-
-                  Our Reference:  DPA <%= values.number %>
-
-                  Date: <%= Date.today.strftime('%e %b %Y') %>
-
-                  DATA PROTECTION ACT 2018:  SUBJECT ACCESS REQUEST
-                  <%= values.subject_full_name&.upcase %>-<%= values.prison_number&.upcase%>
-
-                  Dear Sirs
-
-                  Thank you for your subject access request (SAR) dated <%= values.received_date.strftime('%e %b %Y') %>.
-
-                  Please note that as your client has previously submitted a SAR and has been supplied with data, we will only be providing you with data not previously received, i.e. data from the date of the previous request. The previous request was started on [INSERT DATE PREVIOUS REQUEST RECEIVED].
-
-                  To complete the SAR, we will have to identify information from a number of business areas, including establishments. Because of this, there is very little information we can provide in response to enquiries. It would be helpful if you could limit correspondence during this period.
-
-                  Please note, if you have requested medical information as part of the request, responsibility for providing medical information has transferred to the National Health Service / Clinical Commissioning Groups. If you require medical data please contact the Healthcare Services team at the establishment.
-
-                  Yours sincerely
-
-
-                  Application Team
-                  Data Protection Compliance Team
-                  Ministry of Justice
+                  <p><strong>DATA PROTECTION ACT 2018:  SUBJECT ACCESS REQUEST
+                  <br><%= values.subject_full_name&.upcase %>-<%= values.prison_number&.upcase %></strong>
+                  <br>
+                  <br>Dear Sirs
+                  <br>
+                  <br>Thank you for your subject access request (SAR) dated <%= values.received_date.strftime('%e %b %Y') %>.
+                  <br>
+                  <br>Please note that as your client has previously submitted a SAR and has been supplied with data, we will only be providing you with data not previously received, i.e. data from the date of the previous request. The previous request was started on [INSERT DATE PREVIOUS REQUEST RECEIVED].
+                  <br>
+                  <br>To complete the SAR, we will have to identify information from a number of business areas, including establishments. <strong>Because of this, there is very little information we can provide in response to enquiries. It would be helpful if you could limit correspondence during this period.</strong>
+                  <br>
+                  <br>Please note, if you have requested medical information as part of the request, responsibility for providing medical information has transferred to the National Health Service / Clinical Commissioning Groups. If you require medical data please contact the Healthcare Services team at the establishment.
+                  </p>
                 EOF
                 )
 
