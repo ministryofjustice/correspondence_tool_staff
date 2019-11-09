448473ef7752d48756fb4e80300e21411e433e9f
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-09 12:04:40 +0000

    CT-2154 Ensure docker image is rebuilr prior to deployment

--- cloud-platform-deploy.sh
+++ cloud-platform-deploy.sh
@@ -7,6 +7,8 @@ $(aws ecr get-login --no-include-email --region eu-west-2) && \
 echo 'Begin environment upload' && \
 echo '----------------------------' && \
 echo '' && \
+echo 'Build image' && \
+docker build -t correspondence/track-a-query-ecr . && \
 docker tag correspondence/track-a-query-ecr:latest 754256621582.dkr.ecr.eu-west-2.amazonaws.com/correspondence/track-a-query-ecr:latest && \
 docker push 754256621582.dkr.ecr.eu-west-2.amazonaws.com/correspondence/track-a-query-ecr:latest && \
 kubectl delete --filename config/kubernetes/$1 --namespace track-a-query-$1 && \
