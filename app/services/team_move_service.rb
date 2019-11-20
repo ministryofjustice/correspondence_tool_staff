e7534977cf2d2c893a3450d47fb41bb5575755cd
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2019-11-20 16:13:21 +0000

    CT-2589 Review updates (recommended)

--- app/services/team_move_service.rb
+++ app/services/team_move_service.rb
@@ -1,17 +1,19 @@
 class TeamMoveService
   class OriginalDirectorateError < RuntimeError
     def initialize
-      super("Cannot move to the original directorate")
+      super("Cannot move to the original Directorate")
     end
   end
+
   class InvalidDirectorateError < RuntimeError
     def initialize
-      super("Cannot move a Business Unit to a team that is not a directorate")
+      super("Cannot move a Business Unit to a team that is not a Directorate")
     end
   end
+
   class TeamNotBusinessUnitError < RuntimeError
     def initialize
-      super("Cannot move a team which is not a business unit")
+      super("Cannot move a team which is not a Business Unit")
     end
   end
 
@@ -21,33 +23,36 @@ class TeamMoveService
     @team = team
     @directorate = directorate
     @result = :incomplete
-    raise TeamNotBusinessUnitError.new if @team.type != 'BusinessUnit'
-    raise InvalidDirectorateError.new if @directorate.type != 'Directorate'
-    raise OriginalDirectorateError.new if @directorate == team.directorate
+
+    raise TeamNotBusinessUnitError.new unless @team.is_a? BusinessUnit
+    raise InvalidDirectorateError.new unless @directorate.is_a? Directorate
+    raise OriginalDirectorateError.new if directorate == team.directorate
   end
 
   def call
-    ActiveRecord::Base.transaction do
-      begin
-        move_team
+    begin
+      ActiveRecord::Base.transaction do
+        move_team!
         @result = :ok
-      rescue
-        @team.reload
-        @result = :error
       end
+    rescue
+      @team.reload
+      @result = :error
     end
   end
 
   private
 
-  def move_team
+  def move_team!
     @new_team = @team.dup
+
     @new_team.directorate = @directorate
     @new_team.name << " (Moved from #{@team.directorate.name})"
     @new_team.correspondence_type_roles = @team.correspondence_type_roles
     @new_team.properties = @team.properties
     @new_team.user_roles = @team.user_roles
+
     @team.destroy_related_user_roles!
-    @new_team.save
+    @new_team.save!
   end
 end
--- spec/services/team_move_service_spec.rb
+++ spec/services/team_move_service_spec.rb
@@ -1,10 +1,7 @@
-
 require 'rails_helper'
 
 describe TeamMoveService do
-
   describe '#call' do
-
     context 'moving a business unit to another directorate' do
       let(:original_dir)     { find_or_create :directorate }
       let(:bu)               { find_or_create(:business_unit, name: 'Business Unit name', directorate: original_dir) }
@@ -14,25 +11,29 @@ describe TeamMoveService do
         expect{
           TeamMoveService.new(original_dir, target_dir)
         }.to raise_error TeamMoveService::TeamNotBusinessUnitError,
-                         "Cannot move a team which is not a business unit"
+                         "Cannot move a team which is not a Business Unit"
       end
+
       it 'returns error when the target directorate is not a directorate' do
         expect{
           TeamMoveService.new( bu, bu )
         }.to raise_error TeamMoveService::InvalidDirectorateError,
-                         "Cannot move a Business Unit to a team that is not a directorate"
+                         "Cannot move a Business Unit to a team that is not a Directorate"
       end
+
       it 'returns error for the original directorate' do
         expect{
           TeamMoveService.new( bu, original_dir)
         }.to raise_error TeamMoveService::OriginalDirectorateError,
-                         "Cannot move to the original directorate"
+                         "Cannot move to the original Directorate"
       end
+
       it 'Creates a copy of the team in the target directorate' do
         @service = TeamMoveService.new( bu, target_dir)
         @service.call
         expect(@service.new_team.directorate).to eq target_dir
       end
+
       it 'moves team user_roles to the new team, and removes them from the original team' do
         team_user_role = TeamsUsersRole.create(
           user: create(:user),
@@ -45,9 +46,7 @@ describe TeamMoveService do
         expect(bu.user_roles.first).to eq nil
       end
 
-      # original team must be  deactivated
-      # ^^ Both to be covered in a later ticket
-
+      # TODO: original team must be deactivated, to be covered in a later ticket
     end
   end
 end
