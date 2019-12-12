446e30d91deaef03479a28ef6465d22d5e585141
danielatdigital <danielatdigital@users.noreply.github.com>
2019-12-12 15:36:12 +0000

    Create team join service
    To add one teams open cases and users to another team
    Deactivating the initial team

---
+++ spec/services/team_join_service_spec.rb
@@ -0,0 +1,147 @@
+require 'rails_helper'
+
+describe TeamJoinService do
+  let(:original_dir) { find_or_create :directorate }
+  let(:target_dir) { find_or_create :directorate }
+  let(:responder) { create(:foi_responder, responding_teams: [business_unit]) }
+  let(:service) { TeamJoinService.new(business_unit, target_business_unit) }
+  let(:business_unit) {
+    find_or_create(
+      :business_unit,
+      name: 'Business Unit name',
+      directorate: original_dir,
+      code: 'ABC'
+    )
+  }
+  let(:target_business_unit) {
+    find_or_create(
+      :business_unit,
+      name: 'Target Business Unit name',
+      directorate: target_dir,
+      code: 'XYZ'
+    )
+  }
+  let!(:kase) {
+    create(
+      :case_being_drafted,
+      responding_team: business_unit,
+      responder: responder
+    )
+  }
+  let!(:klosed_kase) {
+    create(
+      :closed_case,
+      responding_team: business_unit,
+      responder: responder
+    )
+  }
+
+
+  describe '#initialize' do
+    it 'returns error when the team is not a business unit' do
+      expect{
+        TeamJoinService.new(original_dir, target_business_unit)
+      }.to raise_error TeamJoinService::TeamNotBusinessUnitError,
+                       "Cannot move a team which is not a Business Unit"
+    end
+
+    it 'returns error when the target team is not a team' do
+      expect{
+        TeamJoinService.new(business_unit, original_dir)
+      }.to raise_error TeamJoinService::InvalidTargetBusinessUnitError,
+                       "Cannot join a Business Unit to a team that is not a Business Unit"
+    end
+
+    it 'returns error for the original team' do
+      expect{
+        TeamJoinService.new(business_unit, business_unit)
+      }.to raise_error TeamJoinService::OriginalBusinessUnitError,
+                       "Cannot join a Business Unit to itself"
+    end
+
+    it 'requires a business unit and a target business unit' do
+      expect(service.instance_variable_get(:@team)).to eq business_unit
+      expect(service.instance_variable_get(:@target_team)).to eq target_business_unit
+      expect(service.instance_variable_get(:@result)).to eq :incomplete
+    end
+  end
+
+  describe '#call' do
+    context 'joining a business unit into another business unit' do
+      it 'Joins team users to the new team, and removes them from the original team' do
+        expect(business_unit.users).to match_array [responder]
+        service.call
+
+        expect(service.target_team.users).to match_array [responder]
+        expect(business_unit.users).to be_empty
+      end
+
+      it 'Joins team user_roles to the new team, and removes them from the original team' do
+        team_user_role = business_unit.user_roles.first
+        service.call
+
+        expect(service.target_team.user_roles.first).to eq team_user_role
+        expect(business_unit.reload.user_roles).to be_empty
+      end
+
+      it 'sets old team to deleted' do
+        service.call
+
+        expect(business_unit.reload.deleted_at).not_to be_nil
+      end
+
+      it 'sets moved to on old team' do
+        service.call
+        expect(service.target_team).to eq business_unit.moved_to_unit
+      end
+
+      context 'when the team being moved has open cases' do
+        it 'Joins open cases to the new team and removes them from the original team' do
+          expect(business_unit.open_cases.first).to eq kase
+          service.call
+
+          expect(business_unit.open_cases).to be_empty
+          expect(service.target_team.open_cases.first).to eq kase
+
+          #TODO Maybe create a test for the target team, where the target team already has cases ?
+        end
+
+        it 'Joins all transitions of the open case to the new team' do
+          service.call
+
+          # using factory :case_being_drafted, the case has TWO transitions,
+          # One for the transition to drafted,
+          # and one for the current _being drafted_ state (in the second, the target team is nill)
+          expect(kase.transitions.first.target_team_id).to eq service.target_team.id
+          expect(kase.transitions.second.acting_team_id).to eq service.target_team.id
+        end
+      end
+
+      context 'when the team being moved has closed cases' do
+        it 'leaves closed cases with the original team' do
+          expect(business_unit.cases.closed.first).to eq klosed_kase
+          service.call
+
+          expect(service.target_team.cases.closed).to be_empty
+          expect(business_unit.cases.closed.first).to eq klosed_kase
+        end
+      end
+
+      context 'when the team being moved has trigger cases' do
+        let(:kase) {
+          create(
+            :case_being_drafted, :flagged,
+            responding_team: business_unit,
+            responder: responder
+          )
+        }
+        it 'leaves the approving teams as Disclosure' do
+          expect(kase.approving_teams).to eq [BusinessUnit.dacu_disclosure]
+          service.call
+
+          expect(kase.reload.approving_teams).to eq [BusinessUnit.dacu_disclosure]
+        end
+      end
+    end
+  end
+end
