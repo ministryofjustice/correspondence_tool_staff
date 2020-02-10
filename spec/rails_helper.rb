9016b0ff6241c46a0957fdf84756ddeb94c7f9fe
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-02-10 13:45:57 +0000

    test specific require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,6 +76,9 @@ Capybara.server = :puma, { Silent: true }
 # Timecop.freeze do...end blocks
 Timecop.safe_mode = true
 
+# include missing module because of load order issues on CI
+require Rails.root.join('spec/support/features/interactions/overturned_ico.rb')
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
