7fe80d768028e84fe506f84001c67714baf10a9c
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-02-10 09:53:31 +0000

    log out the webdrivers versions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -102,5 +102,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  Webdrivers.logger.level = :DEBUG
 end
