63d0d018a34c0154e82ee2975fc66e3d018dc2fa
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-03-25 10:27:49 +0000

    teams controller action for displaying form

--- spec/site_prism/page_objects/pages/teams/join_team_form_page.rb
+++ spec/site_prism/page_objects/pages/teams/join_team_form_page.rb
@@ -13,16 +13,7 @@ module PageObjects
         element :heading, '.page-heading--primary'
         element :subhead, '.page-heading--secondary'
 
-        section :business_groups, '.business-groups' do
-          elements :links, 'li a'
-        end
 
-        section :directorates_list, 'ul.teams' do
-          sections :directorates, 'li.team' do
-            element :directorate_details, 'div.team-details'
-            element :move_to_directorate_link, 'div.team-actions a'
-          end
-        end
       end
     end
   end
