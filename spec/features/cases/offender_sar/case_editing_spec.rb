d66c7955ba44345b2ba1d61948362c17083c79b6
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-04-21 17:57:22 +0100

    editing spec wip

--- spec/features/cases/offender_sar/case_editing_spec.rb
+++ spec/features/cases/offender_sar/case_editing_spec.rb
@@ -14,5 +14,13 @@ feature 'Offender SAR Case editing by a manager' do
   scenario 'creating a case that does not need clearance', js: true do
     expect(cases_show_page).to be_displayed(id: offender_sar_case.id)
 
+    cases_show_page.offender_sar_subject_details.change_link.click
+    expect(cases_edit_offender_sar_subject_details_page).to be_displayed
+    cases_edit_offender_sar_subject_details_page.edit_name 'Bob Hope'
+    click_on "Continue"
+    expect(cases_show_page).to be_displayed(id: offender_sar_case.id)
+    expect(cases_show_page).to have_content 'Bob Hope'
+    expect(cases_show_page).to have_content 'Case edited successfully'
+
   end
 end
