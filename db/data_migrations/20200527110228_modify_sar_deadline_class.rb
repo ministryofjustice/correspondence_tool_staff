1c8f7430ebbee9465f0bd537cb782c233a7bd3d6
vampireinsun <yikang.mao@yahoo.com>
2020-05-27 16:43:44 +0100

    removed the code for debug purpose

--- db/data_migrations/20200527110228_modify_sar_deadline_class.rb
+++ db/data_migrations/20200527110228_modify_sar_deadline_class.rb
@@ -1,7 +1,6 @@
 class ModifySarDeadlineClass < ActiveRecord::DataMigration
   def up
     CorrespondenceType.all.each do |ct|
-      byebug
       if ct.abbreviation == 'SAR' || ct.abbreviation == 'OFFENDER_SAR'
         ct.deadline_calculator_class = 'CalendarMonth'
         ct.save!
