64431187c19d7e9a834a7b0f409b18d02a7778ad
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-07-24 12:34:43 +0100

    Added the test sprec for form back

--- spec/decorators/case/offender_sar_decorator_spec.rb
+++ spec/decorators/case/offender_sar_decorator_spec.rb
@@ -7,12 +7,6 @@ describe Case::SAR::OffenderDecorator do
     expect(Case::SAR::Offender.new.decorate).to be_instance_of Case::SAR::OffenderDecorator
   end
 
-  describe "#steps" do
-    it "returns the list of steps" do
-      expect(offender_sar_case.steps).to eq ["subject-details", "requester-details", "recipient-details", "requested-info", "request-details", "date-received"]
-    end
-  end
-
   describe "#current_step" do
     it "returns the first step by default" do
       expect(offender_sar_case.current_step).to eq "subject-details"
@@ -44,42 +38,6 @@ describe Case::SAR::OffenderDecorator do
       expect(offender_sar_case.get_step_partial).to eq "request_details_step"
       offender_sar_case.next_step
       expect(offender_sar_case.get_step_partial).to eq "date_received_step"
-    end
-  end
-
-  describe "#valid_attributes?" do
-    context "when params is empty" do
-      params = ActionController::Parameters.new({}).permit!
-
-      it "returns false" do
-        expect(offender_sar_case.valid_attributes?(params)).to be false
-      end
-    end
-
-    context "when params is nil" do
-      let(:params) { nil }
-
-      it "returns false" do
-        expect(offender_sar_case.valid_attributes?(params)).to be false
-      end
-    end
-
-    context "when params is set and not valid" do
-      let(:params) { ActionController::Parameters.new(
-        offender_sar_case: { name: "", email: "notanemail" }
-      ).require(:offender_sar_case).permit(:name, :email)}
-
-      context "and the form model has the values merged" do
-        it "returns false" do
-          expect(offender_sar_case.valid_attributes?(params)).to be false
-        end
-      end
-
-      context "and the form model does not have the values merged" do
-        it "returns false" do
-          expect(offender_sar_case.valid_attributes?(params)).to be false
-        end
-      end
     end
   end
 
---
+++ spec/form_models/offender_sar_case_form_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe OffenderSARCaseForm do
+  let(:case_form) {create(:offender_sar_case).decorate }
+
+  it 'can be created' do
+    expect(case_form).to be_an_instance_of Case::SAR::Offender
+  end
+
+  describe "#steps" do
+    it "returns the list of steps" do
+      expect(case_form.steps).to eq ["subject-details", "requester-details", "recipient-details", "requested-info", "request-details", "date-received"]
+    end
+  end
+
+  describe "#valid_attributes?" do
+    context "when params is empty" do
+      params = ActionController::Parameters.new({}).permit!
+
+      it "returns false" do
+        expect(case_form.valid_attributes?(params)).to be false
+      end
+    end
+
+    context "when params is nil" do
+      let(:params) { nil }
+
+      it "returns false" do
+        expect(case_form.valid_attributes?(params)).to be false
+      end
+    end
+
+    context "when params is set and not valid" do
+      let(:params) { ActionController::Parameters.new(
+        case_form: { name: "", postal_address: "address" }
+      ).require(:case_form).permit(:name, :postal_address)}
+
+      context "and the form model has the values merged" do
+        it "returns false" do
+          expect(case_form.valid_attributes?(params)).to be false
+        end
+      end
+
+      context "and the form model does not have the values merged" do
+        it "returns false" do
+          expect(case_form.valid_attributes?(params)).to be false
+        end
+      end
+    end
+  end
+end
