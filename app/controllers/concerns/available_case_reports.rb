6367b9bc8062e379c51bcbb6b88c2f72f369930a
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-08-10 17:06:15 +0100

    fixed the typo

--- app/controllers/concerns/available_case_reports.rb
+++ app/controllers/concerns/available_case_reports.rb
@@ -1,12 +1,12 @@
 module AvailableCaseReports
   extend ActiveSupport::Concern
   included do
-    before_action -> { get_avaliable_reports }
+    before_action -> { get_available_reports }
   end
 
   private 
 
-  def get_avaliable_reports
+  def get_available_reports
     if current_user
       @available_reports = Pundit.policy_scope(current_user, get_cases_reports)
     else
