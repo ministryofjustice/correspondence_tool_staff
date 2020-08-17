46cfbbacd1ea31a1350da8d57180d1edfeb67422
willmcb <william.mcbrien@digital.justice.gov.uk>
2020-08-17 14:19:41 +0100

    Add basic failing feature test

--- spec/features/cases/offender_sar/case_editing_spec.rb
+++ spec/features/cases/offender_sar/case_editing_spec.rb
@@ -62,6 +62,53 @@ feature 'Offender SAR Case editing by a manager' do
     then_i_expect_the_new_date_to_be_reflected_on_the_case_show_page
   end
 
+  fscenario 'user can edit/update exempt and pages for dispatch counts' do
+    expect(cases_show_page).to be_displayed(id: offender_sar_case.id)
+
+    when_i_record_some_data_requests
+    and_i_update_the_exempt_pages_count
+    then_i_should_see_the_updated_exempt_page_count_on_the_show_page
+
+    when_i_update_the_pages_for_dispatch 
+    then_i_should_see_the_pages_for_dispatch_reflected_on_the_show_page
+  end
+
+  def when_i_record_some_data_requests
+    click_on 'Record data request'
+    fill_in 'case_data_requests_attributes_0_location', with: 'North East Antartica'
+    fill_in 'case_data_requests_attributes_0_data', with: 'Data on quality of snow'
+    fill_in 'case_data_requests_attributes_1_location', with: 'South East Antartica'
+    fill_in 'case_data_requests_attributes_1_data', with: 'Data on number of penguins'
+    click_on 'Record requests'
+    expect(page).to have_content('Data request successfully recorded')
+    expect(page).to have_content('North East Antartica')
+    expect(page).to have_content('South East Antartica')
+  end
+
+  def and_i_update_the_exempt_pages_count
+    click_on 'Update exempt pages'
+    expect(page).to have_content('Update exempt pages')
+    fill_in 'number_of_pages', with: '1541'
+    click_on 'Continue'
+  end
+
+  def when_i_update_the_pages_for_dispatch
+    click_on 'Update pages for dispatch'
+    expect(page).to have_content('Update pages for dispatch')
+    fill_in 'number_of_pages', with: '1308'
+    click_on 'Continue'
+  end
+
+  def then_i_should_see_the_updated_exempt_page_count_on_the_show_page
+    expect(page).to have_content('1541')
+    expect(page).to have_content('Exempt page count successfully updated')
+  end
+
+  def then_i_should_see_the_pages_for_dispatch_reflected_on_the_show_page 
+    expect(page).to have_content('1308')
+    expect(page).to have_content('Pages for dispatch successfully updated')
+  end
+
   def when_i_progress_case_to_a_closed_state
     click_on "Mark as waiting for data"
     click_on "Mark as ready for vetting"
