4e9ff32bf75008f57a9bec129d363ee810e95ece
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2020-08-26 16:21:48 +0100

    CT-3027 Remove extra blank lines

--- spec/support/features/steps/cases/responding_steps.rb
+++ spec/support/features/steps/cases/responding_steps.rb
@@ -23,11 +23,8 @@ def mark_case_as_sent_step(responded_date:,
                            expected_status:,
                            expected_to_be_with:)
   cases_show_page.actions.mark_as_sent.click
-
   cases_respond_page.fill_in_date_responded(responded_date)
-
   cases_respond_page.submit_button.click
-
   expect(open_cases_page)
     .to have_content("The response has been marked as sent.")
   expect(cases_show_page.case_status.details.who_its_with.text)
@@ -37,33 +34,21 @@ end
 
 def close_case_step(responded_date: Date.today)
   cases_show_page.actions.close_case.click
-
   cases_close_page.fill_in_date_responded(responded_date)
-
   cases_close_page.click_on 'Continue'
-
   expect(cases_closure_outcomes_page).to be_displayed
-
   cases_closure_outcomes_page.is_info_held.yes.click
-
   cases_closure_outcomes_page.wait_until_outcome_visible
-
   cases_closure_outcomes_page.outcome.granted_in_full.click
-
   cases_closure_outcomes_page.submit_button.click
-
   expect(cases_show_page).to have_content("You've closed this case")
-
   expect(cases_show_page.case_status.details.copy.text).to eq "Closed"
 end
 
 def close_sar_case_step(timeliness: 'in time', tmm: false, editable: true)
   cases_show_page.actions.close_case.click
-
   cases_close_page.fill_in_date_responded(Date.today)
-
   cases_close_page.click_on 'Continue'
-
   expect(cases_closure_outcomes_page).to be_displayed
 
   if tmm
@@ -73,20 +58,19 @@ def close_sar_case_step(timeliness: 'in time', tmm: false, editable: true)
   end
 
   cases_close_page.click_on 'Close case'
-
   show_page = cases_show_page.case_details
+
   if editable
     expect(cases_show_page.notice.text).to eq("You've closed this case. Edit case closure details")
   else
     expect(cases_show_page.notice.text).to eq("You've closed this case")
   end
 
-  expect(show_page.response_details.date_responded.data.text)
-    .to eq Date.today.strftime(Settings.default_date_format)
-  expect(show_page.response_details.timeliness.data.text)
-    .to eq "Answered #{timeliness}"
+  expect(show_page.response_details.date_responded.data.text).to eq Date.today.strftime(Settings.default_date_format)
+  expect(show_page.response_details.timeliness.data.text).to eq "Answered #{timeliness}"
 
   # Regex required to handle both cases of 1 or more days to respond
+
   if show_page.has_css?('.overturned-sar-basic-details')
     expect(show_page.response_details.time_taken.data.text).to match(/\d+ working day[s{1}]?/)
   else
@@ -94,8 +78,7 @@ def close_sar_case_step(timeliness: 'in time', tmm: false, editable: true)
   end
 
   if tmm
-    expect(show_page.response_details.refusal_reason.data.text)
-      .to eq 'SAR Clarification/Tell Me More'
+    expect(show_page.response_details.refusal_reason.data.text).to eq 'SAR Clarification/Tell Me More'
   else
     expect(show_page.response_details).to have_no_refusal_reason
   end
@@ -104,11 +87,8 @@ end
 
 def close_ico_appeal_case_step(timeliness: 'in time', decision: 'upheld')
   cases_show_page.actions.close_case.click
-
   expect(cases_close_page).to be_displayed
-
   cases_close_page.fill_in_ico_date_responded(Date.today)
-
   cases_close_page.click_on 'Continue'
 
   if decision == 'upheld'
@@ -121,7 +101,6 @@ def close_ico_appeal_case_step(timeliness: 'in time', decision: 'upheld')
 
   show_page = cases_show_page.case_details
   expect(cases_show_page.notice.text).to eq("You've closed this case")
-
   expect(show_page.response_details.date_responded.data.text)
     .to eq Date.today.strftime(Settings.default_date_format)
   expect(show_page.response_details.timeliness.data.text)
