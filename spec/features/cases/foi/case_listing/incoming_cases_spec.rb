67567cdddc22770c06ee34306cc9beddfd08a25c
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-08-30 09:09:31 +0100

    CT-3044 Fix date issue with incoming cases spec

--- spec/features/cases/foi/case_listing/incoming_cases_spec.rb
+++ spec/features/cases/foi/case_listing/incoming_cases_spec.rb
@@ -5,44 +5,62 @@ feature 'listing incoming on the system' do
   given(:press_officer) { find_or_create :press_officer }
   given(:private_officer) { find_or_create :private_officer }
 
-  given(:assigned_case) { create :assigned_case,
-                                 created_at: 1.business_days.ago,
-                                 identifier: 'assigned_case' }
-  given(:fresh_assigned_case) { create :assigned_case,
-                                       identifier: 'fresh_assigned_case' }
+  given(:assigned_case) do
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
+           created_at: 1.business_days.ago,
+           identifier: 'assigned_case'
+    end
+  end
+  given(:fresh_assigned_case) do
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
+           identifier: 'fresh_assigned_case'
+    end
+  end
   given(:assigned_case_flagged_for_dacu_disclosure) do
-    create :assigned_case,
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
            :flagged,
            created_at: 2.business_days.ago,
            identifier: 'assigned_case_flagged_for_dacu_disclosure'
+    end
   end
   given(:assigned_case_flagged_for_dacu_disclosure_accepted) do
-    create :assigned_case,
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
            :flagged_accepted,
            created_at: 2.business_days.ago,
            identifier: 'assigned_case_flagged_for_dacu_disclosure_accepted'
+    end
   end
   given(:assigned_case_flagged_for_press_office_accepted) do
-    create :assigned_case,
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
            :flagged_accepted,
            :press_office,
            created_at: 2.business_days.ago,
            identifier: 'assigned_case_flagged_for_press_office_accepted'
+    end
   end
   given(:assigned_cr_case_flagged_for_press_office_accepted) do
-    create :awaiting_responder_compliance_review,
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :awaiting_responder_compliance_review,
            :flagged_accepted,
            :press_office,
            created_at: 2.business_days.ago,
            identifier: 'assigned_cr_case_flagged_for_press_office_accepted'
+    end
   end
 
   given(:assigned_case_flagged_for_private_office_accepted) do
-    create :assigned_case,
+    Timecop.freeze(Date.new(2020, 8, 19)) do
+      create :assigned_case,
            :flagged_accepted,
            :private_office,
            created_at: 2.business_days.ago,
            identifier: 'assigned_case_flagged_for_private_office_accepted'
+    end
   end
 
   context 'with cases setup for dacu disclosure' do
@@ -67,9 +85,13 @@ feature 'listing incoming on the system' do
   end
 
   context 'with cases setup for press office' do
-    given(:too_old_assigned_case) { create :assigned_case,
-                                           created_at: 4.business_days.ago,
-                                           identifier: 'too_old_assigned_case' }
+    given(:too_old_assigned_case) do
+      Timecop.freeze(Date.new(2020, 8, 19)) do
+        create(:assigned_case,
+          created_at: 4.business_days.ago,
+          identifier: 'too_old_assigned_case')
+      end
+    end
 
     background do
       too_old_assigned_case
@@ -81,23 +103,29 @@ feature 'listing incoming on the system' do
     end
 
     scenario 'for press office' do
-      login_as press_officer
+      Timecop.freeze(Date.new(2020, 8, 19)) do
+        login_as press_officer
 
-      visit '/cases/incoming'
+        visit '/cases/incoming'
 
-      cases = incoming_cases_page.case_list
+        cases = incoming_cases_page.case_list
 
-      expect(cases.count).to eq 2
-      expect(cases.first.number).to have_text assigned_case.number
-      expect(cases.second.number)
-        .to have_text assigned_case_flagged_for_dacu_disclosure.number
+        expect(cases.count).to eq 2
+        expect(cases.first.number).to have_text assigned_case.number
+        expect(cases.second.number)
+          .to have_text assigned_case_flagged_for_dacu_disclosure.number
+      end
     end
   end
 
   context 'with cases setup for private office' do
-    given(:too_old_assigned_case) { create :assigned_case,
-                                           created_at: 4.business_days.ago,
-                                           identifier: 'too_old_assigned_case' }
+    given(:too_old_assigned_case) do
+      Timecop.freeze(Date.new(2020, 8, 19)) do
+        create :assigned_case,
+               created_at: 4.business_days.ago,
+               identifier: 'too_old_assigned_case'
+      end
+    end
 
     background do
       too_old_assigned_case
@@ -108,16 +136,18 @@ feature 'listing incoming on the system' do
     end
 
     scenario 'for press office' do
-      login_as private_officer
+      Timecop.freeze(Date.new(2020, 8, 19)) do
+        login_as private_officer
 
-      visit '/cases/incoming'
+        visit '/cases/incoming'
 
-      cases = incoming_cases_page.case_list
+        cases = incoming_cases_page.case_list
 
-      expect(cases.count).to eq 2
-      expect(cases.first.number).to have_text assigned_case.number
-      expect(cases.second.number)
-          .to have_text assigned_case_flagged_for_dacu_disclosure.number
+        expect(cases.count).to eq 2
+        expect(cases.first.number).to have_text assigned_case.number
+        expect(cases.second.number)
+            .to have_text assigned_case_flagged_for_dacu_disclosure.number
+      end
     end
   end
 end
