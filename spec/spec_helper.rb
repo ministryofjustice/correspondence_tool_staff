f37eae4ef43c66b6ea7ec2733e2e51abc3051bf2
willmcb <william.mcbrien@digital.justice.gov.uk>
2020-09-04 11:12:41 +0100

    Add deferred garbage collection to application

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,15 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
+  # defer garbage collection to speed up test runs
+  config.before(:all) do
+    DeferredGarbageCollection.start
+  end
+
+  config.after(:all) do
+    DeferredGarbageCollection.reconsider
+  end
+
   # # Many RSpec users commonly either run the entire suite or an individual
   # # file, and it's useful to allow more verbose output when running an
   # # individual spec file.
