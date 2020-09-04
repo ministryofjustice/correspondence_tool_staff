065ca3ef7720410a0c3ef694b859d8ff449975e6
willmcb <william.mcbrien@digital.justice.gov.uk>
2020-09-04 16:38:28 +0100

    Add more fine-grained checks into feature spec

--- spec/features/cases/offender_sar/case_creating_spec.rb
+++ spec/features/cases/offender_sar/case_creating_spec.rb
@@ -18,7 +18,7 @@ feature 'Offender SAR Case creation by a manager', js: true do
     and_fill_in_requested_info_page
     and_fill_in_request_details_page
     and_fill_in_date_received_page
-    then_expect_cases_show_page_to_be_correct
+    then_expect_cases_show_page_to_be_correct_for_data_subject_requesting_own_record
     then_expect_open_cases_page_to_be_correct
   end
 
@@ -30,11 +30,11 @@ feature 'Offender SAR Case creation by a manager', js: true do
     and_fill_in_requested_info_page
     and_fill_in_request_details_page
     and_fill_in_date_received_page
-    then_expect_cases_show_page_to_be_correct
+    then_expect_cases_show_page_to_be_correct_for_data_subject_sending_data_to_third_party
     then_expect_open_cases_page_to_be_correct
   end
 
-  scenario '3 Solicitor requesting data subject record' do
+ scenario '3 Solicitor requesting data subject record' do
     when_i_navigate_to_offender_sar_subject_page
     and_fill_in_subject_details_page
     and_fill_in_requester_details_page(:third_party)
@@ -42,7 +42,7 @@ feature 'Offender SAR Case creation by a manager', js: true do
     and_fill_in_requested_info_page
     and_fill_in_request_details_page
     and_fill_in_date_received_page
-    then_expect_cases_show_page_to_be_correct
+    then_expect_cases_show_page_to_be_correct_for_solicitor_requesting_data_subject_record
     then_expect_open_cases_page_to_be_correct
   end
 
@@ -54,10 +54,38 @@ feature 'Offender SAR Case creation by a manager', js: true do
     and_fill_in_requested_info_page
     and_fill_in_request_details_page
     and_fill_in_date_received_page
-    then_expect_cases_show_page_to_be_correct
+    then_expect_cases_show_page_to_be_correct_for_solicitor_requesting_data_for_data_subject
     then_expect_open_cases_page_to_be_correct
   end
 
+  def then_expect_cases_show_page_to_be_correct_for_solicitor_requesting_data_for_data_subject
+    basic_details_of_show_page_are_correct
+    expect(cases_show_page).to have_content "Information requested on someone's behalf? Yes"
+    expect(cases_show_page).to have_content "Where should the data be sent? The data subject"
+    expect_to_have_correct_sending_address
+  end
+
+  def then_expect_cases_show_page_to_be_correct_for_solicitor_requesting_data_subject_record
+    basic_details_of_show_page_are_correct
+    expect(cases_show_page).to have_content "Information requested on someone's behalf? Yes"
+    expect(cases_show_page).to have_content "Where should the data be sent? Requester"
+    expect_to_have_correct_sending_address
+  end
+
+  def then_expect_cases_show_page_to_be_correct_for_data_subject_requesting_own_record
+    basic_details_of_show_page_are_correct
+
+    expect(cases_show_page).to have_content "Information requested on someone's behalf? No"
+    expect(cases_show_page).to have_content "Where should the data be sent? The data subject"
+  end
+
+  def then_expect_cases_show_page_to_be_correct_for_data_subject_sending_data_to_third_party
+    basic_details_of_show_page_are_correct
+
+    expect(cases_show_page).to have_content "Information requested on someone's behalf? No"
+    expect(cases_show_page).to have_content "Where should the data be sent? Third party"
+    expect_to_have_correct_sending_address
+  end
 
   def when_i_navigate_to_offender_sar_subject_page
     expect(cases_page).to have_new_case_button
@@ -103,17 +131,24 @@ feature 'Offender SAR Case creation by a manager', js: true do
     click_on "Continue"
   end
 
-  def then_expect_cases_show_page_to_be_correct
+  def basic_details_of_show_page_are_correct
     expect(cases_show_page).to be_displayed
     expect(cases_show_page).to have_content "Case created successfully"
     expect(cases_show_page.page_heading).to have_content "Sabrina Adams"
-    click_on "Cases"
   end
 
   def then_expect_open_cases_page_to_be_correct
+    click_on "Cases"
     expect(open_cases_page).to be_displayed
     expect(cases_page).to have_content "Branston Registry"
     expect(open_cases_page).to have_content "Sabrina Adams"
+  end
+
+  def expect_to_have_correct_sending_address
+    expect(cases_show_page).to have_content "Representative name Mr J. Smith"
+    expect(cases_show_page).to have_content "Company name Foogle and Sons Solicitors at Law"
+    expect(cases_show_page).to have_content "Relationship Solicitor"
+    expect(cases_show_page).to have_content "Address\n22 High Street"
   end
 end
 
