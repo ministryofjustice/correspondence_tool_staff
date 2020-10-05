4e35a32498311fe773e50d220b9a872fb361cca2
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-10-05 10:53:14 +0100

    trying to fix the error again

--- spec/services/user_active_case_count_service_spec.rb
+++ spec/services/user_active_case_count_service_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 describe UserActiveCaseCountService do
 
-  let!(:responding_team)   { find_or_create :foi_responding_team }
-  let!(:user_1)            { find_or_create :foi_responder }
-  let!(:user_2)            { create :foi_responder, identifier: 'foi responder 2' }
-  let!(:users)             { [ user_1, user_2] }
-  let!(:kase_1)           { create :accepted_case, responder: user_1 }
-  let!(:kase_2)           { create :accepted_case, responder: user_1 }
-  let!(:kase_3)           { create :accepted_case, responder: user_2 }
-  let!(:kase_4)           { create :closed_case, responder: user_2 }
+  let(:responding_team)   { create :foi_responding_team }
+  let(:user_1)            { create :foi_responder, identifier: 'foi responder 1'}
+  let(:user_2)            { create :foi_responder, identifier: 'foi responder 2' }
+  let(:users)             { [ user_1, user_2] }
+  # let(:kase_1)           { create :accepted_case, responder: user_1 }
+  # let(:kase_2)           { create :accepted_case, responder: user_1 }
+  # let(:kase_3)           { create :accepted_case, responder: user_2 }
+  # let(:kase_4)           { create :closed_case, responder: user_2 }
   let(:service)           { UserActiveCaseCountService.new }
 
   describe '#case_counts_by_user' do
     it 'returns number of cases indexed by user id' do
+      kase_1 = create :accepted_case, responder: user_1
+      kase_2 = create :accepted_case, responder: user_1
+      kase_3 = create :accepted_case, responder: user_2
+      kase_4 = create :closed_case, responder: user_2
       case_counts = service.case_counts_by_user(users)
       expect(case_counts).to eq(
            {
@@ -25,11 +29,19 @@ describe UserActiveCaseCountService do
 
   describe '#active_cases_for_user' do
     it 'returns active cases for user' do
+      kase_1 = create :accepted_case, responder: user_1
+      kase_2 = create :accepted_case, responder: user_1
+      kase_3 = create :accepted_case, responder: user_2
+      kase_4 = create :closed_case, responder: user_2
       expected = [ kase_1, kase_2 ]
       expect(service.active_cases_for_user(user_1)).to match_array expected
     end
 
     it "works with admins" do
+      kase_1 = create :accepted_case, responder: user_1
+      kase_2 = create :accepted_case, responder: user_1
+      kase_3 = create :accepted_case, responder: user_2
+      kase_4 = create :closed_case, responder: user_2
       admin = create :admin, responding_teams: [responding_team]
       expect(service.active_cases_for_user(admin)).to be_empty
     end
