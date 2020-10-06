eb0b76b67e192537d804c552f7d5de758d6e1feb
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-10-06 12:33:37 +0100

    Minor wording change

--- app/services/case_deletion_service.rb
+++ app/services/case_deletion_service.rb
@@ -11,7 +11,7 @@ class CaseDeletionService
   def call
     if @kase.related_case_links.present?
       @kase.errors.add(:related_case_links, 
-                       'are not empty, please remove all linked cases first in order to be able to delete this case.')
+                        I18n.t('activerecord.errors.models.case.attributes.related_case_links.not_empty'))
       @result = :error
     elsif @kase.update(@update_parameters.merge(deleted: true))
       @kase.state_machine.destroy_case!(acting_user: @user, acting_team: @kase.managing_team)
