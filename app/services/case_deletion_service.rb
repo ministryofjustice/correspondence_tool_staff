d37db9b1c63f464767182d651c49d94829c8c2ac
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-10-13 21:33:53 +0100

    wording change plus adding the part of clear up relationships after deletion

--- app/services/case_deletion_service.rb
+++ app/services/case_deletion_service.rb
@@ -14,6 +14,7 @@ class CaseDeletionService
                         I18n.t('activerecord.errors.models.case.attributes.related_case_links.not_empty'))
       @result = :error
     elsif @kase.update(@update_parameters.merge(deleted: true))
+      clear_up_linked_cases
       @kase.state_machine.destroy_case!(acting_user: @user, acting_team: @kase.managing_team)
       @result = :ok
     else
@@ -22,4 +23,14 @@ class CaseDeletionService
 
     @result
   end
+
+  private 
+
+  def clear_up_linked_cases
+    case_linkage_records = LinkedCase.where(type: 'related', linked_case_id: @kase.id)
+    case_linkage_records.each do | case_linkage_record |
+      case_linkage_record.destroy()
+    end
+  end
+
 end
