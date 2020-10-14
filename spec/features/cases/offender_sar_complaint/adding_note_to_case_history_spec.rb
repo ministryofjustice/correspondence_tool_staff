8e60c6f93f61a60f560ce72c7ef2db342e187ac9
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-10-14 14:55:33 +0100

    Adding note to case history

---
+++ spec/features/cases/offender_sar_complaint/adding_note_to_case_history_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'When viewing an offender sar case' do
+  given(:responder)         { find_or_create :branston_user }
+  given(:offender_sar_complaint) { create(:offender_sar_complaint).decorate }
+
+  scenario 'adding a note' do
+    login_as responder
+    cases_show_page.load(id: offender_sar_complaint.id)
+
+    expect(cases_show_page).to have_content "Add a note to this case"
+    cases_show_page.new_message.input.set "Hi this is nice"
+    click_on "Add to case history"
+
+    expect(cases_show_page).to be_displayed
+    expect(cases_show_page).to have_content "Hi this is nice"
+  end
+end
