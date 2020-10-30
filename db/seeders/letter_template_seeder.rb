7a58b2cd50142c78ca013bb173a9a653bb4c5f0b
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-10-30 08:38:46 +0000

    CT-3209-3210-3211 Add missing para

--- db/seeders/letter_template_seeder.rb
+++ db/seeders/letter_template_seeder.rb
@@ -190,6 +190,8 @@ class LetterTemplateSeeder
                   <br>
                   <br>Please note that copies of the data provided to you will be retained for no longer than nine months. Once this period has passed, we will be unable to answer any questions you may have or provide duplicates of this information. It will not normally be disclosed in any future SARs. I would like to suggest that you do not keep this information where it can be accessed by others. Once you have read through the information it can be placed in your stored property.
                   <br>
+                  <br>Finally, should you require a copy of any other personal information held by the MoJ we advise you to contact your key worker within the prison who can provide support regarding the disclosure of routine information that does not need to be provided through a formal SAR.
+                  <br>
                   <br>After the COVID-19 pandemic or as soon as reasonably practicable (based on Government and Public Health England guidance), we will revert to the provision of full services. We apologise for any inconvenience the reduced but pragmatic service may cause you.
                   <br>
                   <br>Yours sincerely
@@ -262,6 +264,8 @@ class LetterTemplateSeeder
                   <br>Thank you for your subject access request (SAR) dated <%= values.request_dated&.strftime('%e %B %Y') %>.
                   <br>
                   <br>We are not currently able to respond to SARs in full due to coronavirus COVID-19 disruption. During this unprecedented period of our history the Ministry of Justice (MoJ) is continuing to deliver its critical services, with a focus on those areas where resources are immediately required. For this reason and to support our colleagues in Her Majesty's Prison and Probation Service (HMPPS), we are only able to provide you with a copy of the personal information held on the PNOMIS electronic system.
+                  <br>
+                  <br>Should you or your client require a copy of any other personal information held by the MoJ we advise you to contact your client’s key worker within the prison who can provide support regarding the disclosure of routine information that does not need to be provided through a formal SAR.
                   <br>
                   <br>After the COVID-19 pandemic or as soon as reasonably practicable (based on Government and Public Health England guidance), we will revert to the provision of full services. We apologise for any inconvenience the reduced but pragmatic service may cause you.
                   <br>
@@ -349,6 +353,8 @@ class LetterTemplateSeeder
                   <br>If you have any queries regarding your request please contact the Offender Subject Access Request Team, at the address above. It is also open to you to ask the Information Commissioner to look into the case. You can contact the Information Commissioner at this address: Information Commissioner's Office, Wycliffe House, Water Lane, Wilmslow, Cheshire, SK9 5AF Internet: ico.org.uk
                   <br>
                   <br>Please note that copies of the data provided to you will be retained for no longer than nine months. Once this period has passed, we will be unable to answer any questions you may have or provide duplicates of this information. It will not normally be disclosed in any future SARs. I would like to suggest that you do not keep this information where it can be accessed by others. It would be helpful to remind your client of this. In a prison establishment the information can be placed in stored property.
+                  <br>
+                  <br>Finally, should you or your client require a copy of any other personal information held by the MoJ we advise you to contact your client’s key worker within the prison who can provide support regarding the disclosure of routine information that does not need to be provided through a formal SAR.
                   <br>
                   <br>After the COVID-19 pandemic or as soon as reasonably practicable (based on Government and Public Health England guidance), we will revert to the provision of full services. We apologise for any inconvenience the reduced but pragmatic service may cause you.
                   <br>
