74f183ee48d8e28daf2d295c03d1bf84e9c5227f
ymao2 <yikang.mao@digital.justice.gov.uk>
2020-12-01 21:54:59 +0000

    Added a few test spec for standard state flow

--- spec/factories/case/offender_sar_complaints.rb
+++ spec/factories/case/offender_sar_complaints.rb
@@ -14,7 +14,7 @@ FactoryBot.define do
       i_am_deleted        { false }
     end
 
-    current_state                   { 'data_to_be_requested' }
+    current_state                   { 'to_be_assessed' }
     association :original_case, factory: [:offender_sar_case, :closed]
     sequence(:name)                 { |n| "#{identifier} name #{n}" }
     email                           { Faker::Internet.email(name: identifier) }
@@ -51,16 +51,39 @@ FactoryBot.define do
       recipient { 'requester_recipient' }
     end
 
-    trait :data_to_be_requested do
+    trait :to_be_assessed do
       # Default state for a new offender_sar_complaint
+    end
+
+    trait :data_review_required do
+      transient do
+        identifier { 'Data review required - Complaint' }
+      end
+
+      after(:create) do |kase|
+        create :case_transition_data_review_required, case: kase
+        kase.reload
+      end
+    end
+
+    trait :data_to_be_requested do
+      transient do
+        identifier { 'Data to be requested - Complaint' }
+      end
+
+      after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
+        kase.reload
+      end
     end
 
     trait :waiting_for_data do
       transient do
-        identifier { 'Waiting for data Offender SAR' }
+        identifier { 'Waiting for data - Complaint' }
       end
 
       after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
         create :case_transition_waiting_for_data, case: kase
         kase.reload
       end
@@ -68,10 +91,11 @@ FactoryBot.define do
 
     trait :ready_for_vetting do
       transient do
-        identifier { 'Ready for vetting Offender SAR' }
+        identifier { 'Ready for vetting - Complaint' }
       end
 
       after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
         create :case_transition_waiting_for_data, case: kase
         create :case_transition_ready_for_vetting, case: kase
         kase.reload
@@ -80,10 +104,11 @@ FactoryBot.define do
 
     trait :vetting_in_progress do
       transient do
-        identifier { 'Vetting in progress Offender SAR' }
+        identifier { 'Vetting in progress - Complaint' }
       end
 
       after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
         create :case_transition_waiting_for_data, case: kase
         create :case_transition_ready_for_vetting, case: kase
         create :case_transition_vetting_in_progress, case: kase
@@ -93,10 +118,11 @@ FactoryBot.define do
 
     trait :ready_to_copy do
       transient do
-        identifier { 'Ready to close Offender SAR' }
+        identifier { 'Ready to close - Complaint' }
       end
 
       after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
         create :case_transition_waiting_for_data, case: kase
         create :case_transition_ready_for_vetting, case: kase
         create :case_transition_vetting_in_progress, case: kase
@@ -105,37 +131,59 @@ FactoryBot.define do
       end
     end
 
-    trait :ready_to_dispatch do
-      date_responded { Date.today }
-      info_held_status { find_or_create :info_status, :held }
-      transient do
-        identifier { 'Ready to dispatch Offender SAR' }
-      end
+    # trait :ready_to_dispatch do
+    #   date_responded { Date.today }
+    #   info_held_status { find_or_create :info_status, :held }
+    #   transient do
+    #     identifier { 'Ready to dispatch Offender SAR' }
+    #   end
 
-      after(:create) do |kase|
-        create :case_transition_waiting_for_data, case: kase
-        create :case_transition_ready_for_vetting, case: kase
-        create :case_transition_vetting_in_progress, case: kase
-        create :case_transition_ready_to_copy, case: kase
-        create :case_transition_ready_to_dispatch, case: kase
-        kase.reload
-      end
-    end
+    #   after(:create) do |kase|
+    #     create :case_transition_waiting_for_data, case: kase
+    #     create :case_transition_ready_for_vetting, case: kase
+    #     create :case_transition_vetting_in_progress, case: kase
+    #     create :case_transition_ready_to_copy, case: kase
+    #     create :case_transition_ready_to_dispatch, case: kase
+    #     kase.reload
+    #   end
+    # end
 
-    trait :closed do
+    trait :response_required do
       transient do
-        identifier { 'Closed Offender SAR' }
+        identifier { 'Responde required - Complaint' }
       end
 
       received_date  { 22.business_days.ago }
       date_responded { 4.business_days.ago }
 
       after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
         create :case_transition_waiting_for_data, case: kase
         create :case_transition_ready_for_vetting, case: kase
         create :case_transition_vetting_in_progress, case: kase
         create :case_transition_ready_to_copy, case: kase
-        create :case_transition_ready_to_dispatch, case: kase
+        # create :case_transition_ready_to_dispatch, case: kase
+        create :case_transition_response_required, case: kase
+        kase.reload
+      end
+    end
+
+    trait :closed do
+      transient do
+        identifier { 'Closed - Complaint' }
+      end
+
+      received_date  { 22.business_days.ago }
+      date_responded { 4.business_days.ago }
+
+      after(:create) do |kase|
+        create :case_transition_data_to_be_requested, case: kase
+        create :case_transition_waiting_for_data, case: kase
+        create :case_transition_ready_for_vetting, case: kase
+        create :case_transition_vetting_in_progress, case: kase
+        create :case_transition_ready_to_copy, case: kase
+        # create :case_transition_ready_to_dispatch, case: kase
+        create :case_transition_response_required, case: kase
         create :case_transition_closed_for_offender_sar_type, case: kase
         kase.reload
       end
