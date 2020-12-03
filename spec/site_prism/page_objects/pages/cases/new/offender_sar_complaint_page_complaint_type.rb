1fbc98e6d59923b112195a7657e7629abcd2f050
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-12-03 07:46:58 +0000

    Specify the complaint type

---
+++ spec/site_prism/page_objects/pages/cases/new/offender_sar_complaint_page_complaint_type.rb
@@ -0,0 +1,33 @@
+module PageObjects
+  module Pages
+    module Cases
+      module New
+        class OffenderSARComplaintPageComplaintType < PageObjects::Pages::Base
+
+          set_url '/cases/offender_sar_complaints/complaint-type'
+
+          section :primary_navigation,
+                  PageObjects::Sections::PrimaryNavigationSection, '.global-nav'
+
+          section :page_heading,
+                  PageObjects::Sections::PageHeadingSection, '.page-heading'
+
+          element :submit_button, '.button'
+
+          def fill_in_case_details(params={})
+            kase = FactoryBot.build :offender_sar_complaint, params
+
+            if kase.standard?
+              choose('offender_sar_complaint_complaint_type_standard', visible: false)
+            elsif kase.ico?
+              choose('offender_sar_complaint_complaint_type_ico', visible: false)
+            elsif kase.litigation?
+              choose('offender_sar_complaint_complaint_type_litigation', visible: false)
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
