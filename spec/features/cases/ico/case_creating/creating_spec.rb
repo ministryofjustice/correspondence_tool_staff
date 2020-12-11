0b7e33b30337a924aec5441075241a3bccb00b9d
ymao2 <yikang.mao@gmail.com>
2020-12-11 13:24:11 +0000

    CT-3312 Fixed bug of not being able to create ico-sar (#1404)
    * fixed bug of not being able to create ico-sar
    
    * corrected typoes

--- spec/features/cases/ico/case_creating/creating_spec.rb
+++ spec/features/cases/ico/case_creating/creating_spec.rb
@@ -7,6 +7,8 @@ feature 'ICO case creation' do
   given(:manager)                     { find_or_create :disclosure_bmt_user }
   given(:managing_team)               { create :managing_team, managers: [manager] }
   given(:original_foi)                { create :closed_case }
+  given(:original_sar)                { create :closed_sar }
+  given(:related_sar)                { create :closed_sar }
   given(:related_foi)                 { create :closed_case }
   given(:another_related_foi)         { create :closed_case }
   given(:related_timeliness_review)   { create :closed_timeliness_review }
@@ -25,7 +27,7 @@ feature 'ICO case creation' do
   end
 
   context 'creating an ICO appeal' do
-    scenario ' - linking Original case', js: true do
+    scenario ' - linking Original FOI case', js: true do
       cases_new_ico_page.load
 
       cases_new_ico_page.form.original_case_number.set ''
@@ -38,6 +40,21 @@ feature 'ICO case creation' do
       expect(cases_new_ico_page.form).to have_no_original_case_number_error
       expect(cases_new_ico_page.form.original_case.linked_records.first.link)
         .to have_copy original_foi.number
+    end
+
+    scenario ' - linking Original SAR case', js: true do
+      cases_new_ico_page.load
+
+      cases_new_ico_page.form.original_case_number.set ''
+      cases_new_ico_page.form.link_original_case.click
+      expect(cases_new_ico_page.form.original_case_number_error.text)
+        .to eq 'Enter original case number'
+
+      cases_new_ico_page.form.original_case_number.set original_sar.number
+      cases_new_ico_page.form.link_original_case.click
+      expect(cases_new_ico_page.form).to have_no_original_case_number_error
+      expect(cases_new_ico_page.form.original_case.linked_records.first.link)
+        .to have_copy original_sar.number
     end
 
     scenario ' - removing Original case', js: true do
@@ -99,11 +116,23 @@ feature 'ICO case creation' do
       expect(cases_new_ico_page.form).to have_no_related_cases
     end
 
-    scenario 'creating a case with request attachments', js: true  do
+    scenario 'creating an ICO appeal linking to FOI case with request attachments', js: true  do
       request_attachment = Rails.root.join('spec', 'fixtures', 'request-1.pdf')
 
       create_ico_case_step(original_case: original_foi,
                             related_cases: [related_foi], 
+                            uploaded_request_files: [request_attachment])
+
+      new_case = Case::Base.last
+      request_attachment = new_case.attachments.request.first
+      expect(request_attachment.key).to match %{/request-1.pdf$}
+    end  
+
+    scenario 'creating an ICO appeal linking to SAR case with request attachments', js: true  do
+      request_attachment = Rails.root.join('spec', 'fixtures', 'request-1.pdf')
+
+      create_ico_case_step(original_case: original_sar,
+                            related_cases: [related_sar], 
                             uploaded_request_files: [request_attachment])
 
       new_case = Case::Base.last
