457935428d578e2ceee26fca6cd49ad744fdbd6e
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-01-25 22:30:58 +0000

    CT-3248 Added performance reports for standard, ico complaint

---
+++ app/services/stats/r206_offender_standard_complaint_monthly_performance_report.rb
@@ -0,0 +1,25 @@
+module Stats
+  class R206OffenderStandardComplaintMonthlyPerformanceReport < R205OffenderSarMonthlyPerformanceReport
+
+    class << self
+      def title
+        'Monthly report'
+      end
+
+      def description
+        'Includes performance data about Offender standard complaint requests we received and responded to from the beginning of the year by month.'
+      end
+    end
+
+    def case_scope
+      Case::Base.offender_sar_complaint
+        .where("properties->>'complaint_type'::text = ? ", "standard_complaint")
+        .where(received_date: @period_start..@period_end)
+    end
+
+    def report_type
+      ReportType.r206
+    end
+
+  end
+end
---
+++ app/services/stats/r207_offender_ico_complaint_monthly_performance_report.rb
@@ -0,0 +1,25 @@
+module Stats
+  class R207OffenderICOComplaintMonthlyPerformanceReport < R205OffenderSarMonthlyPerformanceReport
+
+    class << self
+      def title
+        'Monthly report'
+      end
+
+      def description
+        'Includes performance data about Offender ICO complaint requests we received and responded to from the beginning of the year by month.'
+      end
+    end
+
+    def case_scope
+      Case::Base.offender_sar_complaint
+      .where("properties->>'complaint_type'::text = ? ", "ico_complaint")
+      .where(received_date: @period_start..@period_end)
+    end
+
+    def report_type
+      ReportType.r207
+    end
+
+  end
+end
