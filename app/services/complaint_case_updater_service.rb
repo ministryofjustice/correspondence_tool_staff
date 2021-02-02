d482fa59bd23f591d8cd8c8031c4d421ad5fd1f2
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-02-02 15:47:28 +0000

    feature test passing for type change and status reset

---
+++ app/services/complaint_case_updater_service.rb
@@ -0,0 +1,20 @@
+class ComplaintCaseUpdaterService < CaseUpdaterService
+  attr_reader :result
+
+  INITAL_STATE = 'to_be_assessed'
+
+  def call
+    reset_status_on_complaint_type_change
+    super
+  end
+
+  private
+
+  def reset_status_on_complaint_type_change
+    case_type = @kase.complaint_type
+    params_case_type = @params['complaint_type']
+    if case_type != params_case_type && params_case_type.present?
+      @kase.current_state = INITAL_STATE
+    end
+  end
+end
