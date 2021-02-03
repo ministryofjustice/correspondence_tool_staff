8855d6c5c9fc09bc3ce73106b9832957fa6a9f07
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-02-03 17:32:11 +0000

    Bug fix and add test to cover it
    Fix bug so when you change the contact details but not the type the
    status doesn't reset.
    
    Was due to the difference in comparing @kase.complaint_type (say 'ICO')
    and @params['complaint_type'] which returns 'ico_complaint'. Added a
    mapping function to get around this distinction and a test to the
    service class to cover it.

--- app/services/complaint_case_updater_service.rb
+++ app/services/complaint_case_updater_service.rb
@@ -11,10 +11,27 @@ class ComplaintCaseUpdaterService < CaseUpdaterService
   private
 
   def reset_status_on_complaint_type_change
+    if current_case_type_doesnt_match_selected_case_type 
+      reset_case_status_to_default_state
+    end
+  end
+
+  def reset_case_status_to_default_state
+      @kase.current_state = INITAL_CASE_STATUS
+  end
+
+  def current_case_type_doesnt_match_selected_case_type
     case_type = @kase.complaint_type
     params_case_type = @params['complaint_type']
-    if case_type != params_case_type && params_case_type.present?
-      @kase.current_state = INITAL_CASE_STATUS
-    end
+
+    complaint_type_mapping(case_type) != params_case_type && params_case_type.present?
+  end
+
+  def complaint_type_mapping(case_complaint_type)
+    {
+      'Litigation': 'litigation_complaint',
+      'ICO': 'ico_complaint',
+      'Standard': 'standard_complaint'
+    }[case_complaint_type.to_sym]
   end
 end
--- spec/services/complaint_case_updater_service_spec.rb
+++ spec/services/complaint_case_updater_service_spec.rb
@@ -5,7 +5,7 @@ describe ComplaintCaseUpdaterService do
   describe '#call' do
     let(:team)          { find_or_create :team_branston }
     let(:user)          { team.users.first }
-    let(:kase)          { create :offender_sar_complaint, current_state: 'waiting_for_data'}
+    let(:kase)          { create :offender_sar_complaint, current_state: 'waiting_for_data', complaint_type: 'ico_complaint'}
     let(:state_machine) { double ConfigurableStateMachine::Machine, edit_case!: true }
 
     before(:each) do
@@ -30,6 +30,23 @@ describe ComplaintCaseUpdaterService do
     context 'when the case state is not updated but other details are' do
       let(:params) { 
         {
+          "priority"=>"high"
+        }
+      }
+
+      it 'the case state remains the same' do
+        @service.call
+
+        expect(kase.current_state).to eq 'waiting_for_data'
+        expect(kase.priority).to eq 'high'
+      end
+    end
+
+    context 'when the case state is not updated but the states contact details are' do
+      let(:params) { 
+        {
+          "ico_contact_phone"=>"12323489", 
+          "ico_reference"=>"test_ref_123",
           "priority"=>"high"
         }
       }
