03b30ec7d5c53d2a33d4a37a2cedd22457af2008
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-02-09 16:11:17 +0000

    Use state machine for status reset rather than manual

--- app/services/complaint_case_updater_service.rb
+++ app/services/complaint_case_updater_service.rb
@@ -15,21 +15,13 @@ class ComplaintCaseUpdaterService < CaseUpdaterService
   end
 
   def reset_case_status_to_default_state
-    @kase.current_state = @kase.state_machine.initial_state 
+    @kase.state_machine.reset_to_initial_state!(user: @user, team: @team)
   end
 
   def current_case_type_doesnt_match_selected_case_type
-    case_type = @kase.complaint_type
+    case_type = @kase.object.complaint_type
     params_case_type = @params['complaint_type']
 
-    complaint_type_mapping(case_type) != params_case_type && params_case_type.present?
-  end
-
-  def complaint_type_mapping(case_complaint_type)
-    {
-      'Litigation': 'litigation_complaint',
-      'ICO': 'ico_complaint',
-      'Standard': 'standard_complaint'
-    }[case_complaint_type.to_sym]
+    case_type != params_case_type && params_case_type.present?
   end
 end
