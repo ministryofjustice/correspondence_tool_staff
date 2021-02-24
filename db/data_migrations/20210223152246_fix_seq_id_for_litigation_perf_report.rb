2453c50a6db8f250df7d4b750b1f8d2926fe882d
ymao2 <yikang.mao@gmail.com>
2021-02-24 12:23:58 +0000

    CT-3047 Fixed wrong report type for litigation performance report (#1480)
    * CT-3047 Fixed wrong report type for litigation performance report
    
    * added end empty line

---
+++ db/data_migrations/20210223152246_fix_seq_id_for_litigation_perf_report.rb
@@ -0,0 +1,10 @@
+class FixSeqIdForLitigationPerfReport < ActiveRecord::DataMigration
+  def up
+    ReportType.reset_column_information
+
+    report_type = ReportType.find_by(abbr: 'R208')
+    if report_type
+      report_type.update(seq_id: 1400)
+    end
+  end
+end
