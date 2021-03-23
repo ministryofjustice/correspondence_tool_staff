4ec7a41903ce5d75c929df771251f97f0fd8f3aa
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-03-23 21:38:05 +0000

    CT-3523 Fixed the bugs when assgining responder for complaint case

--- spec/features/cases/offender_sar_complaint/case_assign_responder_spec.rb
+++ spec/features/cases/offender_sar_complaint/case_assign_responder_spec.rb
@@ -15,6 +15,8 @@ feature 'cases being reassigned to other team members' do
 
     cases_show_page.load(id: unassigned_complaint_case.id)
     go_to_case_assign_team_member expected_users: [responder, another_responder]
+    not_choose_member_and_continue(assign_to_team_member_page)
+    click_assign_memeber_btn
     do_case_assign_team_member another_responder
   end
 
@@ -23,6 +25,8 @@ feature 'cases being reassigned to other team members' do
 
     cases_show_page.load(id: accepted_complaint_case.id)
     go_to_case_reassign expected_users: [responder, another_responder]
+    not_choose_member_and_continue(reassign_user_page)
+    click_reassign_member_btn
     do_case_reassign_to another_responder
     go_to_case_reassign expected_users: nil
     do_case_reassign_to responder
@@ -48,5 +52,19 @@ feature 'cases being reassigned to other team members' do
     expect(cases_show_page.case_history.entries.first)
       .to have_text("assigned this case to #{user.full_name}")
   end
-  
+
+  def not_choose_member_and_continue(page_object)
+    page_object.confirm_button.click
+
+    expect(cases_show_page).to be_displayed
+    expect(cases_show_page).to have_content 'No changes were made'
+  end
+
+  def click_assign_memeber_btn
+    cases_show_page.actions.assign_to_team_member.click
+  end 
+
+  def click_reassign_member_btn
+    cases_show_page.actions.reassign_user.click
+  end
 end
