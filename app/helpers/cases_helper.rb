9a63a432c71c6c64ceef05cedb7ca0749b645896
= <javid.ali@digital.justice.gov.uk>
2021-05-04 15:25:59 +0100

    CT-3347 Remove useless variable

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -32,7 +32,7 @@ module CasesHelper #rubocop:disable Metrics/ModuleLength
     page = 1 if page.blank?
     
     if position.nil?
-      link_to span + case_number, case_path(kase.id, pos: page_offset + position)
+      link_to span + case_number, case_path(kase.id)
     else
       position += 1
       page_offset = Kaminari.config.default_per_page * (page.to_i - 1)
