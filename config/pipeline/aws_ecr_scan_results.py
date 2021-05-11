04131eb3416c7d16faa50a937b4dc35b03395533
= <javid.ali@digital.justice.gov.uk>
2021-05-11 12:47:10 +0100

    CT-3357 compact message for vulnerabilities

--- config/pipeline/aws_ecr_scan_results.py
+++ config/pipeline/aws_ecr_scan_results.py
@@ -52,31 +52,30 @@ class ECRScanChecker:
         )
 
     def recursive_check_make_report(self, tag):
-        print("Checking ECR scan results...")
+        print("Checking ECR scan results...")   
         for image in self.images_to_check:
             try:
                 findings = self.get_ecr_scan_findings(image, tag)[
                     "imageScanFindings"]
                 if findings["findings"] != []:
-
+                    job_link_tag = "{0} <{1}|{2}>".format(
+                        os.getenv('CIRCLE_BUILD_URL', ""), tag)
                     counts = findings["findingSeverityCounts"]
-                    title = "\n\n:warning: *AWS ECR Scan found results for {}:* \n".format(
-                        image)
-                    severity_counts = "Severity finding counts:\n{}\nDisplaying the first {} in order of severity\n\n".format(
-                        counts, self.report_limit)
-                    self.report = title + severity_counts
+                    title = ":warning: AWS ECR Scan found results for {1}:{2}...\n".format(
+                        job_link_tag, counts)
+                    self.report = title
 
                     for finding in findings["findings"]:
                         cve = finding["name"]
                         severity = finding["severity"]
 
-                        description = "None"
+                        description = ""
                         if "description" in finding:
                             description = finding["description"]
 
                         link = finding["uri"]
-                        result = "*Image:* {0} \n**Tag:* {1} \n*Severity:* {2} \n*CVE:* {3} \n*Description:* {4} \n*Link:* {5}\n\n".format(
-                            image, tag, severity, cve, description, link)
+                        result = "<{4}|{1} - {2} - {3}>\n".format(
+                            severity, cve, description, link)
                         self.report += result
                     print(self.report)
             except:
@@ -94,11 +93,7 @@ class ECRScanChecker:
         return response
 
     def post_to_slack(self, slack_webhook):
-        if self.report != "":
-            branch_info = "*Github Branch:* {0}\n*CircleCI Job Link:* {1}\n\n".format(
-                os.getenv('CIRCLE_BRANCH', ""),
-                os.getenv('CIRCLE_BUILD_URL', ""))
-            self.report += branch_info
+        if self.report != "": 
             print(self.report)
 
             post_data = json.dumps({"text": self.report})
