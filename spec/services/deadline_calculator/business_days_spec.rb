a8b73026017444073233eb3ed82d2e18e0d35fee
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-05-11 20:05:54 +0100

    changed the way of creating the date

--- spec/services/deadline_calculator/business_days_spec.rb
+++ spec/services/deadline_calculator/business_days_spec.rb
@@ -100,14 +100,14 @@ describe DeadlineCalculator::BusinessDays do
 
     context 'received on a Saturday' do
 
-      let(:sat_jul_01) { Date.new(2017, 7, 1) }
-      let(:thu_jul_06) { Date.new(2017, 7, 6) }
-      let(:mon_jul_17) { Date.new(2017, 7, 17) }
-      let(:mon_jul_31) { Date.new(2017, 7, 31) }
-      let(:sat_may_01) { Date.new(2021, 5, 1) }
-      let(:wed_jun_02) { Date.new(2021, 6, 2) }
-      let(:tue_may_18) { Date.new(2021, 5, 18) }
-      let(:fri_may_07) { Date.new(2021, 5, 07) }
+      let(:sat_jul_01) { Time.utc(2017, 7, 1, 12, 0, 0) }
+      let(:thu_jul_06) { Time.utc(2017, 7, 6, 12, 0, 0) }
+      let(:mon_jul_17) { Time.utc(2017, 7, 17, 12, 0, 0) }
+      let(:mon_jul_31) { Time.utc(2017, 7, 31, 12, 0, 0) }
+      let(:sat_may_01) { Time.utc(2021, 5, 1, 12, 0, 0) }
+      let(:wed_jun_02) { Time.utc(2021, 6, 2, 12, 0, 0) }
+      let(:tue_may_18) { Time.utc(2021, 5, 18, 12, 0, 0) }
+      let(:fri_may_07) { Time.utc(2021, 5, 7, 12, 0, 0) }
 
       describe '.escalation_deadline' do
         it 'is 3 days after first working day after received date' do
@@ -134,7 +134,7 @@ describe DeadlineCalculator::BusinessDays do
               if start_date.workday?
                 count += 1
               end
-              start_date += 1
+              start_date = start_date.tomorrow
             end
             expect(count).to eq 3
             expect(deadline_calculator.escalation_deadline)
@@ -146,15 +146,6 @@ describe DeadlineCalculator::BusinessDays do
       describe '.external_deadline' do
         it 'is 20 working days first working day after received date' do
           Timecop.freeze sat_jul_01 do
-            start_date = sat_jul_01
-            count = 0
-            while start_date != mon_jul_31
-              if start_date.workday?
-                count += 1
-              end
-              start_date += 1
-            end
-            expect(count).to eq 20
             expect(deadline_calculator.external_deadline)
               .to eq mon_jul_31.to_date
           end
@@ -168,7 +159,7 @@ describe DeadlineCalculator::BusinessDays do
               if start_date.workday?
                 count += 1
               end
-              start_date += 1
+              start_date = start_date.tomorrow
             end
             expect(count).to eq 20
             expect(deadline_calculator.external_deadline)
