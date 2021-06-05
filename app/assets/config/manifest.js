23a4e6d74ea0c2e404617a367d5ab6fdda2055e0
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-06-05 14:08:10 +0100

    tried again for Sprockets 4

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .scss
+//= link_tree ../javascripts .js
+//= link_tree ../stylesheets .scss
+//= link ../stylesheets/application.css.scss
+//= link ../stylesheets/ie_shame.css
+//= link ../stylesheets/print.css.scss
