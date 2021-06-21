0035feaed7d66b1d6ec7209308b64daee47ddf59
Javid <javid.ali@digital.justice.gov.uk>
2021-06-21 21:02:12 +0100

    CT-3553 dry out env vars in k8 (#1616)
    * CT-3553 dry out env vars in k8
    
    * CT-3553 Add QA and staging and attempt to fix test
    
    * CT-3553 actually fix test and then fix errors it found
    
    * CT-3553 remove useless method now
    
    * CT-3553 Fix typo in path for configmap

--- deploy.sh
+++ deploy.sh
@@ -121,6 +121,10 @@ function _deploy() {
   # kubectl config set-context ${context} --namespace=$namespace
   # kubectl config use-context ${context}
 
+  # Apply config map updates
+  kubectl apply \
+    -f config/kubernetes/${environment}/configmap.yaml -n $namespace
+
   # Apply image specific config
   kubectl set image -f config/kubernetes/${environment}/deployment.yaml \
           webapp=${docker_image_tag} \
