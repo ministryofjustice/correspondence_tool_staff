2d7ffe9062d2f5c61483a960330455ecc4ba54a1
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-07-01 22:31:33 +0100

    Reverted change to one data migration

--- db/data_migrations/20200526115209_update_branston_users.rb
+++ db/data_migrations/20200526115209_update_branston_users.rb
@@ -1,17 +1,13 @@
 class UpdateBranstonUsers < ActiveRecord::DataMigration
   def up
-    if BusinessUnit.find_by(code: Settings.offender_sar_cases.default_managing_team)
-      branston_team = BusinessUnit.dacu_branston
-      branston_team.update_attribute(:role, "responder")
-      branston_team.user_roles.map {|role| role.update_attribute(:role, "responder") }
-    end
+    branston_team = BusinessUnit.dacu_branston
+    branston_team.update_attribute(:role, "responder")
+    branston_team.user_roles.map {|role| role.update_attribute(:role, "responder") }
   end
 
   def down
-    if BusinessUnit.find_by(code: Settings.offender_sar_cases.default_managing_team)
-      branston_team = BusinessUnit.dacu_branston
-      branston_team.update_attribute(:role, "manager")
-      branston_team.user_roles.map {|role| role.update_attribute(:role, "manager") }
-    end
+    branston_team = BusinessUnit.dacu_branston
+    branston_team.update_attribute(:role, "manager")
+    branston_team.user_roles.map {|role| role.update_attribute(:role, "manager") }
   end
 end
