188772b29863136bd2476c16f0168d06750e1539
= <javid.ali@digital.justice.gov.uk>
2021-07-28 14:34:22 +0100

    CT-3744 Sign user in before page controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #accessibility" do
+    let(:user) { create(:user) }
+    before{
+        sign_in user
+      }
+
     it "returns http success" do
       get :accessibility
       expect(response).to have_http_status(:success)
