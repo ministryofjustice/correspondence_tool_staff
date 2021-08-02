07daeb8d3f74f7b42e203ed1cb86cede60a3bc56
Will McBrien <william.mcbrien@digital.justice.gov.uk>
2021-08-02 11:27:37 +0100

    CT-3704 address contact book (#1666)
    * Add contacts model and spec
    
    * Add basic CRUD functionality for Contacts
    
    * remove contacts show action + views
    
    * Fix headings and buttons
    
    * Remove show link from edit page + lookup for show action
    
    * fix form
    
    * fix back links
    
    * config/settings.yml
    
    * Fix index address formatting
    
    * contact resources except show
    
    * WIP add feature test
    
    * Add contacts_feature_spec
    
    * Add 'other' contact type
    
    + delete auto comments
    + delete some spaces added by mistake
    
    * Fix spec
    
    * PR review frontend changes + typo fix

---
+++ app/models/contact.rb
@@ -0,0 +1,30 @@
+class Contact < ApplicationRecord
+  enum contact_type: %i(prison probation solicitor other)
+
+  validates :name, presence: true
+  validates :address_line_1, presence: true
+  validates :postcode, presence: true
+  validates :contact_type, presence: true
+
+  def address
+    format_address("\n")
+  end
+
+  def inline_address
+    format_address(", ")
+  end
+
+  private
+
+  def format_address(separator)
+    [
+      address_line_1,
+      address_line_2,
+      town,
+      county,
+      postcode
+    ].compact
+     .reject(&:empty?)
+     .join(separator)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
+  resources :contacts, except: :show 
+
   devise_for :users, controllers: { passwords: 'passwords' }
 
   gnav = Settings.global_navigation
