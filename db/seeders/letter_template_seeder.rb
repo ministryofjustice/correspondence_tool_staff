55c139f24a2d913ec9bd9aa3638aaf6a6defc4dc
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-08-03 09:52:57 +0100

    Add solicitor ack restricted letter

--- db/seeders/letter_template_seeder.rb
+++ db/seeders/letter_template_seeder.rb
@@ -213,6 +213,44 @@ class LetterTemplateSeeder
                 EOF
                 )
 
+    rec = LetterTemplate.find_by(abbreviation: 'solicitor-acknowledgement-restricted')
+    rec = LetterTemplate.new if rec.nil?
+    rec.update!(name: 'Solicitor acknowledgement letter - RESTRICTED',
+                abbreviation: 'solicitor-acknowledgement-restricted',
+                template_type: 'acknowledgement',
+                body: <<~EOF
+                  <p>
+                  <% if values.recipient == "requester_recipient" %><br><br>Dear Sirs<% end %>
+                  <br>
+                  <br><strong>DATA PROTECTION ACT 2018: SUBJECT ACCESS REQUEST</strong>
+                  <br><strong><%= values.subject_full_name&.upcase %> - <%= values.first_prison_number %></strong>
+                  <br>
+                  <br>Thank you for your subject access request (SAR) dated <%= values.request_dated&.strftime('%e %B %Y') %>.
+                  <br>
+                  <br>As your client has previously submitted a SAR and have been supplied with information, we will only be providing you with information not previously received, i.e. information from the date of the previous request. The previous request was started on.
+                  <br>
+                  To complete the SAR we will have to identify information from a number of business areas, including prison establishments. In view of this there is very little information we can provide in response to enquiries. We aim to process your SAR within one calendar month, and it would be helpful if you could limit any communication with the office during this period.
+                  <br>
+                  Her Majesty’s Prison and Probation Service (HMPPS) holds routine information that can be disclosed to you or your client as part of business as usual requests, without submitting a formal SAR. Please find enclosed a list of documents that can be provided by your client’s keyworker in the prison or probation office. As some of the information requested can be disclosed directly, we will be forwarding your request to the relevant business area to take forward and the documents listed will not be included in your SAR response.
+                  <br>
+                  Please also note if you have requested medical information as part of the request, the responsibility for providing medical information has transferred to the National Health Service/ Clinical Commissioning Groups. If you require medical information, please contact the Healthcare Services team in the prison.
+                  <br>
+                  <br>Yours sincerely
+                  <br>
+                  <br>
+                  <br>
+                  <br>
+                  <br>Application Team
+                  <br>Offender Subject Access Request Team
+                  <br>Ministry of Justice
+                  </p>
+                EOF
+                )
+    rec.update!(letter_address: <<~EOF
+                  #{solictor_receiver}
+                  <br>#{address}
+                EOF
+                )
     rec = LetterTemplate.find_by(abbreviation: 'solicitor-disclosed')
     rec = LetterTemplate.new if rec.nil?
     rec.update!(name: 'Solicitor disclosed letter',
