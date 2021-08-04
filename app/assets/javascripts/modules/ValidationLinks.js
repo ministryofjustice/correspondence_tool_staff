2cdceee98c159ad84fe0a1714456f251bab9026a
= <javid.ali@digital.justice.gov.uk>
2021-08-04 15:44:23 +0100

    CT-3372 try5

--- app/assets/javascripts/modules/ValidationLinks.js
+++ app/assets/javascripts/modules/ValidationLinks.js
@@ -1,6 +1,6 @@
 // This is to fix the mismatch between the prefixes for validation # links that break after
 // we re-use ruby code. i.e. when we use "as ..." in form generation, context breaks links
-moj.Modules.FixValidationLinks = {
+moj.Modules.ixValidationLinks = {
 	$errorSummary: $('.error-summary'),
 	$allValidationLinks: $('.error-summary-list a'),
 	$allValidationDivs: $('.form-group-error'),
@@ -8,5 +8,12 @@ moj.Modules.FixValidationLinks = {
 	
 	init: function() {
 		if(this.$allValidationLinks.length === 0) return; // Don't bother if no error on page
+
+		this.removeAnyExtraSummaries(); // sometimes it shows two summaries, remove redundant one
+	},
+	removeAnyExtraSummaries: function() {
+		if(this.$errorSummary.length > 1) {
+			this.$errorSummary.first().remove();
+		}
 	}
 }
