ec2aa287b41725c816d6ad62f53cea57f16adf9a
= <javid.ali@digital.justice.gov.uk>
2021-08-04 11:59:05 +0100

    CT-3372 Remove extra error summaries, like the one on edit complaint

--- app/assets/javascripts/modules/ValidationLinks.js
+++ app/assets/javascripts/modules/ValidationLinks.js
@@ -1,12 +1,15 @@
 // This is to fix the mismatch between the prefixes for validation # links that break after
 // we re-use ruby code. i.e. when we use "as ..." in form generation, context breaks links
 moj.Modules.fixValidationLinks = {
+	$errorSummary: $('.error-summary'),
 	$allValidationLinks: $('.error-summary-list a'),
 	$allValidationDivs: $('.form-group-error'),
 	$linkPrefix: null,
 	
 	init: function() {
 		if(this.$allValidationLinks.length === 0) return; // Don't bother if no error on page
+
+		this.removeAnyExtraSummaries(); // sometimes it shows two summaries, remove redundant one
 
 		// If there's just one validation div, just make all links go to it..
 		if(this.$allValidationDivs.length === 1 && this.$allValidationLinks.length > 0) { 
@@ -20,6 +23,11 @@ moj.Modules.fixValidationLinks = {
 
 		// Update all links to work if possible and needed
 		this.checkAndUpdateAllLinks();
+	},
+	removeAnyExtraSummaries: function() {
+		if(this.$errorSummary.length > 1) {
+			this.$errorSummary.first().remove();
+		}
 	},
 	matchSingleDivLinks: function(){
 		const LinkHrefId = this.$allValidationDivs[0].id;
