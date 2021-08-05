d552311ff61c124086de8342ae6707cf49e8ce86
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-08-05 11:11:53 +0100

    Get address fill working + refactor js

--- app/assets/javascripts/modules/Contacts.js
+++ app/assets/javascripts/modules/Contacts.js
@@ -6,50 +6,57 @@ moj.Modules.Contacts = {
         height: 400,
         width: 600,
         modal: true,
-        title: 'Find address',
-        close: function(event, ui) { console.log(event); }
+        title: 'Find an address'
     }),
 
     init: function() {
         var self = this;
-
-        self.attach_finder_button_event();
-
-
+        self.attach_button_event_to_open_dialog();
     },
 
-    attach_finder_button_event: function() {
+    attach_button_event_to_open_dialog: function() {
         var self = this;
 
         self.$button.on( "click", function(e) {
             e.preventDefault();
 
-            self.$remote_content.load("/contacts_search", function() {
-                self.$dialog.dialog( "open" );
+            self.load_modal_form();
 
-                setTimeout(function() { 
-                    $('#popup-search-button').focus(); 
-                },1); // fix voiceover focus issue
-
-                setTimeout(function() { 
-                    $('#popup-search').focus(); 
-                },2); // fix voiceover focus issue
-                console.log('initial load');
-            });
             $('#dialog-content').parent().attr('aria-modal','true');
             $('#dialog-content').parent().removeAttr('aria-describedby','');
         });
     },
 
-    fill_in_address : function (address){
-        console.log('fill_in_address');
-        $dialog.dialog('close');
-        $('#offender_sar_subject_address').val(address);
+    load_modal_form : function(){
+        var self = this;
+        self.$remote_content.load("/contacts_search", function() {
+            self.$dialog.dialog( "open" );
+
+            setTimeout(function() { 
+                $('#popup-search-button').focus(); 
+            }, 1); // fix voiceover focus issue
+
+            setTimeout(function() { 
+                $('#popup-search').focus(); 
+            }, 2); // fix voiceover focus issue
+        });
     },
 
-    search_results_loaded : function () { // To refocus on Search for NVDA after popup re-renders
-        console.log('results loaded');
-        $('#popup-search-button').focus(); // fix NVDA focus issue after search
-    }   
+    attach_address_selection_button_events : function() {
+        $('.use-address-button').on('click', function(e){
+            var name = $($(this).parent().find('.contact-name')[0]).text();
+            var address = $($(this).parent().find('.contact-address')[0]).text();
 
+            var formatted_address = name + "\n" + address;
+
+            $('#dialog-content').dialog( "close" );
+
+            $('#selected-address-field').text("");
+            $('#selected-address-field').text(formatted_address);
+        });
+    },
+
+    search_results_loaded : function() { // To refocus on Search for NVDA after popup re-renders
+        $('#popup-search-button').focus(); // fix NVDA focus issue after search
+    }
 };
