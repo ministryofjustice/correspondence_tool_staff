9b5f032aa0afb2de33c558323f5d51e460ec77cd
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-09-07 16:04:31 +0100

    Add address (contact) types via data migration

--- db/data_migrations/20210903145625_add_address_types.rb
+++ db/data_migrations/20210903145625_add_address_types.rb
@@ -3,43 +3,43 @@ class AddAddressTypes < ActiveRecord::DataMigration
   def up
     category_references = [
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'prison',
         value: 'Prison',
         display_order: 10
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'probation',
         value: 'Probation centre',
         display_order: 20
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'solicitor',
         value: 'Solicitor',
         display_order: 30
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'branston',
         value: 'Branson',
         display_order: 40
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'hmpps_hq',
         value: 'HMPPS HQ',
         display_order: 50
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'hmcts',
         value: 'HMCTS',
         display_order: 60
       },
       { 
-        category: 'address_type',
+        category: 'contact_type',
         code: 'other',
         value: 'Other',
         display_order: 70
@@ -53,6 +53,6 @@ class AddAddressTypes < ActiveRecord::DataMigration
   # rubocop:enable Metrics/MethodLength
 
   def down
-    CategoryReference.where(category: 'address_type').delete_all
+    CategoryReference.where(category: 'contact_type').delete_all
   end
 end
