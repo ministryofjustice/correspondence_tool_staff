52c95f706c8c6e0097a2ab58e1ac77eee53f7f6b
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-09-16 18:02:13 +0100

    Fixed the faile offender sar creation test spec

--- spec/site_prism/page_objects/pages/cases/new/offender_sar_page_subject_details.rb
+++ spec/site_prism/page_objects/pages/cases/new/offender_sar_page_subject_details.rb
@@ -36,7 +36,11 @@ module PageObjects
 
           def fill_in_case_details(params={})
             kase = FactoryBot.build :offender_sar_case, params
-            subject_full_name.set(params[:subject_full_name] || 'Sabrina Adams')
+            if params.present? && params[:subject_full_name].present?
+              subject_full_name.set(params[:subject_full_name])
+            else
+              subject_full_name.set('Sabrina Adams')
+            end
             prison_number.set kase.prison_number
             subject_address.set kase.subject_address
             subject_aliases.set kase.subject_aliases
