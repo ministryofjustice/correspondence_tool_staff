09b80a6a218e5267363af7ec4d21ba58c31c8979
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-09-16 17:15:50 +0100

    CT-3054 Clean third part info when option changed

--- spec/site_prism/page_objects/pages/cases/new/offender_sar_page_subject_details.rb
+++ spec/site_prism/page_objects/pages/cases/new/offender_sar_page_subject_details.rb
@@ -36,8 +36,7 @@ module PageObjects
 
           def fill_in_case_details(params={})
             kase = FactoryBot.build :offender_sar_case, params
-
-            subject_full_name.set 'Sabrina Adams'
+            subject_full_name.set (params[:subject_full_name] || 'Sabrina Adams')
             prison_number.set kase.prison_number
             subject_address.set kase.subject_address
             subject_aliases.set kase.subject_aliases
