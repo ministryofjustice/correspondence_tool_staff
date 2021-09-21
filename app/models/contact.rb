1c658cec9e52f09a017e50e996afa4e0ab2592d5
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-09-21 17:14:42 +0100

    Add class methods and specs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -15,6 +15,25 @@ class Contact < ApplicationRecord
     format_address(", ")
   end
 
+  def contact_type_display_value
+    contact_type.value
+  end
+
+  def self.filter_by_contact_type(filters)
+    filter_sql = 'category_references.code IN (?)'
+    joins(:contact_type).where(filter_sql, filters)
+  end
+
+  def self.search_by_contact_name(search_term)
+    search_sql = "LOWER(name) LIKE CONCAT('%', ?, '%')"
+    where(search_sql, search_term).order(:name)
+  end
+
+  def self.filtered_search_by_contact_name(filters, search_term)
+    filter_by_contact_type(filters).search_by_contact_name(search_term)
+  end
+
+
   private
 
   def format_address(separator)
