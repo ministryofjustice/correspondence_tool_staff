710e10f27c0cdbbfa78e689b3de1d3e5c7b90258
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-09-22 09:49:08 +0100

    Add feature test for address lookup filtering

--- spec/features/contacts/contacts_spec.rb
+++ spec/features/contacts/contacts_spec.rb
@@ -16,6 +16,26 @@ feature 'Contacts address book', js: true do
                            contact_type: CategoryReference.find_by(code: 'probation')) 
   }
 
+  given!(:contact_2) { create(:contact, 
+                           name: 'Solicitors LLP',
+                           address_line_1: '456 fake street',
+                           address_line_2: 'little heath',
+                           town: 'bakersville',
+                           county: 'Mercia',
+                           postcode: 'FL7 0YK',
+                           contact_type: CategoryReference.find_by(code: 'solicitor')) 
+  }
+
+  given!(:contact_3) { create(:contact, 
+                           name: 'HMP Wellingsville',
+                           address_line_1: '789 some lane',
+                           address_line_2: 'little heath',
+                           town: 'bakersville',
+                           county: 'Mercia',
+                           postcode: 'TH8 9KO',
+                           contact_type: CategoryReference.find_by(code: 'prison')) 
+  }
+
   before :all do
     add_contact_types_reference_data
   end
@@ -25,7 +45,6 @@ feature 'Contacts address book', js: true do
     login_as manager
     cases_page.load
   end
-
 
   scenario 'branston user can view addresses and create a new address' do
     # click_on 'Addresses'
@@ -68,6 +87,8 @@ feature 'Contacts address book', js: true do
   scenario "addresses can be selected by name from the 'Find an address' dialog" do
     when_i_navigate_to_offender_sar_subject_page
     and_fill_in_subject_details_page
+
+    when_i_open_the_address_dialogue_only_prison_and_probation_addresses_should_load
     and_i_use_the_search_dialog_to_select_an_address
     then_i_expect_the_address_i_searched_for_to_have_been_entered_into_the_form
   end
@@ -75,10 +96,16 @@ feature 'Contacts address book', js: true do
   def then_i_expect_the_address_i_searched_for_to_have_been_entered_into_the_form
     expect(cases_new_offender_sar_subject_details_page.subject_address).to have_content(contact.address)
   end
-
-  def and_i_use_the_search_dialog_to_select_an_address
+  
+  def when_i_open_the_address_dialogue_only_prison_and_probation_addresses_should_load
     click_link 'Back'
     cases_new_offender_sar_subject_details_page.find_an_address_button.click 
+    expect(page).to have_content('HMP halifax')
+    expect(page).to have_content('HMP Wellingsville')
+    expect(page).to_not have_content('Solicitors LLP')
+  end
+
+  def and_i_use_the_search_dialog_to_select_an_address
     fill_in 'popup-search', with: "HMP HALIFAX"
     click_on 'Search'
     click_on 'Use HMP halifax'
@@ -110,7 +137,7 @@ feature 'Contacts address book', js: true do
   
   def and_user_deletes_an_address
     accept_confirm do
-      click_on 'Delete' 
+      click_on 'Delete', match: :first
     end
   end
 
@@ -129,7 +156,7 @@ feature 'Contacts address book', js: true do
   end
 
   def and_user_can_edit_a_contact
-    click_link('Edit')
+    click_link 'Edit', match: :first
     details = {
       name: 'Granary law',
       address_line_1: '789 another road',
