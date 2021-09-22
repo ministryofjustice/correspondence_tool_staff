1ce628aa073b81e1f4fde9fb61d439756c387cb1
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-09-22 15:24:34 +0100

    Add seeder for category references

---
+++ db/seeders/category_reference_seeder.rb
@@ -0,0 +1,58 @@
+module CategoryReferenceSeeder
+  class ContactTypeSeeder
+    @@category_references = [
+      { 
+        category: 'contact_type',
+        code: 'prison',
+        value: 'Prison',
+        display_order: 10
+      },
+      { 
+        category: 'contact_type',
+        code: 'probation',
+        value: 'Probation centre',
+        display_order: 20
+      },
+      { 
+        category: 'contact_type',
+        code: 'solicitor',
+        value: 'Solicitor',
+        display_order: 30
+      },
+      { 
+        category: 'contact_type',
+        code: 'branston',
+        value: 'Branson',
+        display_order: 40
+      },
+      { 
+        category: 'contact_type',
+        code: 'hmpps_hq',
+        value: 'HMPPS HQ',
+        display_order: 50
+      },
+      { 
+        category: 'contact_type',
+        code: 'hmcts',
+        value: 'HMCTS',
+        display_order: 60
+      },
+      { 
+        category: 'contact_type',
+        code: 'other',
+        value: 'Other',
+        display_order: 70
+      }
+    ]
+
+    def self.seed!
+      @@category_references.each do |category_reference|
+        CategoryReference.find_or_create_by!(category_reference)
+      end
+    end
+
+    def self.unseed!
+      CategoryReference.where(category: :contact_type).delete_all
+    end
+  end
+end
