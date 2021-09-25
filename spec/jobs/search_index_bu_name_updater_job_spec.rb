3d69c59028f7a51c34d7117a6df3066450c08cd2
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-09-25 23:16:18 +0100

    Fixed the failed test spec

--- spec/jobs/search_index_bu_name_updater_job_spec.rb
+++ spec/jobs/search_index_bu_name_updater_job_spec.rb
@@ -14,18 +14,10 @@ describe SearchIndexBuNameUpdaterJob, type: :job do
     it 'updates the index for all cases responded to by responding team 1' do
       expect(BusinessUnit).to receive(:find).with(responding_team_1.id).and_return(responding_team_1)
       expect(responding_team_1).to receive(:responding_cases).and_return( [ k1, k2 ])
-      expect(k1).to receive(:update_index)
-      expect(k2).to receive(:update_index)
+      expect(SearchIndexUpdaterJob).to receive(:perform_later).with(k1.id)
+      expect(SearchIndexUpdaterJob).to receive(:perform_later).with(k2.id)
 
       SearchIndexBuNameUpdaterJob.new.perform(responding_team_1.id)
-    end
-
-    it 'sets the clean flag on cases it has re-indexed' do
-      k1; k2; k3
-      SearchIndexBuNameUpdaterJob.new.perform(responding_team_1.id)
-      expect(k1.reload).to be_clean
-      expect(k2.reload).to be_clean
-      expect(k3.reload).to be_dirty
     end
   end
 end
--- spec/jobs/search_index_updater_job_spec.rb
+++ spec/jobs/search_index_updater_job_spec.rb
@@ -3,28 +3,19 @@ require 'rails_helper'
 describe SearchIndexUpdaterJob, type: :job do
   include ActiveJob::TestHelper
 
-  let!(:k1)     { create :case, :clean }
-  let!(:k2)     { create :case }
-  let!(:k3)     { create :case }
-  let!(:k4)     { create :case, :clean }
-
+  let!(:k1)     { create :case }
 
   describe '#perform' do
-    it 'processes k1 and k4' do
-      expect(Case::Base).to receive(:where).with({dirty: true}).and_return([k2, k3])
+    it 'processes k1' do
+      expect(Case::Base).to receive(:find).with(k1.id).and_return(k1)
 
-      expect(k1).not_to receive(:update_index)
-      expect(k4).not_to receive(:update_index)
-      expect(k2).to receive(:update_index)
-      expect(k3).to receive(:update_index)
-
-      SearchIndexUpdaterJob.new.perform
+      expect(k1).to receive(:update_index)
+      SearchIndexUpdaterJob.new.perform(k1.id)
     end
 
     it 'sets k1 and k4 to clean' do
-      SearchIndexUpdaterJob.new.perform
+      SearchIndexUpdaterJob.new.perform(k1.id)
       expect(k1.reload).to be_clean
-      expect(k4.reload).to be_clean
     end
   end
 end
