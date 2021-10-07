bf9a1b8534f09ef6d5b51042aba45f1a3989a9a7
Will McBrien <william.mcbrien@digital.justice.gov.uk>
2021-10-07 13:47:57 +0100

    CT-3481 Make count tab values to reflect filters (#1753)
    * Allow count tab values to reflect filters
    
    Co-authored-by: ymao2 <yikang.mao@digital.justice.gov.uk>

---
+++ spec/site_prism/page_objects/sections/cases/type_complaint_filter_panel_section.rb
@@ -0,0 +1,20 @@
+module PageObjects
+  module Sections
+    module Cases
+      class TypeComplaintFilterPanelSection < SitePrism::Section
+        # The following checkboxes are invisible because ... govuk form
+        # elements. Check for visibility on the panel section, not the
+        # checkboxes
+        element :complaint_standard_checkbox,
+                '#search_query_filter_complaint_type_standard_complaint',
+                visible: false
+        element :complaint_ico_checkbox,
+                '#search_query_filter_complaint_type_ico_complaint',
+                visible: false
+        element :complaint_litigation_checkbox,
+                '#search_query_filter_complaint_type_litigation_complaint',
+                visible: false
+      end
+    end
+  end
+end
