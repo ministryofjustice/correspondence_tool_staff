caf33b2e2dfef49f105982fd7cfa8966f63ca0ad
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-10-07 12:26:01 +0100

    CT-2424 Hide the note field if reason is other (#1759)

--- spec/site_prism/page_objects/pages/cases/edit/offender_sar_page_record_reason_for_lateness.rb
+++ spec/site_prism/page_objects/pages/cases/edit/offender_sar_page_record_reason_for_lateness.rb
@@ -13,10 +13,14 @@ module PageObjects
                   PageObjects::Sections::PageHeadingSection, '.page-heading'
 
           element :submit_button, '.button'
+          element :note_for_reason, '#offender_sar_reason_for_lateness_note'
 
-          def choose_reason(reason_choice)
+          def choose_reason(reason_choice, reason_note: nil)
             if reason_choice.present?
               choose("offender_sar_reason_for_lateness_id_#{reason_choice.id}", visible: false)
+            end
+            if reason_choice.code == 'other' && reason_note.present?
+              note_for_reason.set(reason_note)
             end
           end
         end
