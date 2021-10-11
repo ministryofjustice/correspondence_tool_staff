74c03e9e43bd6b01e8d1072c7f6fb77f05bc0843
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-10-11 13:05:39 +0100

    refactored the tetsts spec parts based on the feedback

--- spec/features/cases/offender_sar/add_data_requests_spec.rb
+++ spec/features/cases/offender_sar/add_data_requests_spec.rb
@@ -104,22 +104,9 @@ feature 'Data Requests for an Offender SAR' do
       date_to: Date.new(2019, 8, 15),
     }
 
-    cases_show_page.load(id: offender_sar_case.id)
-    click_on 'Record data request'
-
-    data_request_page.form.location.fill_in(with: request_values[:location])
-    data_request_page.form.choose_request_type('nomis_other')
-    data_request_page.form.request_type_note_for_nomis.fill_in(with: request_values[:request_type_note])
-    data_request_page.form.set_date_requested(request_values[:date_requested])
-    click_on 'Continue'
-
-    expect(cases_show_page).to be_displayed
-    row = cases_show_page.data_requests.rows[0]
-    expect(row.location).to have_text request_values[:location]
-    expect(row.request_type).to have_text 'NOMIS other'
-    expect(row.request_type).to have_text request_values[:request_type_note]
-    expect(row.date_requested).to have_text request_values[:date_requested].strftime(Settings.default_date_format)
-    data_requests = cases_show_page.data_requests.rows
-    expect(data_requests.size).to eq 1
+    record_a_data_request_of_nomis_other(offender_sar_case, request_values)
+    validate_nomis_other_info(request_values)
   end
+
+
 end
