3298d8c1cdcb7184dd8fbee057114a38ee8ce81b
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-10-19 11:44:45 +0100

    Add check for revealing panelbe
    Contanerises the code so it doesn't effect other pages

--- app/assets/javascripts/modules/RequesterDetails.js
+++ app/assets/javascripts/modules/RequesterDetails.js
@@ -2,28 +2,32 @@ moj.Modules.RequesterDetails = {
     $is_solicitor: $("#offender_sar_is_solicitor_solicitor"),
     $is_other : $("#offender_sar_is_solicitor_other"),
     $is_third_party: $("#offender_sar_third_party_true"),
+    $revealing_panel_address_page_check: $(".js-in-revealing-panel-address-lookup").length,
 
 
     init: function() {
         var self = this;
-        self.$is_third_party.on('change', function(){
-            self.set_and_hide_relationship();
-        });
-        self.$is_other.on('change', function(){
-            self.show_relationship_input();
-            var address_button = $('#open-button');
-            address_button.hide(); 
-        });
-        self.$is_solicitor.on('change', function(){
-            self.set_and_hide_relationship();
-            var address_button = $('#open-button');
-            address_button.show(); 
-        });
 
-        $( document ).ready(function() {
-            console.log("hi");
-            self.on_load();
-        });
+        if (self.$revealing_panel_address_page_check) {
+            self.$is_third_party.on('change', function(){
+                self.set_and_hide_relationship();
+            });
+            self.$is_other.on('change', function(){
+                self.show_relationship_input();
+                var address_button = $('#open-button');
+                address_button.hide(); 
+            });
+            self.$is_solicitor.on('change', function(){
+                self.set_and_hide_relationship();
+                var address_button = $('#open-button');
+                address_button.show(); 
+            });
+
+            $( document ).ready(function() {
+                console.log("hi");
+                self.on_load();
+            });
+        }
     },
 
     set_and_hide_relationship: function() {
