06a8aaa2bc2633516f2963a448336f397350dbc2
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-11-10 13:07:55 +0000

    Remove unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -114,7 +114,6 @@ Rails.application.routes.draw do
 
     resources :sar_internal_review, only: only, controller: 'sar_internal_review', as: :case_sar_internal_review do
       get '/(:step)', on: :collection, to: 'sar_internal_review#new', as: 'step'
-      post '/sar_internal_review/(:number)', on: :collection, to: 'sar_internal_review#start_internal_review', as: 'start_internal_review'
     end
 
     resources :icos, only: only, controller: 'ico', as: :case_ico do
