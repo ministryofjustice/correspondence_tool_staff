ad9b00994a881044637d1c474deb9455bee79329
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-12-01 16:12:31 +0000

    Fix error messages

--- app/models/case/sar/internal_review.rb
+++ app/models/case/sar/internal_review.rb
@@ -6,18 +6,16 @@ class Case::SAR::InternalReview < Case::SAR::Standard
 
   attr_accessor :original_case_number
 
-  validate :validate_email
-  validate :validate_postal_address
-  validate :name
-  validate :validate_subject
-  validate :validate_third_party_relationship
-
-  after_initialize :remove_conditional_validators
-
   validates_presence_of :sar_ir_subtype
 
   jsonb_accessor :properties,
                  sar_ir_subtype: :string
+
+  HUMANIZED_ATTRIBUTES = {
+    sar_ir_subtype: 'Case type',
+    subject: 'Case summary',
+    message: 'Full case details'
+  }
 
   class << self
     def type_abbreviation
@@ -30,89 +28,15 @@ class Case::SAR::InternalReview < Case::SAR::Standard
     def state_machine_name
       'sar'
     end
+
+    def human_attribute_name(attr, options = {})
+      HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+    end
   end
 
   enum sar_ir_subtype: {
     timeliness: 'timeliness',
     compliance: 'compliance'
   }
-
-  private
-
-  def validate_email
-    if send_by_email? && email.blank?
-      errors.add(
-        :email,
-        :blank
-      )
-    end
-  end
-
-  def validate_name
-    if third_party && name.blank?
-        errors.add(
-          :name,
-          :blank
-        )
-    end
-  end
-
-  def validate_postal_address
-    if send_by_post? && postal_address.blank?
-      errors.add(
-        :postal_address,
-        :blank
-      )
-    end
-  end
-
-  def validate_subject
-    if subject.blank?
-      errors.add(
-        :subject,
-        :blank
-      )
-    end
-
-    if subject.present? && subject.size > 100
-      errors.add(
-        :subject,
-        message: "Subject must be under 100 characters in length"
-      )
-    end
-  end
-
-  def validate_third_party_relationship
-    if third_party && third_party_relationship.blank?
-      errors.add(
-        :third_party_relationship,
-        :blank
-      )
-    end
-  end
-
-  def remove_conditional_validators
-    # This method is called in SarInternalReview 
-    # due to an issue whereby the inline conditional 
-    # validation declarations in the parent class 
-    # (Case::SAR::Standard) are not respected by the 
-    # .valid_attributes? method in ApplicationRecord.
-    #
-    # This causes the validations to fire incorrectly.
-    # This method removes the inherited validators
-    # which are then replaced with validator methods
-    # in this class which are respected by
-    # .valid_attributes? in ApplicationRecord
-    parent_class_validators_to_remove = [
-      :name,
-      :email,
-      :postal_address,
-      :third_party_relationship,
-      :subject
-    ]
-    _validators.reject! do |key, _val| 
-      parent_class_validators_to_remove.include?(key)
-    end
-  end
 
 end
