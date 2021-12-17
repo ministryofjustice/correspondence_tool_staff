531beb499b4c3a561ec635d80f4fdf217d50cc19
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-12-17 15:09:30 +0000

    Add sar ir workflow spec

--- spec/factories/case/sar_internal_reviews.rb
+++ spec/factories/case/sar_internal_reviews.rb
@@ -33,7 +33,7 @@ FactoryBot.define do
   factory :sar_internal_review, class: Case::SAR::InternalReview do
     transient do
       creation_time       { 4.business_days.ago }
-      identifier          { "new sar ir case" }
+      identifier          { "new sar internal review case" }
       managing_team       { find_or_create :team_dacu }
       manager             { managing_team.managers.first }
       responding_team     { find_or_create :sar_responding_team }
@@ -111,7 +111,7 @@ FactoryBot.define do
       end
     end
 
-    trait :extended_deadline_sar do
+    trait :extended_deadline_sar_internal_review do
       after(:create) do |kase, evaluator|
         create :case_transition_extend_sar_deadline_by_30_days,
          case: kase,
@@ -123,164 +123,191 @@ FactoryBot.define do
     end
   end
 
-  # factory :awaiting_responder_sar, parent: :sar_internal_review, aliases: [:assigned_sar], class: Case::SAR::Standard do
-  #   transient do
-  #     identifier { "assigned sar" }
-  #   end
+  factory :awaiting_responder_sar_internal_review, parent: :sar_internal_review, aliases: [:assigned_sar_internal_review], class: Case::SAR::InternalReview do
+    transient do
+      identifier { "assigned sar internal review" }
+    end
 
-  #   created_at      { creation_time }
-  #   received_date   { creation_time }
+    created_at      { creation_time }
+    received_date   { creation_time }
 
-  #   after(:create) do |kase, evaluator|
-  #     create :assignment,
-  #            case: kase,
-  #            team: evaluator.responding_team,
-  #            state: 'pending',
-  #            role: 'responding',
-  #            created_at: evaluator.creation_time
-  #     create :case_transition_assign_responder,
-  #            case: kase,
-  #            acting_user: evaluator.manager,
-  #            acting_team: evaluator.managing_team,
-  #            target_team: evaluator.responding_team,
-  #            created_at: evaluator.creation_time
-  #     kase.reload
-  #   end
-  # end
+    after(:create) do |kase, evaluator|
+      create :assignment,
+             case: kase,
+             team: evaluator.responding_team,
+             state: 'pending',
+             role: 'responding',
+             created_at: evaluator.creation_time
+      create :case_transition_assign_responder,
+             case: kase,
+             acting_user: evaluator.manager,
+             acting_team: evaluator.managing_team,
+             target_team: evaluator.responding_team,
+             created_at: evaluator.creation_time
+      kase.reload
+    end
+  end
 
-  # factory :accepted_sar, parent: :assigned_sar,
-  #         aliases: [:sar_being_drafted] do
-  #   transient do
-  #     identifier { "accepted sar" }
-  #   end
 
-  #   after(:create) do |kase, evaluator|
-  #     responder = evaluator.responder || responding_team.responders.first
-  #     kase.responder_assignment.update_attribute :user, responder
-  #     kase.responder_assignment.accepted!
-  #     create :case_transition_accept_responder_assignment,
-  #            case: kase,
-  #            acting_user: kase.responder,
-  #            acting_team: kase.responding_team,
-  #            created_at: evaluator.creation_time
-  #     kase.reload
-  #   end
-  # end
+  factory :accepted_sar_internal_review, parent: :assigned_sar_internal_review,
+          aliases: [:sar_internal_review_being_drafted] do
+    transient do
+      identifier { "accepted sar internal review" }
+    end
 
-  # factory :pending_dacu_clearance_sar, parent: :accepted_sar do
-  #   transient do
-  #     flag_for_disclosure { :accepted }
-  #   end
+    after(:create) do |kase, evaluator|
+      responder = evaluator.responder || responding_team.responders.first
+      kase.responder_assignment.update_attribute :user, responder
+      kase.responder_assignment.accepted!
+      create :case_transition_accept_responder_assignment,
+             case: kase,
+             acting_user: kase.responder,
+             acting_team: kase.responding_team,
+             created_at: evaluator.creation_time
+      kase.reload
+    end
+  end
 
-  #   after(:create) do |kase, evaluator|
-  #     create :case_transition_progress_for_clearance,
-  #            case: kase,
-  #            acting_team: evaluator.responding_team,
-  #            acting_user: evaluator.responder,
-  #            target_team: evaluator.approving_team
-  #     kase.reload
+  factory :pending_dacu_clearance_sar_internal_review, parent: :accepted_sar_internal_review do
+    transient do
+      flag_for_disclosure { :accepted }
+    end
 
-  #   end
-  # end
+    after(:create) do |kase, evaluator|
+      create :case_transition_progress_for_clearance,
+             case: kase,
+             acting_team: evaluator.responding_team,
+             acting_user: evaluator.responder,
+             target_team: evaluator.approving_team
+      kase.reload
 
-  # factory :approved_sar, parent: :pending_dacu_clearance_sar do
-  #   transient do
-# # date draft compliant is passed in in a transient blocked so it can is be
-# # changed in the tests. It is added to the the case in the after create block
-# # to match the order the code updates the case.
-  #     date_draft_compliant { received_date + 2.days }
-  #   end
+    end
+  end
 
-  #   after(:create) do |kase, evaluator|
-  #     create :case_transition_approve,
-  #            case: kase,
-  #            acting_team: evaluator.approving_team,
-  #            acting_user: evaluator.approver
+  factory :awaiting_dispatch_sar_internal_review, parent: :pending_dacu_clearance_sar_internal_review do
+    transient do
+      flag_for_disclosure { :accepted }
+    end
 
-  #     kase.approver_assignments.each { |a| a.update approved: true }
-  #     kase.update!(date_draft_compliant: evaluator.date_draft_compliant)
-  #     kase.reload
-  #   end
-  # end
+    after(:create) do |kase, evaluator|
+      create :case_transition_add_responses,
+             case: kase,
+             acting_team: evaluator.responding_team,
+             acting_user: evaluator.responder,
+             target_team: evaluator.approving_team
+      kase.reload
 
-  # factory :amends_requested_sar, parent: :pending_dacu_clearance_sar do
-  #   transient do
-  #     is_draft_compliant? { true }
-  #   end
+    end
+  end
 
-  #   after(:create) do |kase, evaluator|
-  #     transition = create :case_transition_request_amends,
-  #                         case: kase,
-  #                         acting_team: evaluator.approving_team,
-  #                         acting_user: evaluator.approver
-  #     if evaluator.is_draft_compliant?
-  #       kase.update!(date_draft_compliant: transition.created_at)
-  #     end
-  #     kase.approver_assignments.each { |a| a.update approved: true }
-  #     kase.reload
-  #   end
-  # end
+  factory :responded_sar_internal_review, parent: :awaiting_dispatch_sar_internal_review do
+    transient do
+      identifier { "responded case" }
+    end
 
-  # factory :closed_sar, parent: :accepted_sar do
-  #   missing_info { false }
+    date_responded { Date.today }
 
-  #   transient do
-  #     identifier { "closed sar" }
-  #   end
+    after(:create) do |kase, evaluator|
+      create :case_transition_respond,
+             case: kase,
+             acting_user: evaluator.responder,
+             acting_team: evaluator.responding_team
+      kase.reload
+    end
+  end
 
-  #   received_date  { 22.business_days.ago }
-  #   date_responded { 4.business_days.ago }
+  factory :approved_sar_internal_review, parent: :pending_dacu_clearance_sar_internal_review do
+    transient do
+# date draft compliant is passed in in a transient blocked so it can is be
+# changed in the tests. It is added to the the case in the after create block
+# to match the order the code updates the case.
+      date_draft_compliant { received_date + 2.days }
+    end
 
-  #   after(:create) do |kase, evaluator|
-  #     if evaluator.flag_for_disclosure
-  #       create :case_transition_progress_for_clearance,
-  #              case: kase,
-  #              acting_team: evaluator.responding_team,
-  #              acting_user: evaluator.responder,
-  #              target_team: evaluator.approving_team
+    after(:create) do |kase, evaluator|
+      create :case_transition_approve,
+             case: kase,
+             acting_team: evaluator.approving_team,
+             acting_user: evaluator.approver
 
-  #       create :case_transition_approve,
-  #              case: kase,
-  #              acting_team: evaluator.approving_team,
-  #              acting_user: evaluator.approver
-  #     end
+      kase.approver_assignments.each { |a| a.update approved: true }
+      kase.update!(date_draft_compliant: evaluator.date_draft_compliant)
+      kase.reload
+    end
+  end
 
-  #     create :case_transition_respond,
-  #            case: kase,
-  #            acting_user: evaluator.responder,
-  #            acting_team: evaluator.responding_team,
-  #            target_user: evaluator.responder,
-  #            target_team: evaluator.responding_team
-  #     create :case_transition_close,
-  #            case: kase,
-  #            acting_user: evaluator.manager,
-  #            acting_team: evaluator.managing_team,
-  #            target_team: evaluator.responding_team
-  #     kase.reload
-  #   end
-  # end
+  factory :amends_requested_sar_internal_review, parent: :pending_dacu_clearance_sar_internal_review do
+    transient do
+      is_draft_compliant? { true }
+    end
 
-  # factory :closed_sar_with_response, parent: :closed_sar do
-  #   transient do
-  #     identifier { "closed sar case with response" }
-  #     responses { [build(:correspondence_response, type: 'response', user_id: responder.id)] }
-  #   end
+    after(:create) do |kase, evaluator|
+      transition = create :case_transition_request_amends,
+                          case: kase,
+                          acting_team: evaluator.approving_team,
+                          acting_user: evaluator.approver
+      if evaluator.is_draft_compliant?
+        kase.update!(date_draft_compliant: transition.created_at)
+      end
+      kase.approver_assignments.each { |a| a.update approved: true }
+      kase.reload
+    end
+  end
 
-  #   after(:create) do |kase, evaluator|
-  #     kase.attachments.push(*evaluator.responses)
+  factory :closed_sar_internal_review, parent: :accepted_sar_internal_review do
+    missing_info { false }
 
-  #     create :case_transition_add_responses_without_state_change_for_sar,
-  #            case: kase,
-  #            acting_team: kase.managing_team,
-  #            acting_user: kase.manager,
-  #            filenames: [evaluator.responses.map(&:filename)]
-  #     kase.reload
-  #   end
-  # end
+    transient do
+      identifier { "closed sar" }
+    end
 
-  # trait :clarification_required do
-  #   refusal_reason              { find_or_create :refusal_reason, :sar_tmm }
-  #   missing_info                { true }
-  #   message                     { 'info held other, clarification required' }
-  # end
+    received_date  { 22.business_days.ago }
+    date_responded { 4.business_days.ago }
+
+    after(:create) do |kase, evaluator|
+      if evaluator.flag_for_disclosure
+        create :case_transition_progress_for_clearance,
+               case: kase,
+               acting_team: evaluator.responding_team,
+               acting_user: evaluator.responder,
+               target_team: evaluator.approving_team
+
+        create :case_transition_approve,
+               case: kase,
+               acting_team: evaluator.approving_team,
+               acting_user: evaluator.approver
+      end
+
+      create :case_transition_respond,
+             case: kase,
+             acting_user: evaluator.responder,
+             acting_team: evaluator.responding_team,
+             target_user: evaluator.responder,
+             target_team: evaluator.responding_team
+      create :case_transition_close,
+             case: kase,
+             acting_user: evaluator.manager,
+             acting_team: evaluator.managing_team,
+             target_team: evaluator.responding_team
+      kase.reload
+    end
+  end
+
+  factory :closed_sar_internal_review_with_response, parent: :closed_sar_internal_review do
+    transient do
+      identifier { "closed sar case with response" }
+      responses { [build(:correspondence_response, type: 'response', user_id: responder.id)] }
+    end
+
+    after(:create) do |kase, evaluator|
+      kase.attachments.push(*evaluator.responses)
+
+      create :case_transition_add_responses_without_state_change_for_sar_internal_review,
+             case: kase,
+             acting_team: kase.managing_team,
+             acting_user: kase.manager,
+             filenames: [evaluator.responses.map(&:filename)]
+      kase.reload
+    end
+  end
 end
