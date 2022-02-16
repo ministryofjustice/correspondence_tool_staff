b2a422be7b1d57193e14d39459a7f6ff8a928b12
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-02-16 16:37:39 +0000

    Update r102 for SAR IRs

--- app/services/stats/r102_sar_appeals_performance_report.rb
+++ app/services/stats/r102_sar_appeals_performance_report.rb
@@ -5,7 +5,8 @@ module Stats
       [
         ["#{self.class.title} - #{reporting_period}"],
         R002_SPECIFIC_SUPERHEADINGS
-            .merge(AppealAnalyser::ICO_APPEAL_SUPERHEADINGS).values
+            .merge(AppealAnalyser::ICO_APPEAL_SUPERHEADINGS)
+            .merge(AppealAnalyser::SAR_IR_APPEAL_SUPERHEADINGS).values
       ]
     end
 
@@ -14,11 +15,13 @@ module Stats
     end
 
     def self.description
-      'Shows all ICO appeals which are open, or have been closed this month, analysed by timeliness'
+      'Shows all ICO appeals, and SAR IRs which are open, or have been closed this month, analysed by timeliness'
     end
 
     def case_ids
-      CaseSelector.new(Case::ICO::SAR.all).ids_for_period_appeals(@period_start, @period_end)
+      ico_sar_case_ids = CaseSelector.new(Case::ICO::SAR.all).ids_for_period_appeals(@period_start, @period_end)
+      sar_ir_case_ids = CaseSelector.new(Case::SAR::InternalReview.all).ids_for_period(@period_start, @period_end)
+      ico_sar_case_ids + sar_ir_case_ids
     end
 
     def report_type
@@ -30,10 +33,11 @@ module Stats
     def column_headings
       R002_SPECIFIC_COLUMNS
           .merge(AppealAnalyser::ICO_APPEAL_COLUMNS)
+          .merge(AppealAnalyser::SAR_IR_APPEAL_COLUMNS)
     end
 
     def appeal_types
-      %w{ ico }
+      %w{ ico sar_ir }
     end
   end
 end
