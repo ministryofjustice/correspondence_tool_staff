e74b27936d7dd414ed42b2b1285579a7120d858f
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-02-24 10:40:53 +0000

    Add SAR IR roles to BUs with access to SARs

---
+++ db/data_migrations/20220224094726_add_sar_ir_ct_role_to_bus_sars.rb
@@ -0,0 +1,55 @@
+class AddSarIrCtRoleToBusSars < ActiveRecord::DataMigration
+  def up
+    sar_ct_id = CorrespondenceType.sar.id
+    bu_with_sars = BusinessUnit
+                    .joins(:correspondence_type_roles)
+                    .where('correspondence_type_roles.correspondence_type_id' => sar_ct_id)
+
+    bu_with_sars.each do |bus_unit|
+      case bus_unit.role
+      when 'manager'
+        set_correspondence_type_roles_for_sar_ir(
+          bus_unit: bus_unit,
+          roles: %w{ view edit manage }
+        )
+      when 'responder'
+        set_correspondence_type_roles_for_sar_ir(
+          bus_unit: bus_unit,
+          roles: %w{ view respond }
+        )
+      when 'approver'
+        set_correspondence_type_roles_for_sar_ir(
+          bus_unit: bus_unit,
+          roles: %w{ view approve }
+        )
+      end
+
+      bus_unit.save
+    end
+  end
+
+  def set_correspondence_type_roles_for_sar_ir(bus_unit:, roles:)
+    ct = CorrespondenceType.sar_internal_review
+    tcr = TeamCorrespondenceTypeRole.find_by(team_id: bus_unit.id, correspondence_type_id: cat.id)
+    if tcr.nil?
+      TeamCorrespondenceTypeRole.create(
+        team_id: bus_unit,
+        correspondence_type_id: ct.id,
+        roles: roles
+      )
+    end
+  end
+
+  def down
+    sar_ir_ct_id = CorrespondenceType.sar_internal_review.id
+
+    bu_with_sars = BusinessUnit
+                    .joins(:correspondence_type_roles)
+                    .where('correspondence_type_roles.correspondence_type_id' => sar_ir_ct_id)
+    
+    sar_ir_ct_team_roles = TeamCorrespondenceTypeRole
+                             .where(team_id: bu_with_sars.ids, 
+                                    correspondence_type_id: sar_ir_ct_id)
+    sar_ir_ct_team_roles.delete_all
+  end
+end
