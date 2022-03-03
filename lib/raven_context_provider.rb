b31eff0e31ce60b13680b31d2a3a9b2a1caa6c62
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-03-03 14:29:13 +0000

    Upgrade sentry-raven to sentry-ruby and sentry-rails

--- lib/raven_context_provider.rb
+++ lib/raven_context_provider.rb
@@ -3,14 +3,13 @@ class RavenContextProvider
   def self.set_context(controller = nil)
     if controller.respond_to?(:current_user) && controller.current_user.present?
       user = controller.current_user
-      Raven.user_context(id: user.id, email: user.email)
+      Sentry.set_user(id: user.id, email: user.email)
     end
 
-    Raven.extra_context(
+    Sentry.set_extras(
       host_environment: ENV['ENV'] || 'Not set',
       build_date: ENV['APP_BUILD_DATE'],
       git_commit_sha: ENV['APP_GIT_COMMIT']
     )
-
   end
 end
