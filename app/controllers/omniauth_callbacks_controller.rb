6211f9a09442b456e808207b49f1bf9c00ea3223
Jesus Laiz <zheileman@users.noreply.github.com>
2022-03-22 10:10:55 +0000

    CT-4073 Implement SSO with Azure Active Directory
    This PR implements SSO user journey with Azure Active Directory, through the already existing Devise gem and its OmniAuth capability.
    
    The feature can be enabled/disabled through the `enabled_features` mechanism configured in `config/settings.yml`
    At the time of this PR it is enabled on Local, for developers to test and play with it.
    
    For more details in Confluence:
    https://dsdmoj.atlassian.net/wiki/spaces/STSDD/pages/3925574013/Single+Sign-on

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+
+  def azure_activedirectory_v2
+    user = User.active_users.find_by!('email ILIKE ?', auth_info['email'])
+
+    sign_in_and_redirect(
+      user, event: :authentication
+    )
+  end
+
+  private
+
+  def auth_info
+    request.env['omniauth.auth']['info']
+  end
+
+  def user_not_found
+    set_flash_message(:alert, :not_found, scope: translation_scope)
+    redirect_to after_omniauth_failure_path_for(:user)
+  end
+end
