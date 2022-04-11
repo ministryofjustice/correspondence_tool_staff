8226c35c2dadb5b11719a4215d35668459b11d3b
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-04-11 09:33:03 +0100

    Remove a console.log in js file

--- app/assets/javascripts/modules/RetentionSchedules.js
+++ app/assets/javascripts/modules/RetentionSchedules.js
@@ -2,7 +2,6 @@ moj.Modules.RetentionSchedules = {
     $select_all_checkbox: $("#retention-select-all-checkbox"),
 
     init: function() {
-        console.log('loaded')
         var self = this;
         self.$select_all_checkbox.change(function () {
             $("input:checkbox").prop('checked', $(this).prop("checked"));
