616102a9b879725a04259a38e8195d6d15f5d836
David Silva <Davidslv@users.noreply.github.com>
2022-06-28 13:29:56 +0100

    Rubocop ignore Rails/ActiveRecordAliases

--- app/services/case_require_further_action_service.rb
+++ app/services/case_require_further_action_service.rb
@@ -15,7 +15,10 @@ class CaseRequireFurtherActionService
   def call
     begin
       ActiveRecord::Base.transaction do
+        # rubocop:disable Rails/ActiveRecordAliases
         update_attributes
+        # rubocop:enable Rails/ActiveRecordAliases
+
         if validate_dates?
           save_attributes
           trigger_flow_action
@@ -31,9 +34,9 @@ class CaseRequireFurtherActionService
       @error_message = error.message
       @result = :error
     end
-  end 
-    
-  private 
+  end
+
+  private
 
   def update_attributes
     update_deadline_attributes
@@ -67,21 +70,21 @@ class CaseRequireFurtherActionService
 
   def validate_dates?
     if require_update_deadlines?
-      # Check whether the deadline dates are in the past, this validation rule is not 
+      # Check whether the deadline dates are in the past, this validation rule is not
       # suitable on the data model level as users may changed them after initial action
       if @kase.internal_deadline <= Date.today
         @kase.errors.add(
           :internal_deadline,
           I18n.t('activerecord.errors.models.case/ico.attributes.internal_deadline.past')
         )
-      end 
+      end
 
       if @kase.external_deadline <= Date.today
         @kase.errors.add(
           :external_deadline,
           I18n.t('activerecord.errors.models.case/ico.attributes.external_deadline.past')
         )
-      end 
+      end
     end
     !@kase.errors.present?
   end
@@ -90,13 +93,13 @@ class CaseRequireFurtherActionService
     team = @user.case_team_for_event(@kase, 'record_further_action')
     determine_event_name
     @kase.state_machine.send(@trigger_event_name + '!', {
-      acting_user: @user, 
+      acting_user: @user,
       acting_team: team,
-      target_team: @target_team, 
-      message: I18n.t("event.case/ico.#{@trigger_event_name}_message", team: @target_team.nil? ? '' : @target_team.name)   
+      target_team: @target_team,
+      message: I18n.t("event.case/ico.#{@trigger_event_name}_message", team: @target_team.nil? ? '' : @target_team.name)
     })
   end
-  
+
   def add_ico_further_files
     if @update_parameters[:uploaded_request_files].present?
       uploader = S3Uploader.new(@kase, @user)
@@ -107,14 +110,14 @@ class CaseRequireFurtherActionService
   def determine_event_name
     if @kase.responding_team.active?
       check_responder_state
-    else 
+    else
       @trigger_event_name = 'require_further_action_unassigned'
     end
   end
 
   def check_responder_state
     @target_team = @kase.responding_team
-    responder_active = !@kase.responder.deactivated? 
+    responder_active = !@kase.responder.deactivated?
     case_responder_in_responding_team = @kase.responding_team.users.include?(@kase.responder)
     if responder_active && case_responder_in_responding_team
       @trigger_event_name = 'require_further_action'
@@ -122,7 +125,6 @@ class CaseRequireFurtherActionService
       @kase.reset_responding_assignment_flag
       @trigger_event_name = 'require_further_action_to_responder_team'
     end
-  end 
+  end
 
 end
-
