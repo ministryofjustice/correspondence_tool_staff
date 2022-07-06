ec1760735eddcd4bd376d1c3053fbf484f6270fb
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2022-07-06 23:32:14 +0100

    Data migration that sets a default request_method for SAR and Offender SAR cases
    Run using `rake data:migrate` once this appllication version has been deployed in each namespace.
    
    This should not be run for historical SAR IR cases as they don't validate this value

---
+++ db/data_migrations/20220706222050_add_unknown_request_method_to_sars.rb
@@ -0,0 +1,10 @@
+class AddUnknownRequestMethodToSars < ActiveRecord::DataMigration
+  def up
+    Case::Base.non_offender_sar.each { |k| p k.update_attribute(:request_method, "unknown") }
+    Case::Base.offender_sar.each { |k| p k.update_attribute(:request_method, "unknown") }
+  end
+
+  def down
+    # there is no down for this, as the enum will not allow setting this back to nil
+  end
+end
