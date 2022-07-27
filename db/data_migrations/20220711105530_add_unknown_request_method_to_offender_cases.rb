d458ac2c504c7b6d348b4dd011c3268bdf1c8352
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2022-07-27 12:21:52 +0100

    fixing rake data migration for offender sar

--- db/data_migrations/20220711105530_add_unknown_request_method_to_offender_cases.rb
+++ db/data_migrations/20220711105530_add_unknown_request_method_to_offender_cases.rb
@@ -1,5 +1,5 @@
 class AddUnknownRequestMethodToOffenderCases < ActiveRecord::DataMigration
   def up
-    Case::Base.offender_sar.each { |k| k.update_attribute(:request_method, "unknown") unless k.readonly? }
+    Case::Base.offender_sar.find_each { |k| k.update_attribute(:request_method, "unknown") unless k.readonly? }
   end
 end
