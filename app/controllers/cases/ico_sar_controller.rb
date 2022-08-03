1d9796171ef85f3c83810bf4cef287178cfacbec
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2022-08-03 20:57:50 +0100

    ICO SAR record further action almost working! Now reports wrong case status
     1) Require further action for ICO-SAR responded case 1 - Send the case back to the responder
         Failure/Error: expect(cases_show_page.case_status.details.copy.text).to eq post_action_state
    
           expected: "Draft in progress"
                got: "To be accepted"
    
           (compared using ==)
         # ./spec/features/cases/ico/require_further_action_ico_sar_spec.rb:100:in `validate_results'
         # ./spec/features/cases/ico/require_further_action_ico_sar_spec.rb:16:in `block (2 levels) in <top (required

--- app/controllers/cases/ico_sar_controller.rb
+++ app/controllers/cases/ico_sar_controller.rb
@@ -11,13 +11,60 @@ module Cases
       :require_further_action
     ]
 
+    before_action -> { set_case(params[:id]) }, only: [
+      :confirm_record_further_action,
+      :confirm_require_further_action
+    ]
+
     def record_further_action
       authorize @case, :can_record_further_action?
 
       set_permitted_events
       read_info_from_session
       @s3_direct_post = S3Uploader.for(@case, 'requests')
-      render 'cases/ico_foi/record_further_action'
+      render 'cases/ico_sar/record_further_action'
+    end
+
+    def confirm_record_further_action
+      authorize @case, :can_record_further_action?
+      set_permitted_events
+      if params_valid?
+        session_persist_state(params_for_record_further_action)
+        redirect_to require_further_action_case_ico_sar_path(@case)
+      else
+        @case = @case.decorate
+        @s3_direct_post = S3Uploader.for(@case, 'requests')
+        render :record_further_action
+      end
+    end
+
+    def require_further_action
+      authorize @case, :can_require_further_action?
+
+      set_permitted_events
+      init_deadlines
+      render 'cases/ico_sar/require_further_action'
+    end
+
+    def confirm_require_further_action
+      authorize @case, :can_record_further_action?
+      set_permitted_events
+
+      service = CaseRequireFurtherActionService.new(
+        current_user,
+        @case,
+        params_for_requiring_further_action
+      )
+      result = service.call
+      if result == :ok
+        flash[:notice] = prepare_flash_message
+        clear_up_session
+        redirect_to case_path(@case)
+      else
+        @case = @case.decorate
+        flash.now[:alert] = service.error_message
+        render :require_further_action
+      end
     end
 
     def case_type
@@ -25,6 +72,43 @@ module Cases
     end
 
     private
+
+    def init_deadlines
+      @case.internal_deadline = nil
+      @case.external_deadline = nil
+    end
+
+    def prepare_flash_message
+      case @case.current_state
+      when "drafting"
+        notice_message = I18n.t('notices.case/ico.required_further_action.same_responder')
+      when "awaiting_responder"
+        notice_message = I18n.t('notices.case/ico.required_further_action.same_team')
+      else
+        notice_message = I18n.t('notices.case/ico.required_further_action.reassign')
+      end
+      notice_message
+    end
+
+    def params_for_record_further_action
+      case_params = params.require(:ico)
+
+      case_params.permit(
+        :message,
+        uploaded_request_files: [],
+      )
+    end
+
+    def params_for_requiring_further_action
+      case_params = params.require(:ico)
+      case_params.merge! session[session_info_key] || {}
+      case_params.permit(
+        :message,
+        :external_deadline_dd, :external_deadline_mm, :external_deadline_yyyy,
+        :internal_deadline_dd, :internal_deadline_mm, :internal_deadline_yyyy,
+        uploaded_request_files: [],
+      )
+    end
 
     def session_info_key
       "ico_sar_requir_further_action"
@@ -51,11 +135,20 @@ module Cases
       end
     end
 
+    def params_valid?
+      @case.assign_attributes(params_for_record_further_action)
+      @case.valid?
+    end
+
     def read_info_from_session
       clear_up_files_params
       if session[session_info_key].present?
         @case.assign_attributes(session[session_info_key])
       end
+    end
+
+    def clear_up_session
+      session[session_info_key] = {}
     end
   end
 end
