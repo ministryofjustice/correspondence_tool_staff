d88c02624f9e729b58330c3836e5af168dbf299a
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2022-08-11 09:01:55 +0100

    Clean up and document empty ico_sar_controller and ico_foi_controller files

--- app/controllers/cases/ico_sar_controller.rb
+++ app/controllers/cases/ico_sar_controller.rb
@@ -1,13 +1,5 @@
 module Cases
   class IcoSarController < IcoController
-    # def new
-    #   authorize case_type, :can_add_case?
-
-    #   permitted_correspondence_types
-    #   new_case_for @correspondence_type, case_type
-    # end
-    def case_type
-      Case::ICO::SAR
-    end
+    # this class needs to exist
   end
 end
