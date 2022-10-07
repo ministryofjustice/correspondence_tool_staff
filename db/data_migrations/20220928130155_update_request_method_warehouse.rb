f29fed0b432e571370930c29dddf6f70b1b243f2
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2022-10-07 10:03:53 +0100

    Fix the operator to return TEXT result not JSON result, in order to not end up with spurious quotes in the updated column value

--- db/data_migrations/20220928130155_update_request_method_warehouse.rb
+++ db/data_migrations/20220928130155_update_request_method_warehouse.rb
@@ -2,7 +2,7 @@ class UpdateRequestMethodWarehouse < ActiveRecord::DataMigration
   def up
     execute <<-EOF
       UPDATE warehouse_case_reports wcr
-      SET request_method = properties -> 'request_method'
+      SET request_method = properties->>'request_method'
       FROM cases c
       WHERE wcr.case_id = c.id;
     EOF
