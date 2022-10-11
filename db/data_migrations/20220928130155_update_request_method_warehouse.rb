a879724b06d8e0a36ebe3be0b47b84b27149640b
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2022-10-11 23:00:14 +0100

    Comment out migration causing CrashLoopBackoff on production (#2082)

--- db/data_migrations/20220928130155_update_request_method_warehouse.rb
+++ db/data_migrations/20220928130155_update_request_method_warehouse.rb
@@ -1,10 +1,10 @@
 class UpdateRequestMethodWarehouse < ActiveRecord::DataMigration
   def up
-    execute <<-EOF
-      UPDATE warehouse_case_reports wcr
-      SET request_method = properties->>'request_method'
-      FROM cases c
-      WHERE wcr.case_id = c.id;
-    EOF
+    # execute <<-EOF
+    #   UPDATE warehouse_case_reports wcr
+    #   SET request_method = properties->>'request_method'
+    #   FROM cases c
+    #   WHERE wcr.case_id = c.id;
+    # EOF
   end
 end
