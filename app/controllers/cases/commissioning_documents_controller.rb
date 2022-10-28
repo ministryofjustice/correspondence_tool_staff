f02d4ce108d8e774ae06aff39fe22ffe04fc3c96
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2022-10-28 14:59:00 +0100

    remove service and generate in model

--- app/controllers/cases/commissioning_documents_controller.rb
+++ app/controllers/cases/commissioning_documents_controller.rb
@@ -2,23 +2,20 @@ module Cases
   class CommissioningDocumentsController < ApplicationController
     before_action :set_case
     before_action :set_data_request
+    before_action :set_commissioning_document
 
     def new
-      @commissioning_document = CommissioningDocument.new(data_request: @data_request)
     end
 
     def create
-      service = CommissioningDocumentTemplateService.new(
-        data_request: @data_request,
-        template_type: create_params[:template].to_sym,
-      )
+      @commissioning_document.template_name = create_params[:template_name].to_sym
 
-      service.call
-
-      case service.result
-      when :ok
-        send_data service.document, filename: service.filename, type: service.mime_type
-      when :error
+      if @commissioning_document.valid?
+        send_data(@commissioning_document.document,
+          filename: @commissioning_document.filename,
+          type: @commissioning_document.mime_type,
+        )
+      else
         flash[:alert] = t('.error')
         redirect_to new_case_data_request_commissioning_document_path(@case, @data_request)
       end
@@ -34,8 +31,12 @@ module Cases
       @data_request = DataRequest.find(params[:data_request_id])
     end
 
+    def set_commissioning_document
+      @commissioning_document = CommissioningDocument.new(data_request: @data_request)
+    end
+
     def create_params
-      params.require(:commissioning_document).permit(:template)
+      params.require(:commissioning_document).permit(:template_name)
     end
   end
 end
--- app/models/commissioning_document.rb
+++ app/models/commissioning_document.rb
@@ -1,21 +1,50 @@
 class CommissioningDocument
   include ActiveModel::Conversion
   extend  ActiveModel::Naming
+  include ActiveModel::Validations
 
   TEMPLATE_TYPES = {
-    prison: "HMPS",
-    security: "Security",
-    probation: "Probation",
-    cctv: "CCTV",
-    mappa: "MAPPA",
-    pdp: "PDP",
-    cat_a: "CATA",
-    cross_border: "TX",
+    prison: CommissioningDocumentTemplate::Prison,
+    security: CommissioningDocumentTemplate::Security,
+    probation: CommissioningDocumentTemplate::Probation,
+    cctv: CommissioningDocumentTemplate::Cctv,
+    mappa: CommissioningDocumentTemplate::Mappa,
+    pdp: CommissioningDocumentTemplate::Pdp,
+    cat_a: CommissioningDocumentTemplate::CatA,
+    cross_border: CommissioningDocumentTemplate::CrossBorder,
   }.freeze
+
+  attr_accessor :template_name
+
+  validates :template_name, presence: true, inclusion: { in: CommissioningDocument::TEMPLATE_TYPES.keys }
 
   def initialize(data_request:)
     @data_request = data_request
   end
 
-  def template; end
+  def document
+    return unless valid?
+
+    Sablon.template(template.path).render_to_string(template.context)
+  end
+
+  def filename
+    return unless valid?
+
+    number = @data_request.kase.number
+    name = @data_request.kase.subject_full_name.tr(' ', '-')
+    timestamp = Time.current.strftime('%Y%m%dT%H%M')
+    type = template.request_type
+    "Day1_#{type}_#{number}_#{name}_#{timestamp}.docx"
+  end
+
+  def mime_type
+    :docx
+  end
+
+  private
+
+  def template
+    TEMPLATE_TYPES[@template_name].new(data_request: @data_request)
+  end
 end
