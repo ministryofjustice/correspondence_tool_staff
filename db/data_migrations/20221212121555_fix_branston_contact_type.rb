5c4cfb7deb3706a3d1fa3f002edff6b634b57c98
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2022-12-12 14:56:10 +0000

    Fix Branston Contact Type (#2126)

---
+++ db/data_migrations/20221212121555_fix_branston_contact_type.rb
@@ -0,0 +1,13 @@
+class FixBranstonContactType < ActiveRecord::DataMigration
+  def up
+    cat_ref = CategoryReference.find_by(
+      category: 'contact_type',
+      code: 'branston'
+    )
+
+    return unless cat_ref
+
+    cat_ref.value = 'Branston'
+    cat_ref.save
+  end
+end
