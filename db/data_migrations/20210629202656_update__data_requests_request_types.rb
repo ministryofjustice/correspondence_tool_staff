1c2f5ec7b2b3e54be23fc8bc1061bdddf56859cf
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-03-13 13:56:54 +0000

    Update request_type

--- db/data_migrations/20210629202656_update__data_requests_request_types.rb
+++ db/data_migrations/20210629202656_update__data_requests_request_types.rb
@@ -13,7 +13,7 @@ class UpdateDataRequestsRequestTypes < ActiveRecord::DataMigration
 
     execute <<-SQL
       UPDATE data_requests SET request_type = 'other', request_type_note='prison_and_probation_records' where id in (select id from data_requests where request_type='prison_and_probation_records');
-      CREATE TYPE request_types AS ENUM ('all_prison_records', 'security_records', 'nomis_records', 'nomis_other', 'nomis_contact_logs', 'probation_records', 'cctv_and_bwcf', 'telephone_recordings', 'telephone_pin_logs', 'probation_archive', 'mappa', 'pdp', 'court', 'other');
+      CREATE TYPE request_types AS ENUM ('all_prison_records', 'security_records', 'nomis_records', 'nomis_other', 'nomis_contact_logs', 'probation_records', 'cctv_and_bwcf', 'telephone_recordings', 'telephone_pin_logs', 'probation_archive', 'mappa', 'pdp', 'court', 'other', 'cross_borders', 'cat_a', 'ndelius');
       ALTER TABLE data_requests ALTER COLUMN request_type TYPE request_types USING request_type::text::request_types;
       DROP TYPE request_types_enum;
     SQL
