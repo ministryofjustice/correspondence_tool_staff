ef20f10c243f9f496762c6acafcc272663aa1a81
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-03-31 14:15:51 +0100

    Fixed failing test error messages

--- spec/models/case/overturned_ico/foi_spec.rb
+++ spec/models/case/overturned_ico/foi_spec.rb
@@ -108,14 +108,14 @@ describe Case::OverturnedICO::FOI do
           it 'errors' do
             new_case.received_date = 41.days.ago
             expect(new_case).not_to be_valid
-            expect(new_case.errors[:received_date]).to eq ['Date decision received from ICO must be within 40 days of today']
+            expect(new_case.errors[:received_date]).to eq ['decision received from ICO must be within 40 days of today']
           end
         end
 
         context 'on create' do
           it 'errors' do
             record = described_class.create(received_date: 41.days.ago)
-            expect(record.errors[:received_date]).to eq ['Date decision received from ICO must be within 40 days of today']
+            expect(record.errors[:received_date]).to eq ['decision received from ICO must be within 40 days of today']
           end
         end
 
