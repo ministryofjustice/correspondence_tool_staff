a2e80b6849894862134b61861326352d04ef41ee
N.Preddy <nick.preddy@digital.justice.gov.uk>
2023-05-12 09:00:47 +0100

    Test updates
    - Changed Site-Prism page warning object to the new page banner.
    -  Changed references to page_warning to page_banner in spec tests.
    - Added a test to check all spaces and carriage returns are stripped from the recipient email list when displayed.

--- spec/views/cases/data_requests/send_email_html_slim_spec.rb
+++ spec/views/cases/data_requests/send_email_html_slim_spec.rb
@@ -50,7 +50,7 @@ describe 'cases/data_requests/send_email', type: :view do
 
       it 'has required content' do
         expect(@page.page_heading.heading.text).to eq 'Are you sure you want to send the commissioning email?'
-        expect(@page.page_warning.text).to include 'The selected location does not have an email address.'
+        expect(@page.page_banner.text).to include 'The selected location does not have an email address.'
         expect(@page.button_send_email.disabled?).to eq true
         expect(@page.link_cancel.text).to eq 'Cancel'
       end
@@ -73,5 +73,30 @@ describe 'cases/data_requests/send_email', type: :view do
         expect(@page.link_cancel.text).to eq 'Cancel'
       end
     end
+
+    context 'data request with several contact email addresses, each separated by multiple spaces and carriage returns which are removed.' do
+        let(:contact) {
+          create(
+            :contact,
+            data_request_emails: "oscar@grouch.com\s\s\s\n\s\sbig@bird.com\n\ncookie@monster.com\s\n\nmr@snuffleupagus.com\n"
+          )
+        }
+        before do
+          assign(:data_request, data_request)
+          assign(:case, data_request.kase)
+          assign(:commissioning_document, commissioning_document)
+  
+          render
+          data_request_email_confirmation_page.load(rendered)
+          @page = data_request_email_confirmation_page
+        end
+  
+        it 'has required content' do
+          expect(@page.page_heading.heading.text).to eq 'Are you sure you want to send the commissioning email?'
+          expect(@page).to have_text 'oscar@grouch.combig@bird.comcookie@monster.commr@snuffleupagus.com'
+          expect(@page.button_send_email.value).to eq 'Send commissioning email'
+          expect(@page.link_cancel.text).to eq 'Cancel'
+        end
+      end
   end
 end
