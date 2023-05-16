c55fc09714cbd3350b0692c3231b135340ced643
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-05-16 16:40:31 +0100

    Add test

--- spec/site_prism/page_objects/pages/cases/upload_commissioning_document_page.rb
+++ spec/site_prism/page_objects/pages/cases/upload_commissioning_document_page.rb
@@ -1,7 +1,9 @@
 module PageObjects
   module Pages
     module Cases
-      class UploadCommissioningDocumentPage < SitePrism::Page
+      class UploadCommissioningDocumentPage < PageObjects::Pages::Base
+        include SitePrism::Support::DropInDropzone
+
         set_url '/cases/{case_id}/data_requests/{data_request_id}/commissioning_documents/{id}/replace'
 
         section :primary_navigation,
@@ -10,9 +12,18 @@ module PageObjects
         section :page_heading,
           PageObjects::Sections::PageHeadingSection, '.page-heading'
 
-        section :form, '.edit_commissioning_document' do
-          element :choose_file, 'button-secondary'
-          element :submit_button, '.button'
+        element :dropzone_container, '.dropzone'
+        element :file_fields, '#upload'
+        element :upload_document_button, '.button'
+
+        # only shows up when using drop_in_dropzone
+        element :uploaded_request_file_input, '#uploadedRequestFileInput'
+
+        # Upload a file to Dropzone.js
+        def drop_in_dropzone(file_path)
+          super file_path: file_path,
+                input_name: 'commissioning_document[upload][]',
+                container_selector: '.dropzone:first'
         end
       end
     end
