17f4d1d1858ae7773851f9515e604f690d66329c
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-06-07 09:43:03 +0100

    Set default value

--- app/forms/probation_commissioning_document_email.rb
+++ app/forms/probation_commissioning_document_email.rb
@@ -4,12 +4,7 @@ class ProbationCommissioningDocumentEmail
   include ActiveModel::Validations
 
   attribute :email_branston_archives
-  attribute :probation
+  attribute :probation, default: 1
 
   validates :email_branston_archives, presence: true
-
-  def initialize(*)
-    @probation = 1
-    super
-  end
 end
