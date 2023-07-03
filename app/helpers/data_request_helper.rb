ff6d56a7bff7d3cb586dbf426f83f37cc06f0dbc
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-07-03 14:47:30 +0100

    ignore rubocop warning, revert to using instance variable

--- app/helpers/data_request_helper.rb
+++ app/helpers/data_request_helper.rb
@@ -1,6 +1,5 @@
 module DataRequestHelper
   def only_branston_registry_email
-    recipient_emails = instance_variable_get(:@recipient_emails)
-    recipient_emails.empty? || (recipient_emails.count == 1 && recipient_emails.include?(CommissioningDocumentTemplate::Probation::BRANSTON_ARCHIVES_EMAIL))
+    @recipient_emails.empty? || (@recipient_emails.count == 1 && @recipient_emails.include?(CommissioningDocumentTemplate::Probation::BRANSTON_ARCHIVES_EMAIL)) # rubocop:disable Rails::HelperInstanceVariable
   end
 end
