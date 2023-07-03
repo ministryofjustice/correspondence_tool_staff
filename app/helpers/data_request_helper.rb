2004e5e63d145da14956fb4dd8a79b8e5000a3bb
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-07-03 13:29:32 +0100

    use local variables rather than instance variables - rubocop error

--- app/helpers/data_request_helper.rb
+++ app/helpers/data_request_helper.rb
@@ -1,6 +1,6 @@
 module DataRequestHelper
   def only_branston_registry_email
-    @recipient_emails.empty? || (@recipient_emails.count == 1 && @recipient_emails.include?("BranstonRegistryRequests2@justice.gov.uk"))
-
+    recipient_emails = instance_variable_get(:@recipient_emails)
+    recipient_emails.empty? || (recipient_emails.count == 1 && recipient_emails.include?(CommissioningDocumentTemplate::Probation::BRANSTON_ARCHIVES_EMAIL))
   end
 end
