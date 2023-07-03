49349b9bf61872f6b6d6068224f0407864fe2132
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-07-03 10:53:41 +0100

    Created spec test

--- spec/views/cases/data_requests/send_email_html_slim_spec.rb
+++ spec/views/cases/data_requests/send_email_html_slim_spec.rb
@@ -44,6 +44,25 @@ describe "cases/data_requests/send_email", type: :view do
       end
     end
 
+    context 'data request contact with only the branston probation records email given' do
+      before do
+        assign(:data_request, data_request)
+        assign(:case, data_request.kase)
+        assign(:commissioning_document, commissioning_document)
+        assign(:recipient_emails, [CommissioningDocumentTemplate::Probation::BRANSTON_ARCHIVES_EMAIL])
+
+        render
+        data_request_email_confirmation_page.load(rendered)
+        @page = data_request_email_confirmation_page
+      end
+      it 'has required content' do
+        expect(@page.page_heading.heading.text).to eq 'Are you sure you want to send the commissioning email?'
+        expect(@page.page_banner.text).to include 'The selected location does not have an email address.'
+        expect(@page.button_send_email.disabled?).to eq true
+        expect(@page.link_cancel.text).to eq 'Cancel'
+      end
+    end
+
     context "with data request with contact which has email address" do
       before do
         assign(:data_request, data_request)
