bd27412c81c67503969cc60420aff36fa608d202
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-07-03 15:20:48 +0100

    Use instance variable to determine if recipient_emails are populated prio to branston email

--- app/helpers/data_request_helper.rb
+++
@@ -1,5 +0,0 @@
-module DataRequestHelper
-  def only_branston_registry_email
-    @recipient_emails.empty? || (@recipient_emails.count == 1 && @recipient_emails.include?(CommissioningDocumentTemplate::Probation::BRANSTON_ARCHIVES_EMAIL)) # rubocop:disable Rails::HelperInstanceVariable
-  end
-end
