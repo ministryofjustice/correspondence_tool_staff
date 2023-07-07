933fe23544a8f35b89c529f9b4924e6540f4daee
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-07-07 14:12:31 +0100

    Display email details

---
+++ app/decorators/data_request_email_decorator.rb
@@ -0,0 +1,15 @@
+class DataRequestEmailDecorator < Draper::Decorator
+  delegate_all
+
+  def created_at
+    I18n.l(super, format: :default)
+  end
+
+  def email_type
+    I18n.t("helpers.label.data_request_email.email_type.#{super}")
+  end
+
+  def status
+    super.humanize
+  end
+end
