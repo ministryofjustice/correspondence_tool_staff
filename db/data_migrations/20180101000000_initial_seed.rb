e99efa639363fc529a4e6e58512e61ed7d1d796a
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-08-08 17:21:22 +0100

    remove to_s method

--- db/data_migrations/20180101000000_initial_seed.rb
+++ db/data_migrations/20180101000000_initial_seed.rb
@@ -1,8 +1,8 @@
 class InitialSeed < ActiveRecord::DataMigration
   def up
-    require Rails.root.join("db/seeders/correspondence_type_seeder").to_s
-    require Rails.root.join("db/seeders/case_closure_metadata_seeder").to_s
-    require Rails.root.join("db/seeders/report_type_seeder").to_s
+    require Rails.root.join("db/seeders/correspondence_type_seeder")
+    require Rails.root.join("db/seeders/case_closure_metadata_seeder")
+    require Rails.root.join("db/seeders/report_type_seeder")
 
     CorrespondenceTypeSeeder.new.seed!
     ReportTypeSeeder.new.seed!(verbose: true)
