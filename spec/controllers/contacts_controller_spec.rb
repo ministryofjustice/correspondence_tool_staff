6a8d187622a8dde37048b45c7cd8a2b07aa645e0
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-10-24 12:39:34 +0100

    test contacts order

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,18 +1,26 @@
 require "rails_helper"
 
 RSpec.describe ContactsController, type: :controller do
-  let(:contacts) { ["stafford", "winson green", "brinsford"] }
+  let(:user) { create(:user) }
 
-  describe "#index" do
+  let!(:stafford) { create(:contact, name: "stafford") }
+  let!(:winsongreen) { create(:contact, name: "winson green") }
+  let!(:brinsford) { create(:contact, name: "brinsford") }
+
+  describe "GET #index" do
+    before do
+      sign_in user
+    end
+
     it "returns an alphabetically sorted list of contacts" do
       # Assuming the controller action assigns @contacts
       get :index
 
       # Extract the contacts from the controller's instance variable
-      contacts = assigns(:contacts)
+      contacts = assigns(:contacts.name)
 
-      # Check if the contacts are sorted alphabetically
-      expect(contacts).to eq(["brinsford", "stafford", "winson green"])
+      # Check if the contacts are sorted alphabetically by their name
+      expect(contacts).to eq [brinsford, stafford, winsongreen]
     end
   end
 end
