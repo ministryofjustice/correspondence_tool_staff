3bbc16f3f4bb4b95ece0dbba8c276efb0adfe55f
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-10-25 17:05:09 +0100

    Set as const

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,5 +1,19 @@
 BusinessTime::Config.work_week = %w[mon tue wed thu fri]
 
+ADDITIONAL_BANK_HOLIDAYS = [
+  "2023-11-30", # St Andrew's Day
+  "2024-01-02", # 2nd January
+  "2024-03-18", # St Patrick's Day
+  "2024-07-12", # Battle of the Boyne
+  "2024-08-05", # Summer bank holiday
+  "2024-02-12", # St Andrew's Day (substitute day)
+  "2025-01-02", # 2nd January
+  "2025-03-17", # St Paricks's Day
+  "2025-07-14", # Battle of the Boyne (substitute day)
+  "2025-08-04", # Summer bank holiday
+  "2025-12-01", # St Andrew's Day (substitute day)
+].freeze
+
 ew_bank_holidays = %w[
   2016-01-01
   2016-03-25
@@ -85,22 +99,6 @@ ew_bank_holidays = %w[
   2025-12-25
   2025-12-26
 ]
-
-# rubocop:disable Layout/MultilineArrayLineBreaks
-rest_of_uk_additional_holidays = %w[
-  2023-11-30 # St Andrew's Day
-  2024-01-02 # 2nd January
-  2024-03-18 # St Patrick's Day
-  2024-07-12 # Battle of the Boyne
-  2024-08-05 # Summer bank holiday
-  2024-02-12 # St Andrew's Day (substitute day)
-  2025-01-02 # 2nd January
-  2025-03-17 # St Paricks's Day
-  2025-07-14 # Battle of the Boyne (substitute day)
-  2025-08-04 # Summer bank holiday
-  2025-12-01 # St Andrew's Day (substitute day)
-]
-# rubocop:enable Layout/MultilineArrayLineBreaks
 
 hols = if Rails.env.production?
          BankHoliday.all.map(&:date)
