18d31572591c0ab7c712d56e388628d95242eeb5
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-10-30 09:48:14 +0000

    Ad more bank holidays

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,6 +1,8 @@
 BusinessTime::Config.work_week = %w[mon tue wed thu fri]
 
 ADDITIONAL_BANK_HOLIDAYS = [
+  "2023-07-12", # Battle of the Boyne
+  "2023-08-07", # Summer bank holiday
   "2023-11-30", # St Andrew's Day
   "2024-01-02", # 2nd January
   "2024-03-18", # St Patrick's Day
