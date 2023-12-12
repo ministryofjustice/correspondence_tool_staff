c5c3b6b2835ab1abeb500283cc714d415be705ca
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-12-12 15:55:46 +0000

    correct syntax error

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,9 +1,9 @@
 module ViewsHelper
-  def get_offender_sar_heading(offender_sar_type, case)
+  def get_offender_sar_heading(offender_sar_type, kase)
     if offender_sar_type == "accepted"
       content_tag(:span, t4c(kase, "", "case_type.accepted", case_type: kase.pretty_type), class: "page-heading--secondary")
     else
-      content_tag(:span, t(offender_sar.case_type.rejected), class: "page-heading--secondary")
+      content_tag(:span, t4c(kase, "", "case_type.rejected", case_type: kase.pretty_type), class: "page-heading--secondary")
     end
   end
 end
