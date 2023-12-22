798bc11dcdfdbbca8ba0d69d03d3924eb3d79a57
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-12-22 10:25:44 +0000

    rename view helper and add check for case type

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,6 +1,6 @@
 module ViewsHelper
-  def get_sar_heading(kase)
-    if @case.current_state == "rejected" # rubocop:disable Rails/HelperInstanceVariable
+  def get_headings(kase)
+    if current_state == "rejected" && type.abbreviation == "OFFENDER_SAR"
       content_tag(:span, t("cases.new.offender_sar.rejected"), class: "page-heading--secondary")
     else
       t4c(kase, "", "sub_heading", case_type: kase.pretty_type)
