16c1fce30dbcebd04fcde7b5b14b1235c030241e
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-12-22 11:27:07 +0000

    correct syntax

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,6 @@
 module ViewsHelper
-  def get_headings(kase)
-    debugger # rubocop:disable Lint/Debugger
-    if kase.current_state == "rejected" && kase.type.abbreviation == "OFFENDER_SAR"
+  def get_headings(kase, correspondence_type)
+    if kase.current_state == "rejected" && correspondence_type.abbreviation == "OFFENDER_SAR"
       content_tag(:span, t("cases.new.offender_sar.rejected"), class: "page-heading--secondary")
     else
       t4c(kase, "", "sub_heading", case_type: kase.pretty_type)
