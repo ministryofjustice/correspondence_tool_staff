9ec8662531717dc670a93b76199c81c0d059b794
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-05 12:04:53 +0000

    add date variable

--- spec/services/deadline_calculator/business_days_spec.rb
+++ spec/services/deadline_calculator/business_days_spec.rb
@@ -193,6 +193,8 @@ describe DeadlineCalculator::BusinessDays do
     end
 
     describe "#time_taken" do
+      let(:mon_nov_27) { Date.new(2023, 11, 27) }
+      
       it "returns the number of business days taken to respond to a case" do
         Timecop.freeze mon_nov_27 do
           closed_case = create(:closed_case)
