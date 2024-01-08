d16847eb229c54f5993f5c92694ffc2bb63059a8
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-08 14:55:44 +0000

    correct syntax error

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,7 @@
 module ViewsHelper
   def get_headings(kase, correspondence_type)
     if kase.current_state == "rejected" && correspondence_type.abbreviation == "OFFENDER_SAR"
-      content_tag(:li, t("cases.new.offender_sar.rejected"), class: "page-heading--secondary")
+      content_tag(:sgit pan t("cases.new.offender_sar.rejected"), class: "page-heading--secondary")
     else
       t4c(kase, "", "sub_heading", case_type: kase.pretty_type)
     end
