a54fd4e2c577711843668510cc17e242e235dd2f
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-01-10 16:05:04 +0000

    use method and check whether user is branston

--- app/services/case_filter/open_case_status_filter.rb
+++ app/services/case_filter/open_case_status_filter.rb
@@ -27,14 +27,22 @@ module CaseFilter
         end
       end
 
-      # To ensure 'rejected' is at the end of the filter list, .delete returns the
-      # value of the given key. It is then immediately assigned back.
-      state_choices["rejected"] = state_choices.delete("rejected")
+      offender_sar_move_state_filter_option_to_end(state_choices, "rejected")
 
       { filter_open_case_status: state_choices }
     end
 
   private
+
+    # For users dealing with Offender SARs
+    # To ensure 'rejected' is at the end of the filter list, .delete returns the
+    # value of the given key. It is then immediately assigned back.
+    def offender_sar_move_state_filter_option_to_end(filters, filter_key)
+      if @user.permitted_correspondence_types.any? { |h| h[:abbreviation] == "OFFENDER_SAR" }
+        filters[filter_key] = filters.delete(filter_key)
+        filters
+      end
+    end
 
     def get_tranlation_of_state_for_filter(sub_class, state)
       I18n.t("filters.filter_open_case_status.#{state}", default: nil) || TranslateForCase.translate(sub_class, "state", state)
