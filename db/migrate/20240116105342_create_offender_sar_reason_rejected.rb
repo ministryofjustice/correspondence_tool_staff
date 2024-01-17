0ebe943d941eb5c1adb00429bcd94a07529a1e6c
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-01-17 10:00:14 +0000

    redo migration

--- db/migrate/20240116105342_create_offender_sar_reason_rejected.rb
+++
@@ -1,8 +0,0 @@
-class CreateOffenderSarReasonRejected < ActiveRecord::Migration[6.1]
-  def up
-    create_table :reason_rejected do |t|
-      t.column :reasons, :string
-      t.timestamps
-    end
-  end
-end
