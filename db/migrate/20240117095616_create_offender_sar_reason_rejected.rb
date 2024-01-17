0564fee781af5a06563f32996039cf580883cfe3
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-01-17 13:56:44 +0000

    add belongs_to

--- db/migrate/20240117095616_create_offender_sar_reason_rejected.rb
+++ db/migrate/20240117095616_create_offender_sar_reason_rejected.rb
@@ -1,6 +1,7 @@
 class CreateOffenderSarReasonRejected < ActiveRecord::Migration[6.1]
   def change
     create_table :offender_sar_reason_rejecteds do |t|
+      t.belongs_to :case
       t.column :reason_rejected, :string, index: true
       t.timestamps
     end
