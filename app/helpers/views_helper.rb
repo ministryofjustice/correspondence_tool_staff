dde9ca985acb4985b9bb41b7a4fa906269e131ec
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-17 09:55:44 +0000

    use variable to test type of OFFENDER SAR

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,8 @@
 module ViewsHelper
   def get_headings(kase, correspondence_type)
-    if kase.current_state == "rejected" && correspondence_type.abbreviation == "OFFENDER_SAR"
+    if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
       content_tag(:span, t("cases.new.offender_sar.rejected"), class: "page-heading--secondary")
+      t4c(kase, "", "rejected", case_type: kase.pretty_type)
     else
       t4c(kase, "", "sub_heading", case_type: kase.pretty_type)
     end
