6c57b437fa3b3bc61be0382e6f346a1ad62c140f
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-18 15:41:28 +0000

    correct syntax for rejected title

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,7 @@
 module ViewsHelper
   def get_headings(kase, correspondence_type)
     if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
-      t4c(kase, "", t("rejected"), case_type: kase.pretty_type)
+      t4c(kase, "", t("cases.new.offender_sar.rejected"), case_type: kase.pretty_type)
     else
       t4c(kase, "", "sub_heading", case_type: kase.pretty_type)
     end
