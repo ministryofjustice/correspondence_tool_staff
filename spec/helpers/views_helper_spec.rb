d723174969c745115858404eb822486db88e40d1
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-19 01:14:19 +0000

    write view test

--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -1,10 +1,23 @@
 require "rails_helper"
+  describe ViewsHelper, type: :helper do
+    include ViewsHelper
 
-describe "Creating an offender sar" do
-  context "adding a rejected offender sar case" do
-    it "should display the title for a rejected offender sar" do
-      @kase = described_class.new
-      expect(get_headings(kase, "cases.new.offender_sar.rejected").to eq("Create a rejected offender sar"))
+
+    describe "Creating an offender sar" do
+      context "adding a rejected offender sar case" do
+        describe Case, type: :model do
+
+          it "creates an offender object" do
+            # Use the factory to create an instance of Case::Sar::Offender
+            kase = FactoryBot.build(:case)
+
+            # Assuming get_headings returns a string
+            headings = get_headings(kase, "OFFENDER_SAR")
+
+            # Replace "Your expected title" with the actual expected result
+            expect(headings).to eq("Create a rejected offender sar")
+          end
+        end
+      end
     end
   end
-end
