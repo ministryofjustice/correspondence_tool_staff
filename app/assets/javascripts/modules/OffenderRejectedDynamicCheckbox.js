db1fdf6c30df23b11c5ad9374387c7a640937cb7
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-01-24 16:51:23 +0000

    refactor as  moj module

--- app/assets/javascripts/modules/OffenderRejectedDynamicCheckbox.js
+++ app/assets/javascripts/modules/OffenderRejectedDynamicCheckbox.js
@@ -1,13 +1,16 @@
-// Offender SAR rejected cases form
-// Provides a dynamic text_area which displays when the user chooses the 'other' checkbox
-
-const other_checkbox = document.getElementById('offender_sar_rejected_reasons_other');
-const text_area = document.querySelector('.display-none #offender_sar_other_rejected_reason');
-const text_area_parent_div = text_area.closest('.display-none');
-other_checkbox.addEventListener('click', function() {
-  if (other_checkbox.checked) {
-    text_area_parent_div.classList.remove("display-none")
-  } else {
-    text_area_parent_div.classList.add("display-none")
+moj.Modules.OffenderRejectedDynamicCheckbox = {
+  // Offender SAR rejected cases form
+  // Provides a dynamic text_area which displays when the user chooses the 'other' checkbox
+  init: function () {
+    const other_checkbox = document.getElementById('offender_sar_rejected_reasons_other');
+    const text_area = document.querySelector('.display-none #offender_sar_other_rejected_reason');
+    const text_area_parent_div = text_area.closest('.display-none');
+    other_checkbox.addEventListener('click', function () {
+      if (other_checkbox.checked) {
+        text_area_parent_div.classList.remove("display-none")
+      } else {
+        text_area_parent_div.classList.add("display-none")
+      }
+    })
   }
-})
+};
