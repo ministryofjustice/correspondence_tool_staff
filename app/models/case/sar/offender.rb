7d8faedf6fb00654dabfddb70a2b51e49faac391
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-01-24 13:21:25 +0000

    humanise the array values and add new attribute for the form

--- app/models/case/sar/offender.rb
+++ app/models/case/sar/offender.rb
@@ -43,24 +43,24 @@ class Case::SAR::Offender < Case::Base
   ].freeze
 
   REJECTED_REASONS = {
-    "cctv_bwcv" => "CCTV",
+    "cctv_bwcv" => "CCTV / BWCV request",
     "change_of_name_certificate" => "Change of name certificate",
-    "court_data_request" => "court_data_request",
-    "data_previously_requested" => "data_previously_requested",
-    "further_identification" => "further_identification",
-    "identification_for_ex_inmate_probation" => "identification_for_ex_inmate_probation",
-    "illegible_handwriting_unreadable_content" => "illegible_handwriting_unreadable_content",
-    "id_required" => "id_required",
-    "invalid_authority" => "invalid_authority",
-    "medical_data" => "medical_data",
-    "observation_book_entries" => "observation_book_entries",
-    "police_data" => "police_data",
-    "social_services_data" => "social_services_data",
-    "telephone_recordings_logs" => "telephone_recordings_logs",
-    "telephone_transcripts" => "telephone_transcripts",
-    "third_party_identification" => "third_party_identification",
-    "what_data_no_data_requested" => "what_data_no_data_requested",
-    "other" => "other",
+    "court_data_request" => "Court data request",
+    "data_previously_requested" => "Data previously provided",
+    "further_identification" => "Further identification",
+    "identification_for_ex_inmate_probation" => "Identification for ex-inmate / probation",
+    "illegible_handwriting_unreadable_content" => "Illegible handwriting / unreadable content",
+    "id_required" => "ID required",
+    "invalid_authority" => "Invalid authority",
+    "medical_data" => "Medical data",
+    "observation_book_entries" => "Observation book entries",
+    "police_data" => "Police data ",
+    "social_services_data" => "Social services data",
+    "telephone_recordings_logs" => "Telephone recordings / logs",
+    "telephone_transcripts" => "Telephone manuscripts",
+    "third_party_identification" => "Third party identification",
+    "what_data_no_data_requested" => "What data / no data requested",
+    "other" => "Other",
   }.freeze
 
   acts_as_gov_uk_date(*GOV_UK_DATE_FIELDS)
@@ -94,6 +94,7 @@ class Case::SAR::Offender < Case::Base
                  is_partial_case: :boolean,
                  partial_case_letter_sent_dated: :date,
                  further_actions_required: :string,
+                 other_rejected_reason: :string,
                  rejected_reasons: [:string, { array: true, default: [] }]
 
   attribute :number_final_pages, :integer, default: 0
