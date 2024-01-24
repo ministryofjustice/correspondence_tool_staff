26b89d19b2f014a21d79bfb409023b5eb0f48e1c
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-24 12:30:09 +0000

    correct syntax for sub_heading

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -3,7 +3,7 @@ module ViewsHelper
     if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
       t4c(kase, "", "cases.new.offender_sar.rejected", case_type: kase.type)
     else
-      t4c(kase, "", t("sub_heading"), case_type: kase.type)
+      t4c(kase, "", "cases.new.offender_sar.sub_heading", case_type: kase.type)
     end
   end
 end
