29aed859d94d86d5ad54d33d59c077ba68f9747e
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-24 13:19:58 +0000

    correct syntax

--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -2,33 +2,30 @@ require "rails_helper"
 
 RSpec.configure do |config|
   config.include AbstractController::Translation
-  end
+end
 
 describe ViewsHelper, type: :helper do
-
   include CasesHelper
 
-    describe "#get_headings" do
-      it "text for rejected case type heading" do
+  describe "#get_headings" do
+    it "text for rejected case type heading" do
+      test_kase = build(:offender_sar_case, :rejected)
 
-        test_kase = build(:offender_sar_case, :rejected)
+      test_correspondence_type = build(:offender_sar_correspondence_type)
 
-        test_correspondence_type = build(:offender_sar_correspondence_type)
+      view_title = get_headings(test_kase, test_correspondence_type)
 
-        view_title = get_headings(test_kase, test_correspondence_type)
-
-        expect(view_title).to eq("Create Rejected Offender SAR case")
-      end
-
-      it "#text for a sub_heading" do
-
-        test_kase = build(:offender_sar_case)
-
-        test_correspondence_type = build(:offender_sar_correspondence_type)
-
-        view_title = get_headings(test_kase, test_correspondence_type)
-
-        expect(view_title).to eq("Create Offender SAR case")
-      end
+      expect(view_title).to eq("Create Rejected Offender SAR case")
     end
+
+    it "#text for a sub_heading" do
+      test_kase = build(:offender_sar_case)
+
+      test_correspondence_type = build(:offender_sar_correspondence_type)
+
+      view_title = get_headings(test_kase, test_correspondence_type)
+
+      expect(view_title).to eq("Create Offender SAR case")
+    end
+  end
 end
