02df781a7d5ed990d5c5cc3cbcf6eba24eb14a02
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-25 15:22:17 +0000

    pass key value as an argument

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,9 +1,9 @@
 module ViewsHelper
-  def get_headings(kase, correspondence_type)
-    if kase.current_state == 'rejected' && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
-      t4c(kase, 'cases.new', 'offender_sar.rejected', case_type: kase.decorate.pretty_type)
+  def get_headings(kase, correspondence_type, key_path)
+    if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
+      t4c(kase, key_path, "offender_sar.rejected", case_type: kase.decorate.pretty_type)
     else
-      t4c(kase, 'cases.new', 'sub_heading', case_type: kase.decorate.pretty_type)
+      t4c(kase, key_path, "sub_heading", case_type: kase.decorate.pretty_type)
     end
   end
 end
--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -8,17 +8,18 @@ describe ViewsHelper, type: :helper do
   include CasesHelper
 
   describe "#get_headings" do
-    let (:test_correspondence_type) { build(:offender_sar_correspondence_type) }
+    let(:test_correspondence_type) { build(:offender_sar_correspondence_type) }
+    let(:key_path) { "cases.new" }
 
     it "returns text for rejected case type heading" do
       test_case = build(:offender_sar_case, :rejected)
-      page_title = get_headings(test_case, test_correspondence_type)
+      page_title = get_headings(test_case, test_correspondence_type, key_path)
       expect(page_title).to eq("Create Rejected Offender SAR case")
     end
 
     it "returns text for sub_heading" do
       test_case = build(:offender_sar_case)
-      page_title = get_headings(test_case, test_correspondence_type)
+      page_title = get_headings(test_case, test_correspondence_type, key_path)
       expect(page_title).to eq("Create Offender SAR case")
     end
   end
