c88a2dcdad2fdf4219222c3e59d877ac90ef5d73
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-30 10:46:14 +0000

    reinstate case type

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,9 +1,9 @@
 module ViewsHelper
   def get_heading(kase, correspondence_type, key_path = "")
     if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
-      t4c(kase, key_path, "offender_sar.rejected", kase.decorate.pretty_type)
+      t4c(kase, key_path, "offender_sar.rejected", case_type: kase.decorate.pretty_type)
     else
-      t4c(kase, key_path, "sub_heading", kase.decorate.pretty_type)
+      t4c(kase, key_path, "sub_heading", case_type: kase.decorate.pretty_type)
     end
   end
 end
