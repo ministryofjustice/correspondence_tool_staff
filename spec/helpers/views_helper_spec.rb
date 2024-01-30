768312c229cee96251fa3f8945e9cdaa344c845f
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-30 10:23:08 +0000

    rename view_helper and remove argument

--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -7,19 +7,19 @@ end
 describe ViewsHelper, type: :helper do
   include CasesHelper
 
-  describe "#get_headings" do
+  describe "#get_heading" do
     let(:test_correspondence_type) { build(:offender_sar_correspondence_type) }
     let(:key_path) { "cases.new" }
 
     it "returns text for rejected case type heading" do
       test_case = build(:offender_sar_case, :rejected)
-      page_title = get_headings(test_case, test_correspondence_type, key_path)
+      page_title = get_heading(test_case, test_correspondence_type, key_path)
       expect(page_title).to eq("Create Rejected Offender SAR case")
     end
 
     it "returns text for sub_heading" do
       test_case = build(:offender_sar_case)
-      page_title = get_headings(test_case, test_correspondence_type, key_path)
+      page_title = get_heading(test_case, test_correspondence_type, key_path)
       expect(page_title).to eq("Create Offender SAR case")
     end
   end
