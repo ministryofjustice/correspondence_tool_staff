1c3b7f57297b9a79b90ae732211cbd61f16cfb82
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2024-01-30 11:19:46 +0000

    remove case type from rejected only

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,7 @@
 module ViewsHelper
   def get_heading(kase, correspondence_type, key_path = "")
     if kase.current_state == "rejected" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
-      t4c(kase, key_path, "offender_sar.rejected", case_type: kase.decorate.pretty_type)
+      t4c(kase, key_path, "offender_sar.rejected")
     else
       t4c(kase, key_path, "sub_heading", case_type: kase.decorate.pretty_type)
     end
