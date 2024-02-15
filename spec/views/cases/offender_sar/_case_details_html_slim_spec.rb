e1a9569c1b7f5bd86d6f6804e727a7b8a31df958
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-02-15 17:23:42 +0000

    update test

--- spec/views/cases/offender_sar/_case_details_html_slim_spec.rb
+++ spec/views/cases/offender_sar/_case_details_html_slim_spec.rb
@@ -64,7 +64,7 @@ describe "cases/offender_sar/case_details.html.slim", type: :view do
     end
 
     it "displays rejected details if present" do
-      rejected_case = (create :offender_sar_case, :rejected).decorate
+      rejected_case = (create :offender_sar_case, :rejected, rejected_reasons: %w[further_identification]).decorate
       assign(:case, rejected_case)
       render partial: "cases/offender_sar/case_details", locals: {
         case_details: rejected_case,
