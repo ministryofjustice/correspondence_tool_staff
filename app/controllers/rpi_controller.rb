96e0214636433d68fcdab9686ec6126eb897d77d
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-02-16 10:09:15 +0000

    Record access time

--- app/controllers/rpi_controller.rb
+++ app/controllers/rpi_controller.rb
@@ -2,6 +2,10 @@ class RpiController < ApplicationController
   before_action :get_request
 
   def download
+    @request.last_accessed_at = Time.current
+    @request.last_accessed_by = current_user.id
+    @request.save!
+
     redirect_to @request.temporary_url
   end
 
--- spec/controllers/rpi_controller_spec.rb
+++ spec/controllers/rpi_controller_spec.rb
@@ -9,14 +9,22 @@ RSpec.describe RpiController, type: :controller do
   let(:object) { instance_double(Aws::S3::Object, presigned_url:) }
 
   before do
-    allow(CASE_UPLOADS_S3_BUCKET).to receive(:object)
-                                     .with(rpi.key)
-                                     .and_return(object)
+    allow(CASE_UPLOADS_S3_BUCKET).to receive(:object).with(rpi.key).and_return(object)
   end
 
   describe "GET #download" do
     before do
       sign_in user
+    end
+
+    it "stores current user and time of download" do
+      expect(rpi.last_accessed_at).to be_nil
+
+      get :download, params: { id: submission_id }
+      rpi.reload
+
+      expect(rpi.last_accessed_at).not_to be_nil
+      expect(rpi.last_accessed_by).to eq user.id
     end
 
     it "redirects to the zipfile's url" do
