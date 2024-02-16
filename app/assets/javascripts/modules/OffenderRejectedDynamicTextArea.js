c9165a2a38c4d5d8b93b316ee98eaad57ad0e7bc
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-02-16 14:40:21 +0000

    use var declaration instead of const

--- app/assets/javascripts/modules/OffenderRejectedDynamicTextArea.js
+++ app/assets/javascripts/modules/OffenderRejectedDynamicTextArea.js
@@ -2,8 +2,8 @@ moj.Modules.OffenderRejectedDynamicTextArea = {
   // Offender SAR reason-rejected cases partial
   // Provides a dynamic text_area which displays when the user chooses the 'other' checkbox
   init: function () {
-    const other_checkbox = document.getElementById('offender_sar_rejected_reasons_other');
-    const text_area = document.getElementById('other-text-area-container');
+    var other_checkbox = document.getElementById('offender_sar_rejected_reasons_other');
+    var text_area = document.getElementById('other-text-area-container');
 
     if (!other_checkbox) return;
 
@@ -17,7 +17,7 @@ moj.Modules.OffenderRejectedDynamicTextArea = {
 
     // Replaces the forms legend class styling to match form-hint
     // as collection_check_boxes requires a legend and form-hint.
-    const form_label = document.querySelectorAll("span.form-label-bold");
+    var form_label = document.querySelectorAll("span.form-label-bold");
     form_label[0].classList.replace("form-label-bold", "form-hint");
 
     // Adds a vertical grey bar to the left of the text_area form-group
