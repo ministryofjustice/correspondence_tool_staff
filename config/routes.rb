bd0687c61f98095e76755b851df6ac275e567e9e
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-03-22 14:44:27 +0000

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   resources :contacts, except: :show do
     get "/new/contact_type", on: :collection, to: "contacts#contact_type"
-    patch "/new/contact_type", on: :collection, to: "contacts#confirm_contact_type"
+    post "/new/contact_type", on: :collection, to: "contacts#confirm_contact_type"
   end
 
   get "/contacts_search", to: "contacts#contacts_search"
