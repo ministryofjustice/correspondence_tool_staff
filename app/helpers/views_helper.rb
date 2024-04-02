241b6e2ce92cb9f1766fbc3ee8502db7ad580a28
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-02 16:18:59 +0100

    Simplify method

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,7 +1,7 @@
 module ViewsHelper
-  def get_heading(kase, correspondence_type, key_path = "")
-    if kase.current_state == "invalid_submission" && correspondence_type.abbreviation == CorrespondenceType.offender_sar.abbreviation
-      t4c(kase, key_path, "offender_sar.rejected")
+  def get_sub_heading(kase, key_path = "")
+    if kase.offender_sar? && kase.rejected?
+      t4c(kase, key_path, "rejected")
     else
       t4c(kase, key_path, "sub_heading", case_type: kase.decorate.pretty_type)
     end
