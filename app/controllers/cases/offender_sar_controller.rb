739e4b2ab685eb0a3be77a4273db923b26883a4c
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-04-03 15:04:04 +0100

    remove method

--- app/controllers/cases/offender_sar_controller.rb
+++ app/controllers/cases/offender_sar_controller.rb
@@ -141,7 +141,6 @@ module Cases
     def edit
       permitted_correspondence_types
       authorize case_type, :can_add_case?
-      get_rejected
       @case.current_step = params[:step]
       apply_date_workaround
     end
