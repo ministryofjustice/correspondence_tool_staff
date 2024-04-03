4d6bd9340de74ce7ec7f334fee7f79634d07cf8a
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-03 09:14:02 +0100

    CDPT-1480 Update sub-heading when editing rejected offender SAR (#2375)

--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -7,19 +7,18 @@ end
 describe ViewsHelper, type: :helper do
   include CasesHelper
 
-  describe "#get_heading" do
-    let(:test_correspondence_type) { build(:offender_sar_correspondence_type) }
+  describe "#get_sub_heading" do
     let(:key_path) { "cases.new" }
 
     it "returns text for rejected case type heading" do
       test_case = build(:offender_sar_case, :rejected)
-      page_title = get_heading(test_case, test_correspondence_type, key_path)
+      page_title = get_sub_heading(test_case, key_path)
       expect(page_title).to eq("Create rejected Offender SAR case")
     end
 
     it "returns text for sub_heading" do
       test_case = build(:offender_sar_case)
-      page_title = get_heading(test_case, test_correspondence_type, key_path)
+      page_title = get_sub_heading(test_case, key_path)
       expect(page_title).to eq("Create Offender SAR case")
     end
   end
