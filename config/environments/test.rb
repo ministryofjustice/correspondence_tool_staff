648df88e446712beb5e57420b9b70575cacc14d8
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-10 15:46:21 +0100

    Remove active storage config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :test
+  # config.active_storage.service = :test
 
   config.active_job.queue_adapter = :test
 
