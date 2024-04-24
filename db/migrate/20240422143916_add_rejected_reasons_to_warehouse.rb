0f6a3ad39bb3a9223a294eddfe37b173af291996
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-04-24 08:01:11 +0100

    refactor migration file

--- db/migrate/20240422143916_add_rejected_reasons_to_warehouse.rb
+++ db/migrate/20240422143916_add_rejected_reasons_to_warehouse.rb
@@ -1,6 +1,8 @@
 class AddRejectedReasonsToWarehouse < ActiveRecord::Migration[7.1]
   def change
-    add_column :warehouse_case_reports, :rejected_reasons, :string # rubocop:disable Rails/BulkChangeTable
-    add_column :warehouse_case_reports, :other_rejected_reason, :string
+    change_table :warehouse_case_reports, bulk: true do |t|
+      t.string :rejected_reasons
+      t.string :other_rejected_reason
+    end
   end
 end
