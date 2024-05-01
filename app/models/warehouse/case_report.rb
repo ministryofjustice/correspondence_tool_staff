879d8b065799a48b23eec3b325f248bc1d5855fd
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-05-01 16:19:50 +0100

    use REJECTED REASONS hash

--- app/models/warehouse/case_report.rb
+++ app/models/warehouse/case_report.rb
@@ -275,7 +275,7 @@ module Warehouse
         case_report.user_dealing_with_vetting = kase.user_dealing_with_vetting&.full_name
         case_report.user_id_dealing_with_vetting = kase.user_dealing_with_vetting&.id
         case_report.case_originally_rejected = humanize_boolean(kase.case_originally_rejected)
-        case_report.rejected_reasons = kase.rejected_reasons
+        case_report.rejected_reasons = kase.rejected_reasons.map { |reason| Case::SAR::Offender::REJECTED_REASONS[reason] }
         case_report.other_rejected_reason = kase.other_rejected_reason
       end
 
