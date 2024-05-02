c07b5ea0fe0ac9bf27ef218d6339872fb63b5fac
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-05-02 14:25:20 +0100

    rubocop

--- db/migrate/20240502125941_change_rejected_reasons_type_to_json_for_warehouse.rb
+++ db/migrate/20240502125941_change_rejected_reasons_type_to_json_for_warehouse.rb
@@ -1,7 +1,7 @@
 class ChangeRejectedReasonsTypeToJsonForWarehouse < ActiveRecord::Migration[7.1]
   def up
     remove_column :warehouse_case_reports, :rejected_reasons, :string # rubocop:disable Rails/BulkChangeTable
-    add_column :warehouse_case_reports, :rejected_reasons, :json, using: 'rejected_reasons::json'
+    add_column :warehouse_case_reports, :rejected_reasons, :json, using: "rejected_reasons::json"
   end
 
   def down
