b832535055693e3907d4c60fbf0f7e925d7601b1
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-05-02 14:19:23 +0100

    migrations for case originally rejected and rejected reasons data type changes

--- db/migrate/20240501152558_change_case_originally_rejected_type_for_warehouse.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCaseOriginallyRejectedTypeForWarehouse < ActiveRecord::Migration[7.1]
-  def up
-    change_column :warehouse_case_reports, :case_originally_rejected, :string
-  end
-
-  def down
-    change_column :warehouse_case_reports, :case_originally_rejected, :boolean
-  end
-end
