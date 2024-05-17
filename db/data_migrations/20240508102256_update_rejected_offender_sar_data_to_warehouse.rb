11a3afade85a61e0c8bbdb4316ab7d61d41dcaef
lucas-shaw <129938801+lucas-shaw@users.noreply.github.com>
2024-05-17 13:11:24 +0100

    Use touch to trigger a save (#2399)
    * use touch to trigger a save
    
    * rubocop
    
    * debugger

--- db/data_migrations/20240508102256_update_rejected_offender_sar_data_to_warehouse.rb
+++ db/data_migrations/20240508102256_update_rejected_offender_sar_data_to_warehouse.rb
@@ -1,7 +1,7 @@
 class UpdateRejectedOffenderSARDataToWarehouse < ActiveRecord::DataMigration
   def up
     Case::Base.offender_sar.where("properties ->> 'case_originally_rejected' = 'true'").find_each do |k|
-      k.update_attribute(:rejected_reasons, Warehouse::CaseReport.rejected_reasons_selection(k)) unless k.readonly? # rubocop:disable Rails/SkipsModelValidations
+      k.touch unless k.readonly?
     end
   end
 end
