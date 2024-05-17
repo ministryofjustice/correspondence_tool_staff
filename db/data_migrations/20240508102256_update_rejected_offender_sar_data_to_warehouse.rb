db691683cf673cdf268f440507108cd6c50787bc
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-05-17 11:32:56 +0100

    rubocop

--- db/data_migrations/20240508102256_update_rejected_offender_sar_data_to_warehouse.rb
+++ db/data_migrations/20240508102256_update_rejected_offender_sar_data_to_warehouse.rb
@@ -1,7 +1,7 @@
 class UpdateRejectedOffenderSARDataToWarehouse < ActiveRecord::DataMigration
   def up
     Case::Base.offender_sar.where("properties ->> 'case_originally_rejected' = 'true'").find_each do |k|
-      k.touch unless k.readonly? # rubocop:disable Rails/SkipsModelValidations
+      k.touch unless k.readonly?
     end
   end
 end
