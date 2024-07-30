9e32715622f22fe9949be9a246e9cc0c99578c93
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-07-30 16:17:39 +0100

    path to data request area

--- app/controllers/cases/data_request_areas_controller.rb
+++ app/controllers/cases/data_request_areas_controller.rb
@@ -1,7 +1,7 @@
 module Cases
   class DataRequestAreasController < ApplicationController
     before_action :set_case
-    before_action :set_data_request_area, only: %i[show edit update destroy]
+    # before_action :set_data_request_area, only: %i[show edit update destroy]
 
     def new
       @data_request_area = DataRequestArea.new
@@ -15,18 +15,23 @@ module Cases
         )
       service.call
 
+      @data_request_area = service.data_request_area
+
       case service.result
       when :ok
         flash[:notice] = t(".success")
-        redirect_to case_path(@case)
+        redirect_to case_data_request_area_path(@case, @data_request_area)
       when :error
         @case = service.case
-        @data_request_area = service.data_request_area
         render :new
       else
         raise ArgumentError, "Unknown result: #{service.result.inspect}"
       end
     end
+
+    # def show
+    #   @data_request_area = DataRequestArea.find(params[:id])
+    # end
 
   private
 
@@ -34,9 +39,9 @@ module Cases
       @case = Case::Base.find(params[:case_id])
     end
 
-    def set_data_request_area
-      @data_request_area = DataRequestAtea.find(params[:id]).decorate
-    end
+    # def set_data_request_area
+    #   @data_request_area = DataRequestArea.find(params[:id])
+    # end
 
     def create_params
       params.require(:data_request_area).permit(:data_request_area_type)
