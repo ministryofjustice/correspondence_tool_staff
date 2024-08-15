12adcbf60cab839b86a485794d6761df80819a05
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-08-15 15:09:03 +0100

    add data request area feature spec

---
+++ spec/features/cases/offender_sar/add_data_request_areas_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+feature "Data Requests for an Offender SAR" do
+  given(:manager) { find_or_create :branston_user }
+  given(:offender_sar_case) { create(:offender_sar_case).decorate }
+
+  background do
+    login_as manager
+  end
+
+  scenario "successfully add a new data request area", js: true do
+    cases_show_page.load(id: offender_sar_case.id)
+    click_on "Record data request"
+    expect(data_request_area_page).to be_displayed
+
+    data_request_area_page.form.choose_area_request_type("prison")
+    click_on "Continue"
+    expect(data_request_area_show_page).to be_displayed
+    expect(page).to have_content("Data request area successfully recorded")
+    expect(page).to have_content("No data requests recorded")
+  end
+
+  scenario "no data entry fails", js: true do
+    cases_show_page.load(id: offender_sar_case.id)
+    click_on "Record data request"
+    expect(data_request_area_page).to be_displayed
+
+    click_on "Continue"
+    expect(data_request_area_page).to be_displayed
+    expect(data_request_area_page).to have_text "Select where the data you are requesting is from"
+  end
+
+  scenario "delete a data request area which has no data requests", js: true do
+    cases_show_page.load(id: offender_sar_case.id)
+    click_on "Record data request"
+    expect(data_request_area_page).to be_displayed
+
+    data_request_area_page.form.choose_area_request_type("prison")
+    click_on "Continue"
+    expect(data_request_area_show_page).to be_displayed
+    expect(page).to have_content("Data request area successfully recorded")
+    expect(page).to have_content("No data requests recorded")
+
+    accept_confirm do
+      click_on "Delete", match: :first
+    end
+  end
+end
