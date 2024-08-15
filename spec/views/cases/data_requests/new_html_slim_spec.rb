f7060320f1bfee6682cc60767309f007a985fc7e
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-08-15 11:47:30 +0100

    update data requests new test

--- spec/views/cases/data_requests/new_html_slim_spec.rb
+++ spec/views/cases/data_requests/new_html_slim_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 describe "cases/data_requests/new", type: :view do
   describe "#new" do
     let(:offender_sar) { create :offender_sar_case }
+    let(:data_request_area) { create :data_request_area }
     let(:page) { data_request_page }
 
     before do
-      assign(:data_request, offender_sar.data_requests.new)
+      assign(:data_request, data_request_area.data_requests.new)
       assign(:case, offender_sar)
+      assign(:data_request_area, offender_sar.data_request_areas)
 
       render
       data_request_page.load(rendered)
@@ -15,7 +17,6 @@ describe "cases/data_requests/new", type: :view do
 
     it "has required content" do
       expect(page.page_heading.heading.text).to eq "Record data request"
-      expect(page.form).to have_location
       expect(page.form).to have_request_type
       expect(page.form).to have_date_from_day
       expect(page.form).to have_date_from_month
