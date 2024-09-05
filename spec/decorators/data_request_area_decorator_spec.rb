7dbeaae28b69ffacffcfda0c8ee65ff2d3329aed
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-09-05 12:11:48 +0100

    add data request area decorator tests

--- spec/decorators/data_request_area_decorator_spec.rb
+++ spec/decorators/data_request_area_decorator_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe DataRequestAreaDecorator, type: :model do
-  describe "#location" do
-    let(:data_request_area) { create(:data_request_area) }
+  let(:data_request_area) { create(:data_request_area) }
 
+  describe "#location" do
     context "without linked organisation" do
       let(:decorated) { data_request_area.decorate }
 
@@ -26,27 +26,96 @@ describe DataRequestAreaDecorator, type: :model do
     end
   end
 
-  # describe "#date_completed" do
-  #
-  # end
-  #
-  # describe "#date_requested" do
-  #
-  # end
-  #
-  # describe "#cached_num_pages" do
-  #
-  # end
-  #
-  # describe "#num_of_requests" do
-  #
-  # end
-  #
-  # describe "#data_request_status_tag" do
-  #
-  # end
-  #
-  # describe "#request_document" do
-  #
-  # end
+  describe "#num_of_requests" do
+    let(:decorated) { data_request_area.decorate }
+
+    it "returns the count of data_requests" do
+      create(:data_request, data_request_area:)
+      create(:data_request, data_request_area:)
+      create(:data_request, data_request_area:)
+
+      expect(decorated.num_of_requests).to eq(3)
+    end
+  end
+
+  describe "#cached_num_pages" do
+    let(:decorated) { data_request_area.decorate }
+
+    it "returns the sum of cached_num_pages for all data_requests" do
+      create(:data_request, data_request_area:, cached_num_pages: 5)
+      create(:data_request, data_request_area:, cached_num_pages: 10)
+
+      expect(decorated.cached_num_pages).to eq(15)
+    end
+  end
+
+  describe "#date_requested" do
+    let(:decorated) { data_request_area.decorate }
+
+    it "returns the earliest date_requested of the associated data_requests" do
+      create(:data_request, data_request_area:, date_requested: 2.days.ago)
+      create(:data_request, data_request_area:, date_requested: 1.day.ago)
+
+      expect(decorated.date_requested).to eq(2.days.ago.to_date)
+    end
+
+    it "returns nil if there are no data_requests" do
+      expect(decorated.date_requested).to be_nil
+    end
+  end
+
+  describe "#date_completed" do
+    let(:decorated) { data_request_area.decorate }
+
+    context "when all data_requests are completed" do
+      it "returns the latest cached_date_received" do
+        create(:data_request, :completed, data_request_area:, cached_date_received: 3.days.ago)
+        create(:data_request, :completed, data_request_area:, cached_date_received: 1.day.ago)
+
+        expect(decorated.date_completed).to eq(1.day.ago.to_date)
+      end
+    end
+
+    context "when not all data_requests are completed" do
+      it "returns an empty string" do
+        create(:data_request, data_request_area:)
+
+        expect(decorated.date_completed).to eq(nil)
+      end
+    end
+  end
+
+  describe "#request_document" do
+    let(:decorated) { data_request_area.decorate }
+
+    it 'returns a humanized version of the data_request_area_type with "records" appended' do
+      allow(data_request_area).to receive(:data_request_area_type).and_return("prison")
+      expect(decorated.request_document).to eq("Prison records")
+    end
+  end
+
+  describe "#data_request_area_status_tag" do
+    let(:decorated) { data_request_area.decorate }
+
+    context "when status is :completed" do
+      it 'returns a green "Completed" tag' do
+        expect(decorated.data_request_area_status_tag(:completed))
+          .to eq("<strong class='govuk-tag govuk-tag--green'>Completed</strong>")
+      end
+    end
+
+    context "when status is :in_progress" do
+      it 'returns a yellow "In progress" tag' do
+        expect(decorated.data_request_area_status_tag(:in_progress))
+          .to eq("<strong class='govuk-tag govuk-tag--yellow'>In progress</strong>")
+      end
+    end
+
+    context "when status is :not_started" do
+      it 'returns a red "Not started" tag' do
+        expect(decorated.data_request_area_status_tag(:not_started))
+          .to eq("<strong class='govuk-tag govuk-tag--red'>Not started</strong>")
+      end
+    end
+  end
 end
