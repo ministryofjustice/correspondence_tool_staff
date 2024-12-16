e3894a10b8409bef6e213fd5ca5203fa186ee9a0
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-12-16 13:45:58 +0000

    Add tests for maintenance page

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe ApplicationController, type: :controller do
+  let(:user) { create(:user) }
+
+  describe "#maintenance_mode" do
+    before do
+      sign_in user
+    end
+
+    context "when maintenance mode is off" do
+      it "redirects requests for the maintenance page to the homepage" do
+        ENV["MAINTENANCE_MODE"] = "OFF"
+        get :maintenance_mode
+        expect(response).to redirect_to("/")
+      end
+    end
+
+    context "when maintenance mode is on" do
+      it "renders the maintenance page" do
+        ENV["MAINTENANCE_MODE"] = "ON"
+        get :maintenance_mode
+        expect(response).to render_template(:maintenance)
+      end
+
+      it "redirects other requests to the maintenance page" do
+        ENV["MAINTENANCE_MODE"] = "ON"
+        @controller = Cases::FiltersController.new
+        get :open
+        expect(response).to redirect_to("/maintenance")
+      end
+    end
+  end
+end
