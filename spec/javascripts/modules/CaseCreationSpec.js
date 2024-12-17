5d11f696f0fa5c51d0dab5c3c63034d67e804614
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-12-17 17:21:58 +0000

    Use teaspoon

--- spec/javascripts/modules/CaseCreationSpec.js
+++
@@ -1,6 +0,0 @@
-describe('Modules.CaseCreation.js', function() {
-  it('should exist with expected constructures', function() {
-    expect(moj.Modules.CaseCreation.init).toBeDefined();
-    expect(moj.Modules.CaseCreation.showHideDeliveryMethodFields).toBeDefined();
-  });
-});
--- spec/javascripts/modules/EllipsisSpec.js
+++
@@ -1,5 +0,0 @@
-describe('Modules.Ellipsis.js', function() {
-  it('should exist with expected constructures', function() {
-    expect(moj.Modules.CaseCreation.init).toBeDefined();
-  });
-});
