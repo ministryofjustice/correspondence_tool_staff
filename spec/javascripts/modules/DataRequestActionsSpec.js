76164428de2f651701d388d6805b971ba7efbe84
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-01-07 10:59:57 +0000

    Enable JS tests (#2527)

---
+++ spec/javascripts/modules/DataRequestActionsSpec.js
@@ -0,0 +1,5 @@
+describe('Modules.DataRequestActions.js', function() {
+  it('should exist with expected functions', function() {
+    expect(moj.Modules.DataRequestActions.init).toBeDefined();
+  });
+});
--- spec/javascripts/modules/DropzoneSpec.js
+++ spec/javascripts/modules/DropzoneSpec.js
@@ -3,27 +3,6 @@ describe('Modules.Dropzone.js', function() {
     expect(moj.Modules.Dropzone.init).toBeDefined();
     expect(moj.Modules.Dropzone.$target).toBeDefined();
   });
-
-  describe('moj.Modules.Dropzone.init', function(){
-    var dropzone;
-
-    beforeAll(function(){
-      var body = document.getElementsByTagName('body')[0];
-      var dropzoneContainer = document.createElement('div');
-      dropzoneContainer.classList.add('dropzone');
-      dropzoneContainer.setAttribute('data-url', window.location.href);
-      dropzoneContainer.setAttribute('data-max-filesize-in-mb',20);
-      //dropzoneContainer.setAttribute('data-max-filesize-in-mb',20);
-      body.appendChild(dropzoneContainer);
-      moj.Modules.Dropzone.init();
-      dropzone = dropzoneContainer
-    });
-
-    it('should change the default error messages',function() {
-      expect(dropzone.dropzone.options.dictFileTooBig)
-        .toEqual('File is too big. Max file size is {{maxFilesize}}MB. We cannot upload this file.')
-    })
-  })
 });
 
 describe('Modules.ExternalLinks.js', function() {
