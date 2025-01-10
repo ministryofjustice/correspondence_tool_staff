6759468542e1b683bf29c85bf72b3c6b0f84cc5e
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-10 21:33:59 +0000

    rename second test to check permissions

--- spec/policies/case/sar/offender_policy_spec.rb
+++ spec/policies/case/sar/offender_policy_spec.rb
@@ -4,13 +4,15 @@ describe Case::SAR::OffenderPolicy do
   subject { described_class }
 
   let(:user) { create :branston_user }
+  let(:offender_sar_case_b) { create(:offender_sar_case) }
 
   permissions :can_record_data_request? do
     it { is_expected.to permit(user, create(:offender_sar_case)) }
     it { is_expected.not_to permit(user, create(:sar_case)) }
   end
+
   permissions :can_send_day_1_email? do
-    it { is_expected.to permit(user, create(:offender_sar)) }
+    it { is_expected.to permit(user, offender_sar_case_b) }
     it { is_expected.not_to permit(user, create(:offender_sar_complaint)) }
   end
 end
