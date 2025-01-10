30ed9cdc7b71591cb8641f21c1f7ea11f3701d03
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-10 16:36:16 +0000

    correct rubocop errors

--- spec/policies/case/sar/offender_policy_spec.rb
+++ spec/policies/case/sar/offender_policy_spec.rb
@@ -5,21 +5,12 @@ describe Case::SAR::OffenderPolicy do
 
   let(:user) { create :branston_user }
 
-  describe "user permissions" do
-    it "checks can record data requests" do
-      permissions :can_record_data_request? do
-        it { is_expected.to permit(user, create(:offender_sar_case)) }
-        it { is_expected.not_to permit(user, create(:sar_case)) }
-      end
-    end
+  permissions :can_record_data_request? do
+    it { is_expected.to permit(user, create(:offender_sar_case)) }
+    it { is_expected.not_to permit(user, create(:sar_case)) }
   end
-
-  describe "actions permitted" do
-    it "checks can send day 1 email" do
-      permissions :can_send_day_1_email? do
-        it { is_expected.to permit(user, create(:offender_sar_case)) }
-        it { is_expected.not_to permit(user, create(:offender_sar_complaint)) }
-      end
-    end
+  permissions :can_send_day_1_email? do
+    it { is_expected.to permit(user, create(:offender_sar_complaint)) }
+    it { is_expected.not_to permit(user, create(:offender_sar_complaint)) }
   end
 end
