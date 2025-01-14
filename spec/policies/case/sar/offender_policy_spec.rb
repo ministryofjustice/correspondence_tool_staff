7b8c7adcc496a10c69f299511d36eaca55e9f150
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-14 10:55:35 +0000

    separated out repeated examples

--- spec/policies/case/sar/offender_policy_spec.rb
+++ spec/policies/case/sar/offender_policy_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe Case::SAR::OffenderPolicy do
   subject { described_class }
+
   let(:user) { create :branston_user }
 
   permissions :can_record_data_request? do
@@ -9,8 +10,15 @@ describe Case::SAR::OffenderPolicy do
     it { is_expected.not_to permit(user, create(:sar_case)) }
   end
 
-  permissions :can_send_day_1_email? do
-    it { is_expected.to permit(user, create(:offender_sar_case)) }
-    it { is_expected.not_to permit(user, create(:offender_sar_complaint)) }
+  context "when Offender SAR cases" do
+    permissions :can_send_day_1_email? do
+      it { is_expected.to permit(user, create(:offender_sar_case)) }
+    end
+  end
+
+  context "when Offender Complaint cases" do
+    permissions :can_send_day_1_email? do
+      it { is_expected.not_to permit(user, create(:offender_sar_complaint)) }
+    end
   end
 end
