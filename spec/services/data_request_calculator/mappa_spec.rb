97dcbeae88350b0f9988878dc5afa2c927fe82a3
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-01-29 12:11:07 +0000

    Add examples to tests and use variables

--- spec/services/data_request_calculator/mappa_spec.rb
+++ spec/services/data_request_calculator/mappa_spec.rb
@@ -6,6 +6,20 @@ describe DataRequestCalculator::Mappa do
   let(:kase) { create(:offender_sar_case) }
   let(:data_request_area) { create(:data_request_area, offender_sar_case: kase) }
   let(:start) { Date.new(2025, 1, 20) }
+  let(:chase_one) { start + 21.days }
+  let(:after_chase_one) { chase_one + 1.day }
+  let(:chase_two) { start + 25.days }
+  let(:after_chase_two) { chase_two + 1.day }
+  let(:chase_three) { start + 29.days }
+  let(:after_chase_three) { chase_three + 1.day }
+
+  # Expected chase schedule
+  # Day | Chase number | Email type
+  # 0   | 0            | Day One
+  # 21  | 1            | Chase
+  # 25  | 2            | Chase Escalation
+  # 29  | 3            | Chase Escalation
+  # 33  | 4            | Chase Overdue
 
   describe "#deadline_days" do
     it "is 20" do
@@ -32,29 +46,29 @@ describe DataRequestCalculator::Mappa do
 
     context "when before first chase" do
       it "returns day after deadline" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
-          expect(calculator.next_chase_date).to eq start + 21.days
+        Timecop.freeze(start) do
+          expect(calculator.next_chase_date).to eq chase_one
         end
       end
     end
 
     context "when first chase not sent and the chase window has passed" do
       it "returns current day" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 22.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_date).to eq Date.current
         end
       end
     end
 
     context "when after 1st chase sent" do
-      let(:last_chase_email) { start + 21.days }
+      let(:last_chase_email) { chase_one }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
       end
 
       it "returns 4 days after previous chase sent" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 22.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_date).to eq last_chase_email + 4.days
         end
       end
@@ -66,7 +80,7 @@ describe DataRequestCalculator::Mappa do
       let(:kase) { create(:offender_sar_case, :closed) }
 
       it "returns nil" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
+        Timecop.freeze(start) do
           expect(calculator.next_chase_type).to be_nil
         end
       end
@@ -74,7 +88,7 @@ describe DataRequestCalculator::Mappa do
 
     context "when before first chase" do
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
+        Timecop.freeze(start) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
@@ -82,28 +96,28 @@ describe DataRequestCalculator::Mappa do
 
     context "when first chase not sent and the chase window has passed" do
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 22.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
     end
 
     context "when after 1st chase sent" do
-      let(:last_chase_email) { start + 21.days }
+      let(:last_chase_email) { chase_one }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
       end
 
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 22.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
     end
 
     context "when next chase date is when kase is past deadline" do
-      let(:last_chase_email) { start + 28.days }
+      let(:last_chase_email) { chase_three }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
@@ -111,7 +125,7 @@ describe DataRequestCalculator::Mappa do
       end
 
       it "returns overdue chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 29.days) do
+        Timecop.freeze(after_chase_three) do
           expect(calculator.next_chase_type).to eq :chase_overdue_email
         end
       end
--- spec/services/data_request_calculator/standard_spec.rb
+++ spec/services/data_request_calculator/standard_spec.rb
@@ -6,6 +6,24 @@ describe DataRequestCalculator::Standard do
   let(:kase) { create(:offender_sar_case) }
   let(:data_request_area) { create(:data_request_area, offender_sar_case: kase) }
   let(:start) { Date.new(2025, 1, 20) }
+  let(:chase_one) { start + 6.days }
+  let(:after_chase_one) { chase_one + 1.day }
+  let(:chase_two) { start + 10.days }
+  let(:after_chase_two) { chase_two + 1.day }
+  let(:chase_seven) { start + 30.days }
+  let(:after_chase_seven) { chase_seven + 1.day }
+
+  # Expected chase schedule
+  # Day | Chase number | Email type
+  # 0   | 0            | Day One
+  # 6   | 1            | Chase
+  # 10  | 2            | Chase
+  # 14  | 3            | Chase Escalation
+  # 18  | 4            | Chase Escalation
+  # 22  | 5            | Chase Escalation
+  # 26  | 6            | Chase Escalation
+  # 30  | 7            | Chase Escalation
+  # 34  | 8            | Chase Overdue
 
   describe "#deadline_days" do
     it "is 5" do
@@ -24,7 +42,7 @@ describe DataRequestCalculator::Standard do
       let(:kase) { create(:offender_sar_case, :closed) }
 
       it "returns nil" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
+        Timecop.freeze(start) do
           expect(calculator.next_chase_date).to be_nil
         end
       end
@@ -32,29 +50,29 @@ describe DataRequestCalculator::Standard do
 
     context "when before first chase" do
       it "returns day after deadline" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
-          expect(calculator.next_chase_date).to eq start + 6.days
+        Timecop.freeze(start) do
+          expect(calculator.next_chase_date).to eq chase_one
         end
       end
     end
 
-    context "when first chase not sent and the chase window has passed" do
+    context "when first chase not sent and the first chase window has passed" do
       it "returns current day" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 7.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_date).to eq Date.current
         end
       end
     end
 
     context "when after 1st chase sent" do
-      let(:last_chase_email) { start + 6.days }
+      let(:last_chase_email) { chase_one }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
       end
 
       it "returns 4 days after previous chase sent" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 7.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_date).to eq last_chase_email + 4.days
         end
       end
@@ -66,7 +84,7 @@ describe DataRequestCalculator::Standard do
       let(:kase) { create(:offender_sar_case, :closed) }
 
       it "returns nil" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
+        Timecop.freeze(start) do
           expect(calculator.next_chase_type).to be_nil
         end
       end
@@ -74,7 +92,7 @@ describe DataRequestCalculator::Standard do
 
     context "when before first chase" do
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20)) do
+        Timecop.freeze(start) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
@@ -82,42 +100,42 @@ describe DataRequestCalculator::Standard do
 
     context "when first chase not sent and the chase window has passed" do
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 7.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
     end
 
     context "when after 1st chase sent" do
-      let(:last_chase_email) { start + 6.days }
+      let(:last_chase_email) { chase_one }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
       end
 
       it "returns standard chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 7.days) do
+        Timecop.freeze(after_chase_one) do
           expect(calculator.next_chase_type).to eq :chase_email
         end
       end
     end
 
     context "when after 2nd chase sent" do
-      let(:last_chase_email) { start + 10.days }
+      let(:last_chase_email) { chase_two }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
       end
 
       it "returns escalation chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 11.days) do
+        Timecop.freeze(after_chase_two) do
           expect(calculator.next_chase_type).to eq :chase_escalation_email
         end
       end
     end
 
     context "when next chase date is when kase is past deadline" do
-      let(:last_chase_email) { start + 28.days }
+      let(:last_chase_email) { chase_seven }
 
       before do
         data_request_area.data_request_emails << create(:data_request_email, email_type: "chase", created_at: last_chase_email)
@@ -125,7 +143,7 @@ describe DataRequestCalculator::Standard do
       end
 
       it "returns overdue chase" do
-        Timecop.freeze(Date.new(2025, 1, 20) + 29.days) do
+        Timecop.freeze(after_chase_seven) do
           expect(calculator.next_chase_type).to eq :chase_overdue_email
         end
       end
