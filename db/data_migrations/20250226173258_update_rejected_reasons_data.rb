27904358ca2c32b272613bf84b5ab2836dd78f46
Hettie Street <43522239+HettieS@users.noreply.github.com>
2025-02-26 18:13:30 +0000

    Add data migration

---
+++ db/data_migrations/20250226173258_update_rejected_reasons_data.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class UpdateRejectedReasonsData < ActiveRecord::DataMigration
+  disable_ddl_transaction!
+
+  def up
+    cases = Case::Base.offender_sar.where("properties -> 'rejected_reasons' @> ?", %w[cctv_bwcv].to_json)
+
+    cases.find_each do |offender_sar_case|
+      reasons = offender_sar_case.properties["rejected_reasons"].map do |reason|
+        reason == "cctv_bwcv" ? "cctv_bwcf" : reason
+      end
+
+      offender_sar_case.properties.merge!("rejected_reasons" => reasons)
+      offender_sar_case.save!
+    end
+  end
+end
