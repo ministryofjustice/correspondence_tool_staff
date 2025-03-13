8f6d753d12d9218823acb794a9551564265129fa
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-03-13 11:43:42 +0000

    Remove data changes

--- db/migrate/20250312113935_add_dps_sensitive_request_type.rb
+++ db/migrate/20250312113935_add_dps_sensitive_request_type.rb
@@ -5,10 +5,6 @@ class AddDpsSensitiveRequestType < ActiveRecord::Migration[7.2]
       ALTER TYPE request_types ADD VALUE 'g1_security';
       ALTER TYPE data_request_area_type ADD VALUE 'dps_sensitive';
     SQL
-
-    DataRequestArea.where(data_request_area_type: "security").find_each do |dra|
-      dra.update!(data_request_area_type: "dps_sensitive")
-    end
   end
   # rubocop:enable Rails/ReversibleMigration
 end
