01db2d31cf08c65131e75713b5f2bcf31c8ce005
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-03-20 11:29:40 +0000

    Remove job

--- app/jobs/request_personal_information_delete_job.rb
+++
@@ -1,8 +0,0 @@
-class RequestPersonalInformationDeleteJob < ApplicationJob
-  queue_as :rpi
-
-  def perform
-    SentryContextProvider.set_context
-    PersonalInformationRequest.ready_to_delete.find_each(&:soft_delete)
-  end
-end
