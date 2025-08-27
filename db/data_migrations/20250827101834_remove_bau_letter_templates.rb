48dc5e5028723e1a4b6ec83aebe6201f5e6edca6
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-08-27 12:26:53 +0100

    Remove templates from database.

---
+++ db/data_migrations/20250827101834_remove_bau_letter_templates.rb
@@ -0,0 +1,13 @@
+class RemoveBauLetterTemplates < ActiveRecord::DataMigration
+  def up
+    rec = LetterTemplate.find_by(abbreviation: "bau-prisoner-disclosed-letter")
+    if rec.present?
+      rec.destroy!
+    end
+
+    rec = LetterTemplate.find_by(abbreviation: "bau-solicitor-disclosed-letter")
+    if rec.present?
+      rec.destroy!
+    end
+  end
+end
