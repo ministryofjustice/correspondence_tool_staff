bfcf17bf9179cf698417e00deb2b512d3e4b592b
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-08-27 16:13:14 +0100

    Script simplification.

--- db/data_migrations/20250827101834_remove_bau_letter_templates.rb
+++ db/data_migrations/20250827101834_remove_bau_letter_templates.rb
@@ -1,13 +1,8 @@
 class RemoveBauLetterTemplates < ActiveRecord::DataMigration
   def up
-    rec = LetterTemplate.find_by(abbreviation: "bau-prisoner-disclosed-letter")
-    if rec.present?
-      rec.destroy!
-    end
-
-    rec = LetterTemplate.find_by(abbreviation: "bau-solicitor-disclosed-letter")
-    if rec.present?
-      rec.destroy!
-    end
+    LetterTemplate.destroy_by(abbreviation: %w[
+      bau-prisoner-disclosed-letter
+      bau-solicitor-disclosed-letter
+    ])
   end
 end
