775dea63cf1bc821d1047943bbe67ca29a9d46e2
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2025-09-11 12:41:41 +0100

    CDPT-2828: Remove conditional validation of DPS Flag (#2660)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,24 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  # NOTE: Evaluate individual attributes to allow multi-step wizard
+  # based on individual attributes being gradually completed
+  # https://stackoverflow.com/questions/4804591/rails-activerecord-validate-single-attribute
   def valid_attributes?(attributes)
     attributes.each_key do |attribute|
       self.class.validators_on(attribute).each do |validator|
+        skip = false
+
+        if validator.options[:if].is_a?(Proc)
+          skip = !instance_exec(&validator.options[:if])
+        end
+
+        if validator.options[:unless].is_a?(Proc)
+          skip = instance_exec(&validator.options[:unless])
+        end
+
+        next if skip
+
         validator.validate_each(self, attribute, send(attribute))
       end
     end
