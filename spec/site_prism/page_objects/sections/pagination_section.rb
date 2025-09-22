2987e705b19f0a06679356bc2ed9b5737d0aaf8e
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2025-09-22 15:51:59 +0100

    Add check for links

--- spec/site_prism/page_objects/sections/pagination_section.rb
+++ spec/site_prism/page_objects/sections/pagination_section.rb
@@ -3,6 +3,7 @@ module PageObjects
     class PaginationSection < SitePrism::Section
       element :prev_page_link, ".govuk-pagination__prev a"
       element :next_page_link, ".govuk-pagination__next a"
+      element :page_number_links, ".govuk-pagination__list"
     end
   end
 end
