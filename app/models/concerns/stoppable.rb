e36d6974f66b41dab44329e6bfa3f2654cae3c6a
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2025-11-25 18:01:47 +0000

    Remove duplicate method

--- app/models/concerns/stoppable.rb
+++ app/models/concerns/stoppable.rb
@@ -17,19 +17,15 @@ module Stoppable
     @stopped_at ||= last_stop_the_clock_transition&.details&.fetch("stop_the_clock_date")&.to_date
   end
 
+  def restarted_at
+    @restarted_at ||= last_restart_the_clock_transition&.details&.fetch("restart_the_clock_date")&.to_date
+  end
+
   def last_stop_the_clock_transition
     @last_stop_the_clock_transition ||= transitions.where(event: "stop_the_clock").order(id: :desc).first
   end
 
   def last_restart_the_clock_transition
     @last_restart_the_clock_transition ||= transitions.where(event: "restart_the_clock").order(id: :desc).first
-  end
-
-  def stopped_at
-    @stopped_at ||= last_stop_the_clock_transition&.details&.fetch("stop_the_clock_date")&.to_date
-  end
-
-  def restarted_at
-    @restarted_at ||= last_restart_the_clock_transition&.details&.fetch("restart_the_clock_date")&.to_date
   end
 end
