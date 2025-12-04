99b5a8a0ab60566018a881e0267fea4f6ac43758
MeenaModhvadia <92523605+MeenaModhvadia@users.noreply.github.com>
2025-12-04 15:21:57 +0000

    remove arrays

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,19 +1,19 @@
 BusinessTime::Config.work_week = %w[mon tue wed thu fri]
 
-ADDITIONAL_BANK_HOLIDAYS = [
-  "2023-07-12", # Battle of the Boyne
-  "2023-08-07", # Summer bank holiday
-  "2023-11-30", # St Andrew's Day
-  "2024-01-02", # 2nd January
-  "2024-03-18", # St Patrick's Day
-  "2024-07-12", # Battle of the Boyne
-  "2024-08-05", # Summer bank holiday
-  "2024-12-02", # St Andrew's Day (substitute day)
-  "2025-01-02", # 2nd January
-  "2025-03-17", # St Patricks's Day
-  "2025-07-14", # Battle of the Boyne (substitute day)
-  "2025-08-04", # Summer bank holiday
-  "2025-12-01", # St Andrew's Day (substitute day)
-].freeze
-
-BusinessTime::Config.holidays = BankHoliday.all.map(&:date)
+# ADDITIONAL_BANK_HOLIDAYS = [
+#   "2023-07-12", # Battle of the Boyne
+#   "2023-08-07", # Summer bank holiday
+#   "2023-11-30", # St Andrew's Day
+#   "2024-01-02", # 2nd January
+#   "2024-03-18", # St Patrick's Day
+#   "2024-07-12", # Battle of the Boyne
+#   "2024-08-05", # Summer bank holiday
+#   "2024-12-02", # St Andrew's Day (substitute day)
+#   "2025-01-02", # 2nd January
+#   "2025-03-17", # St Patricks's Day
+#   "2025-07-14", # Battle of the Boyne (substitute day)
+#   "2025-08-04", # Summer bank holiday
+#   "2025-12-01", # St Andrew's Day (substitute day)
+# ].freeze
+#
+# BusinessTime::Config.holidays = BankHoliday.all.map(&:date)
