6561d0bbacd58ea1c2a25adbf8a4bd8a5526ae9f
MeenaModhvadia <92523605+MeenaModhvadia@users.noreply.github.com>
2025-12-11 14:07:12 +0000

    remove subject_type and requester_type humanize

--- app/decorators/case/sar/standard_decorator.rb
+++ app/decorators/case/sar/standard_decorator.rb
@@ -9,10 +9,6 @@ class Case::SAR::StandardDecorator < Case::BaseDecorator
     object.send_by_email? ? object.email : object.postal_address
   end
 
-  def subject_type_display
-    object.subject_type.humanize
-  end
-
   def third_party_display
     object.third_party == true ? "Yes" : "No"
   end
