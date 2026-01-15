9ce837f7bb8eb927f2c6ef1e844c4087fd6b6a27
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-15 23:17:02 +0000

    Added a guardrail for local dev

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,8 +1,19 @@
 BusinessTime::Config.work_week = %w[mon tue wed thu fri]
 
 Rails.application.config.to_prepare do
-  if defined?(BankHolidays)
-    hols = BankHolidays.last.dates_for(:england_and_wales).map(&:to_date)
-    BusinessTime::Config.holidays = hols
+  begin
+    next unless defined?(BankHolidays)
+
+    # During setup/migrations the table may not exist yet.
+    if ActiveRecord::Base.connection.data_source_exists?("bank_holidays")
+      last = BankHolidays.last
+      if last.present?
+        hols = last.dates_for(:england_and_wales).map(&:to_date)
+        BusinessTime::Config.holidays = hols
+      end
+    end
+  rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid
+    # Database or table not ready yet (e.g. during db:setup/migrate).
+    # Safely skip configuring holidays; it will be set on next boot.
   end
 end
