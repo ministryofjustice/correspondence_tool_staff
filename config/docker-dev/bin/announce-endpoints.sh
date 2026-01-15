6dc92ccdb6b6f4775c05a5714c90987048ca77ca
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-15 19:03:44 +0000

    Docker fixes

---
+++ config/docker-dev/bin/announce-endpoints.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. functions.sh
+
+header "Application Endpoints"
+header_additional "${GREEN}Website:${NC} http://localhost:3000"
+header_additional "${GREEN}PGAdmin (DB)${NC} http://localhost:5050
+
