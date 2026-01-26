f68e7bc8269c1e756a19d7504018449fd1678ddb
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-26 09:41:47 +0000

    Fix some migrate brakeman issues

--- db/migrate/20251217151713_create_bank_holidays.rb
+++ db/migrate/20251217151713_create_bank_holidays.rb
@@ -1,9 +1,8 @@
 class CreateBankHolidays < ActiveRecord::Migration[7.2]
   def change
-    drop_table :bank_holidays, if_exists: true
     create_table :bank_holidays do |t|
       t.json :data, null: false
-      t.string :hash_value , null: false
+      t.string :hash_value, null: false
       t.timestamps
     end
   end
