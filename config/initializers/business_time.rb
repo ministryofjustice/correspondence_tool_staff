19a313e42abf36f78be7daa0481a6abdd4600cb7
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-26 18:45:07 +0000

    Load additional bank holidays from the BankHolidays model for Scotland and Northern Ireland

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -1,11 +1,18 @@
 BusinessTime::Config.work_week = %w[mon tue wed thu fri]
 
 Rails.application.config.after_initialize do
-  unless Rails.env.test?
-    holidays = BankHolidays.last
-
-    # Load additional bank holidays for Scotland and Northern Ireland
-    ADDITIONAL_BANK_HOLIDAYS = BankHolidays.last.dates_for_regions(:scotland, :northern_ireland)
-    BusinessTime::Config.holidays = BankHolidays.last.dates_for(:england_and_wales).map(&:to_date)
+  if Rails.env.test?
+    if BankHolidays.count == 0
+      # In tests, use a fixed set of bank holidays from fixtures
+      fixture_json = File.read(Rails.root.join("spec/fixtures/bank_holidays_response.json"))
+      parsed_fixture = JSON.parse(fixture_json)
+      BankHolidays.create!(data: parsed_fixture, hash_value: "test-fixture")
+    end
   end
+
+  holidays = BankHolidays.last
+
+  # Load additional bank holidays for Scotland and Northern Ireland
+  ADDITIONAL_BANK_HOLIDAYS = BankHolidays.last.dates_for_regions(:scotland, :northern_ireland)
+  BusinessTime::Config.holidays = BankHolidays.last.dates_for(:england_and_wales).map(&:to_date)
 end
