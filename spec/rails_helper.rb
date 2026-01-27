311ce015540a170298239181daa0c978a7401b53
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-27 14:50:26 +0000

    Replace puts with Rails.logger.debug for improved logging in policy specs and case printer

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -142,7 +142,7 @@ RSpec.configure do |config|
       renderer.sub!(/^\./, "")
       fullpath = todays_dir.join(filename)
       File.open(fullpath, "w") { |f| f.write response }
-      puts "\033[0;33mrendered #{renderer} content: #{fullpath}\033[0m"
+      Rails.logger.debug "\033[0;33mrendered #{renderer} content: #{fullpath}\033[0m"
     end
   end
 
