7a4304ba4116b204f9712d70d4c7030e44b54434
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-27 11:16:08 +0000

    Skip Lint/ConstantDefinitionInBlock to ensure the global scope of const

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -5,9 +5,9 @@ Rails.application.config.after_initialize do
     BankHolidaysService.new
   end
 
-  BankHolidays.last
-
-  # Load additional bank holidays for Scotland and Northern Ireland
+  # Load additional bank holidays for Scotland and Northern Ireland and ensure global scope
+  # rubocop:disable Lint/ConstantDefinitionInBlock
   ADDITIONAL_BANK_HOLIDAYS = BankHolidays.last.dates_for_regions(:scotland, :northern_ireland)
+  # rubocop:enable Lint/ConstantDefinitionInBlock
   BusinessTime::Config.holidays = BankHolidays.last.dates_for(:england_and_wales).map(&:to_date)
 end
