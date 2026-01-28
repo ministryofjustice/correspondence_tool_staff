55b1d2c615e6126ad0c50144bfe23b44535234f0
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-01-28 22:20:50 +0000

    Fix string quotes in bank_holidays_summary method for consistency

--- app/helpers/bank_holidays_helper.rb
+++ app/helpers/bank_holidays_helper.rb
@@ -2,7 +2,7 @@ module BankHolidaysHelper
   def bank_holidays_summary(rec)
     data = rec[:data].is_a?(Hash) ? rec[:data] : {}
     regions = data.keys
-    events_count = regions.sum { |r| data[r].is_a?(Hash) && data[r]['events'].is_a?(Array) ? data[r]['events'].size : 0 }
+    events_count = regions.sum { |r| data[r].is_a?(Hash) && data[r]["events"].is_a?(Array) ? data[r]["events"].size : 0 }
     [regions, events_count]
   end
 end
