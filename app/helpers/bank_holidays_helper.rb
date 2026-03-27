0a6256bbe4671312b5f98d5e26fbdc48dc243807
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2026-03-27 02:11:49 +0000

    Refactor dashboard output

--- app/helpers/bank_holidays_helper.rb
+++ app/helpers/bank_holidays_helper.rb
@@ -1,8 +1,18 @@
 module BankHolidaysHelper
-  def bank_holidays_summary(rec)
-    data = rec[:data].is_a?(Hash) ? rec[:data] : {}
-    regions = data.keys
-    events_count = regions.sum { |r| data[r].is_a?(Hash) && data[r]["events"].is_a?(Array) ? data[r]["events"].size : 0 }
-    [regions, events_count]
+  def bank_holidays_summary(bank_holiday)
+    regions = bank_holiday.data.keys
+    num_holidays = regions.sum { |region| bank_holiday.data.dig(region, "events")&.size.to_i }
+
+    [regions, num_holidays]
+  end
+
+  # Invalid/missing dates sink to bottom of sorted events list
+  def bank_holidays_for_region(bank_holiday, region)
+    events = bank_holiday.data.dig(region, "events")
+    return [] unless events
+
+    events
+      .sort_by { |ev| Date.parse(ev["date"].to_s) rescue Date.new(0) } # rubocop:disable Style/RescueModifier
+      .reverse
   end
 end
