a623d3b8fe403487fd581eae469a116b4c09997b
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-04-08 22:41:31 +0100

    Add db-restore command to announce-endpoints script for improved visibility

--- config/docker-dev/bin/announce-endpoints.sh
+++ config/docker-dev/bin/announce-endpoints.sh
@@ -5,5 +5,6 @@
 header "Application Endpoints"
 header_additional "${GREEN}Website:     ${NC} http://localhost:3000"
 header_additional "${GREEN}PGAdmin (DB):${NC} http://localhost:5050 \n"
+header_additional "${GREEN}DB restore:   ${YELLOW}make db-restore${NC}"
 header_additional "${GREEN}App terminal: ${YELLOW}make shell${NC}" "close"
 
