3a162cdb53ba0b581b654d75c4bdbf69060fdd08
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2026-04-09 13:45:43 +0100

    Refactor `reports_cache_rake_spec` to streamline `before` block setup

--- spec/lib/tasks/reports_cache_rake_spec.rb
+++ spec/lib/tasks/reports_cache_rake_spec.rb
@@ -5,14 +5,11 @@ RSpec.describe "reports:refresh_cache rake task" do
   before do
     Rake.application.rake_require("tasks/reports_cache")
     Rake::Task.define_task(:environment)
+    task.reenable
   end
 
   let(:task_name) { "reports:refresh_cache" }
   let(:task) { Rake::Task[task_name] }
-
-  before do
-    task.reenable
-  end
 
   it "invokes the Reports::CacheRefresher service" do
     allow(Reports::CacheRefresher).to receive(:call).and_return({ successes: 1, failures: 0, duration_ms: 10.0 })
