56d3dc9593db0007ff7091c71d8ded1f9788ed33
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2026-05-08 10:09:46 +0100

    [CDPTKAN-811] Prevent Commissioning Document Nil error (#2787)

--- spec/controllers/cases/commissioning_documents_controller_spec.rb
+++ spec/controllers/cases/commissioning_documents_controller_spec.rb
@@ -4,35 +4,55 @@ RSpec.describe Cases::CommissioningDocumentsController, type: :controller do
   let(:manager) { find_or_create :branston_user }
   let(:offender_sar_case) { create :offender_sar_case }
   let(:data_request_area) { create :data_request_area, offender_sar_case: }
-  let(:commissioning_document) { create(:commissioning_document, data_request_area:) }
-
-  let(:params) do
-    {
-      case_id: data_request_area.case_id,
-      data_request_area_id: data_request_area.id,
-    }
-  end
 
   before do
     sign_in manager
   end
 
+  # Commissioning Document entries are created automatically when a DataRequestArea is created.
+  # However there are legacy records without any CommissionDocument which may be accessed for posterity.
   describe "#download" do
-    let(:params) do
-      {
-        id: commissioning_document.id,
-        case_id: offender_sar_case.id,
-        data_request_area_id: data_request_area.id,
-      }
+    context "when the commissioning document exists" do
+      let(:commissioning_document) { create(:commissioning_document, data_request_area:) }
+
+      let(:params) do
+        {
+          id: commissioning_document.id,
+          case_id: offender_sar_case.id,
+          data_request_area_id: data_request_area.id,
+        }
+      end
+
+      it "downloads the commissioning document" do
+        get(:download, params:)
+        expect(response.headers["Content-Disposition"]).to match(/filename=".*docx"/)
+      end
     end
 
-    it "downloads the commissioning document" do
-      get(:download, params:)
-      expect(response.headers["Content-Disposition"]).to match(/filename=".*docx"/)
+    context "when the commissioning document does not exist" do
+      before do
+        data_request_area.update!(commissioning_document: nil)
+      end
+
+      let(:params) do
+        {
+          id: "non-existent-id",
+          case_id: offender_sar_case.id,
+          data_request_area_id: data_request_area.id,
+        }
+      end
+
+      it "does not attempt to download and redirects with an alert" do
+        get(:download, params:)
+        expect(response).to redirect_to(case_data_request_area_path(offender_sar_case, data_request_area))
+        expect(flash[:alert]).to eq(I18n.t("cases.commissioning_documents.not_found"))
+      end
     end
   end
 
   describe "#send_email" do
+    let(:commissioning_document) { create(:commissioning_document, data_request_area:) }
+
     let(:params) do
       {
         id: commissioning_document.id,
@@ -40,6 +60,7 @@ RSpec.describe Cases::CommissioningDocumentsController, type: :controller do
         data_request_area_id: data_request_area.id,
       }
     end
+
     let(:service) { instance_double(CommissioningDocumentEmailService, send!: nil) }
 
     before do
